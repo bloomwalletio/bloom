a364d3671e3835173f7c2c671585f2cafcab1b59
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-06-26 03:41:09 -0500

    chore: cleanup workflows (#19)
    * chore: remove v1 workflow
    
    * chore: update workflows
    
    * chore: cleanup build workflows:
    
    * chore: update workflows
    
    * chore: add reusable workflow
    
    * remove sub dir
    
    * fixed env vars
    
    * use only string inputs
    
    * remove secrets from called workflow
    
    * cleanup names
    
    * chore: rename most workflow jobs
    
    * cleanup more workflows
    
    * invert workflow param
    
    * remove testing config
    
    * use reusable build for release desktop
    
    * add reusable release workflow
    
    * format workflow files
    
    * rename workflow files for desktop-only
    
    * chore: add descriptions
    
    * remove acl policy for s3 objects
    
    ---------
    
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>

--- packages/desktop/scripts/notarize.macos.js
+++ packages/desktop/scripts/notarize.macos.js
@@ -11,15 +11,15 @@ module.exports = async (appBundleId, appName) => {
         return true
     }
 
-    const APPLE_ID = process.env.FIREFLY_APPLE_ID
-    const APPLE_ID_PASSWORD = process.env.FIREFLY_APPLE_ID_PASSWORD
+    const APPLE_ID = process.env.BLOOM_APPLE_ID
+    const APPLE_ID_PASSWORD = process.env.BLOOM_APPLE_ID_PASSWORD
 
     if (!APPLE_ID) {
-        throw Error('Notarization failed: Environment variable "FIREFLY_APPLE_ID" is not defined')
+        throw Error('Notarization failed: Environment variable "BLOOM_APPLE_ID" is not defined')
     }
 
     if (!APPLE_ID_PASSWORD) {
-        throw Error('Notarization failed: Environment variable "FIREFLY_APPLE_ID_PASSWORD" is not defined')
+        throw Error('Notarization failed: Environment variable "BLOOM_APPLE_ID_PASSWORD" is not defined')
     }
 
     await notarize({
