86421625a06954304868302a3db59816f79cd12a
Jean Ribeiro <contact@jeanribeiro.dev>
2023-06-28 22:34:14 -0300

    chore: removes unnecessary comments in pincode manager

--- packages/desktop/electron/lib/managers/pincode.manager.ts
+++ packages/desktop/electron/lib/managers/pincode.manager.ts
@@ -1,43 +1,15 @@
 import { ipcRenderer } from 'electron'
 
 export default class PincodeManager {
-    /**
-     * Sets pincode in keychain
-     *
-     * @method set
-     *
-     * @param {string} key
-     * @param {string} pincode
-     *
-     * @returns {Promise}
-     */
     public async set(key: string, pincode: string): Promise<unknown> {
         return ipcRenderer.invoke('keychain-set', key, pincode)
     }
-    /**
-     * Verifies user entered pincode against the one stored in keychain
-     *
-     * @method verify
-     *
-     * @param {string} key
-     * @param {string} pincode
-     *
-     * @returns {Promise}
-     */
+
     public async verify(key: string, pincode: string): Promise<boolean> {
         const storedPincode = await ipcRenderer.invoke('keychain-get', key)
         return storedPincode === pincode
     }
 
-    /**
-     * Removes pincode entry from the keychain
-     *
-     * @method remove
-     *
-     * @param {string} key
-     *
-     * @returns {Promise}
-     */
     public async remove(key: string): Promise<unknown> {
         return ipcRenderer.invoke('keychain-remove', key)
     }
