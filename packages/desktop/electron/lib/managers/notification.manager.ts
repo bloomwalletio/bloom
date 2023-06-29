3e773e7e1a4684caf1efc7aca8582f7c063c7c17
Jean Ribeiro <contact@jeanribeiro.dev>
2023-06-28 22:33:33 -0300

    chore: removes unnecessary comments in notification manager

--- packages/desktop/electron/lib/managers/notification.manager.ts
+++ packages/desktop/electron/lib/managers/notification.manager.ts
@@ -1,12 +1,6 @@
 import { ipcRenderer } from 'electron'
 
 export default class NotificationManager {
-    /**
-     * Create and show a native notification
-     * @param message - the notification message
-     * @param contextData - data to associate with the notification
-     * @returns void
-     */
     public notify(message: string, contextData: unknown): void {
         const notification = new Notification('Bloom', {
             body: message,
