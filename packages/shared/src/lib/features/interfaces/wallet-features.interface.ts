52d8008d121c15b7f722282b9c7f61f5efd68ddb
Mark Nardi <nardimark7@gmail.com>
2023-06-29 11:12:25 +0200

    feat: walletconnect (#114)
    * feat: walletconnect
    
    * fix: fix ts
    
    * yarn lock
    
    * fix: move to new folder structure
    
    * fix: use coin type
    
    * chore: use localize
    
    * feat: add testnet compatibility with walletconnect
    
    ---------
    
    Co-authored-by: Tuditi <daviddetroch@pm.me>

--- packages/shared/src/lib/features/interfaces/wallet-features.interface.ts
+++ packages/shared/src/lib/features/interfaces/wallet-features.interface.ts
@@ -5,7 +5,7 @@ export interface IWalletFeatures extends IFeatureFlag {
     sendAndReceive: IFeatureFlag & {
         nft: IFeatureFlag
     }
-    newSendFlow: IFeatureFlag
+    walletConnect: IFeatureFlag
     assets: IFeatureFlag & {
         burnAsset: IFeatureFlag
     }
