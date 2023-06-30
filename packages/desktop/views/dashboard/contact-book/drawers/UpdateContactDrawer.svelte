3386c30a132779cf2fe4c0ea85c242a438a969e9
Mark Nardi <nardimark7@gmail.com>
2023-06-30 17:24:36 +0200

    feat: edit contact (#202)

--- packages/desktop/views/dashboard/contact-book/drawers/UpdateContactDrawer.svelte
+++ packages/desktop/views/dashboard/contact-book/drawers/UpdateContactDrawer.svelte
@@ -3,13 +3,57 @@
     import { DrawerTemplate } from '@components'
     import { Router } from '@core/router'
     import { ContactBookRoute } from '../contact-book-route.enum'
+    import { ContactManager, selectedContact } from '@core/contacts'
+    import { Button, TextInput } from '@ui'
+    import { showAppNotification } from '@auxiliary/notification'
 
     export let drawerRouter: Router<unknown>
+
+    let contactName = $selectedContact.name
+    let contactNote = $selectedContact.note
+    let contactErrors = {
+        name: '',
+        note: '',
+    }
+
+    $: contactName, contactNote, resetErrors()
+
+    function resetErrors(): void {
+        contactErrors = {
+            name: '',
+            note: '',
+        }
+    }
+
+    function updateContact(): void {
+        resetErrors()
+        try {
+            const contact = { ...$selectedContact, name: contactName, note: contactNote }
+            ContactManager.validateContact(contact)
+            ContactManager.updateContact($selectedContact.id, contact)
+            showAppNotification({
+                type: 'success',
+                message: localize('notifications.updateContact.success'),
+                alert: true,
+            })
+
+            drawerRouter.previous()
+        } catch (error) {
+            contactErrors.name = error.name
+            contactErrors.note = error.note
+        }
+    }
 </script>
 
 <DrawerTemplate
     title={localize(`views.dashboard.drawers.contactBook.${ContactBookRoute.UpdateContact}.title`)}
     {drawerRouter}
 >
-    <div class="flex flex-col justify-between mb-6">Update contact</div>
+    <div class="h-full flex flex-col justify-between">
+        <div class="flex flex-col justify-between gap-4">
+            <TextInput bind:value={contactName} placeholder={localize('general.name')} error={contactErrors.name} />
+            <TextInput bind:value={contactNote} placeholder={localize('general.note')} error={contactErrors.note} />
+        </div>
+        <Button onClick={updateContact}>{localize('actions.save')}</Button>
+    </div>
 </DrawerTemplate>
