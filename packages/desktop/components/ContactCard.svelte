ac6eaa04171954945ad1c1a16d6c2a57d5f70fa6
Mark Nardi <nardimark7@gmail.com>
2023-06-30 15:57:13 +0200

    fix: asd

---
+++ packages/desktop/components/ContactCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { ClickableTile, FontWeight, Text, TextType } from '@ui'
+    import { UiEventFunction } from '@core/utils'
+    import { IContact } from '@core/contacts'
+
+    export let contact: IContact = undefined
+    export let onCardClick: UiEventFunction
+</script>
+
+<ClickableTile classes="bg-white border border-solid border-gray-200 dark:border-transparent" onClick={onCardClick}>
+    <div class="w-full flex flex-col gap-5">
+        <Text type={TextType.h4} fontWeight={FontWeight.semibold}>
+            {contact.name}
+        </Text>
+    </div>
+</ClickableTile>
--- packages/desktop/views/dashboard/contact-book/drawers/ContactListDrawer.svelte
+++ packages/desktop/views/dashboard/contact-book/drawers/ContactListDrawer.svelte
@@ -1,15 +1,43 @@
 <script lang="ts">
     import { localize } from '@core/i18n'
-    import { DrawerTemplate } from '@components'
+    import { DrawerTemplate, ContactCard } from '@components'
     import { Router } from '@core/router'
     import { ContactBookRoute } from '../contact-book-route.enum'
+    import { ContactManager } from '@core/contacts'
+    import { Icon } from '@ui'
+    import { Icon as IconEnum } from '@auxiliary/icon'
 
     export let drawerRouter: Router<unknown>
+
+    const contacts = ContactManager.listContacts()
+
+    function onContactClick(): void {
+        drawerRouter.goTo(ContactBookRoute.Contact)
+    }
+
+    function onAddContactClick(): void {
+        drawerRouter.goTo(ContactBookRoute.AddContact)
+    }
 </script>
 
 <DrawerTemplate
     title={localize(`views.dashboard.drawers.contactBook.${ContactBookRoute.ContactList}.title`)}
     {drawerRouter}
 >
-    <div class="flex flex-col justify-between mb-6">Contact list</div>
+    <div class="flex flex-col justify-between">
+        <div class="flex flex-col justify-between mb-6">
+            {#each contacts as contact}
+                <ContactCard {contact} onCardClick={onContactClick} />
+            {/each}
+        </div>
+
+        <button
+            type="button"
+            class="flex flex-row items-center justify-center w-full space-x-2 bg-transparent text-blue-500 px-8 py-3 text-15 rounded-lg"
+            on:click|stopPropagation={onAddContactClick}
+        >
+            <Icon icon={IconEnum.Plus} height={12} />
+            {localize(`views.dashboard.drawers.contactBook.${ContactBookRoute.ContactList}.title`)}
+        </button>
+    </div>
 </DrawerTemplate>
