8a3306066ec0dd52e445c865510339cedb1e3719
Tuditi <daviddetroch@pm.me>
2023-06-30 15:17:00 +0200

    feat: initial implementation ContactManager

--- packages/shared/src/lib/core/contacts/classes/contact-manager.class.ts
+++ packages/shared/src/lib/core/contacts/classes/contact-manager.class.ts
@@ -1,58 +1,100 @@
 import { getActiveProfile, updateActiveProfile } from '@core/profile/stores'
 import { generateRandomId } from '@core/utils'
 import { getIconColorFromString } from '@core/account/utils'
-import type { IContactAddress, IContact, INetworkContactAddressMap, IContactMetadata } from '../interfaces'
+import type { IContactAddress, IContact, IContactMetadata } from '../interfaces'
 
 export class ContactManager {
-    static addContact ({ name, note } : {name: string, note?: string} , {networkId, addressName, address}: {networkId: string, addressName: string, address: string}): void {
+    static getContact(contactId: string): IContact {
+        const profile = getActiveProfile()
+        return profile.contacts?.[contactId]
+    }
+
+    static addContact(
+        { name, note }: { name: string; note?: string },
+        { networkId, addressName, address }: { networkId: string; addressName: string; address: string }
+    ): void {
         const profile = getActiveProfile()
 
         const contact: IContact = {
             id: generateRandomId(),
             name,
             color: getIconColorFromString(),
-            addresses: [address],
+            addresses: [],
             note,
         }
-
-        const contactAddress: IContactAddress = {
-            address,
-            contactId: contact.id,
-            addressName,
-        }
-
         profile.contacts[contact.id] = contact
 
-        if (!profile.networkContactAddresses[networkId]) {
-            profile.networkContactAddresses[networkId] = {}
-        }
-        profile.networkContactAddresses[networkId][contact.id] = contactAddress
+        ContactManager.addContactAddress(contact.id, networkId, addressName, address)
 
         updateActiveProfile(profile)
     }
 
-    static editContact(contactId: string, payload: Partial<IContactMetadata>): void {
-        throw new Error('Method not implemented.')
+    static updateContact(contactId: string, payload: Partial<IContactMetadata>): void {
+        const contact = ContactManager.getContact(contactId)
+
+        if (contact) {
+            profile.contacts[contactId] = { ...contact, ...payload }
+        } else {
+            throw new Error(`Profile with contact ID ${contactId} doesn't exist!`)
+        }
     }
-    static removeContact(contactId: string): void {
-        throw new Error('Method not implemented.')
+
+    static deleteContact(contactId: string): void {
+        const contact = ContactManager.getContact(contactId)
+
+        if (contact) {
+            Object.keys(profile.networkContactAddresses).forEach((networkId) => {
+                ContactManager.deleteContractAddresses(contact, networkId)
+            })
+        }
+        delete profile.contacts?.[contactId]
     }
+
     static listContacts(): IContactMetadata[] {
-        throw new Error('Method not implemented.')
+        return Object.entries(profile.contacts)
     }
-    static addContactAddress(contactId: string, networkId: string, addressName: string, address: string): IContactAddress {
-        throw new Error('Method not implemented.')
+
+    static addContactAddress(
+        contactId: string,
+        networkId: string,
+        addressName: string,
+        address: string
+    ): IContactAddress {
+        const contact = ContactManager.getContact(contactId)
+
+        if (contact) {
+            contact.address.push(address)
+            const contactAddress: IContactAddress = {
+                address,
+                contactId: contact.id,
+                addressName,
+            }
+
+            if (!profile.networkContactAddresses[networkId]) {
+                profile.networkContactAddresses[networkId] = {}
+            }
+            profile.networkContactAddresses[networkId][contact.id] = contactAddress
+        }
     }
-    static editContactAddress(contactId: string, networkId: string, payload: Partial<IContactAddress>): void {
-        throw new Error('Method not implemented.')
+
+    // TODO: This requires previous address information to be submitted.
+    // static updateContactAddress(contactId: string, networkId: string, payload: Partial<IContactAddress>): void {}
+
+    // TODO: do we need to expose this?
+    static deleteContactAddresses(contactId: string, networkId: string): void {
+        const contact = ContactManager.getContact(contactId)
+
+        contact.addresses.forEach((address) => {
+            delete profile.networkContactAddresses?.[networkId]?.[address]
+        })
     }
-    static removeContactAddresses(contactId: string, networkId: string): void {
-        throw new Error('Method not implemented.')
+    static listContactAddresses(contactId: string): string[] {
+        const contact = ContactManager.getContact(contactId)
+        return contact.addresses ?? []
     }
-    static listContactAddresses(contactId: string): INetworkContactAddressMap {
-        throw new Error('Method not implemented.')
-    }
-    static listContactAddressesForNetwork(networkId: string): INetworkContactAddressMap {
-        throw new Error('Method not implemented.')
+
+    static listContactAddressesForNetwork(networkId: string): IContactAddressMap {
+        const profile = getActiveProfile()
+        return profile.networkContactAddresses[networkId]
     }
 }
