3cd86b8936d52192a751419aa30849eeda0abcf2
Jean Ribeiro <contact@jeanribeiro.dev>
2023-07-02 23:42:19 -0300

    feat: adds AddNetworkAddressDrawer content

--- packages/desktop/views/dashboard/contact-book/drawers/AddNetworkAddressDrawer.svelte
+++ packages/desktop/views/dashboard/contact-book/drawers/AddNetworkAddressDrawer.svelte
@@ -1,15 +1,41 @@
 <script lang="ts">
-    import { localize } from '@core/i18n'
-    import { DrawerTemplate } from '@components'
-    import { Router } from '@core/router'
     import { ContactBookRoute } from '../contact-book-route.enum'
 
+    import { Button, TextInput, NetworkInput } from '@ui'
+    import { DrawerTemplate } from '@components'
+
+    import { ContactManager, selectedContact } from '@core/contacts'
+    import { localize } from '@core/i18n'
+    import { Router } from '@core/router'
+
     export let drawerRouter: Router<unknown>
+
+    let address: string = ''
+    let addressName: string = ''
+    let networkSelection: { networkId: string; address?: string } | undefined
+
+    function onSaveClick(): void {
+        ContactManager.addContactAddress(
+            $selectedContact?.id,
+            networkSelection?.networkId,
+            addressName,
+            address
+        )
+
+        drawerRouter.previous()
+    }
 </script>
 
 <DrawerTemplate
     title={localize(`views.dashboard.drawers.contactBook.${ContactBookRoute.AddNetworkAddress}.title`)}
     {drawerRouter}
 >
-    <div class="flex flex-col justify-between mb-6">Add network address</div>
+    <add-address class="flex flex-col gap-4">
+        <NetworkInput bind:networkSelection showLayer2={true} />
+        <TextInput bind:value={addressName} placeholder={localize('general.addressName')} />
+        <TextInput bind:value={address} placeholder={localize('general.address')} />
+    </add-address>
+    <div slot="footer">
+        <Button onClick={onSaveClick} classes="w-full">{localize('actions.save')}</Button>
+    </div>
 </DrawerTemplate>
