65c629807aa25975ddf8f3f0b63e00ba6aaf8081
Jean Ribeiro <contact@jeanribeiro.dev>
2023-07-02 23:45:20 -0300

    feat: adds all ContactAddressCard functionality and improves code

--- packages/desktop/components/ContactAddressCard.svelte
+++ packages/desktop/components/ContactAddressCard.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
     import { Button, Text, MeatballMenuButton, MenuItem, Modal, NetworkIcon } from '@ui'
     import { ButtonSize, FontWeight } from '@ui/enums'
-    import { IContactAddressMap } from '@core/contacts'
+
+    import { IContactAddressMap, setSelectedNetworkId } from '@core/contacts'
     import { NetworkId } from '@core/network'
     import { setClipboard, truncateString } from '@core/utils'
     import { Icon as IconEnum } from '@auxiliary/icon'
+    import { Router } from '@core/router'
+    import { resetLedgerPreparedOutput, resetShowInternalVerificationPopup } from '@core/ledger'
+    import { NewTransactionType, resetNewTokenTransactionDetails, updateNewTransactionDetails } from '@core/wallet'
 
+    import { openPopup, PopupId } from '@desktop/auxiliary/popup'
+    import { closeDrawer } from '@desktop/auxiliary/drawer'
+
+    import { ContactBookRoute } from '@views/dashboard/contact-book/contact-book-route.enum'
+    import { SendFlowRouter, sendFlowRouter } from '@views/dashboard/send-flow'
+
+    export let drawerRouter: Router<unknown>
     export let networkId: string
     export let contactAddressMap: IContactAddressMap
 
@@ -14,10 +25,36 @@
     function onAddressClick(address: string): void {
         setClipboard(address, true)
     }
+
+    function onEditNetworkAddressesClick(networkId: string): void {
+        setSelectedNetworkId(networkId)
+        drawerRouter.goTo(ContactBookRoute.UpdateNetworkAddresses)
+    }
+    
+    function onRemoveNetworkClick(networkId: string): void {
+        setSelectedNetworkId(networkId)
+        drawerRouter.goTo(ContactBookRoute.RemoveNetworkAddresses)
+    }
+
+    function onSendClick(address: string): void {
+        resetNewTokenTransactionDetails()
+        updateNewTransactionDetails({
+            type: NewTransactionType.TokenTransfer,
+            recipient: { type: 'address', address },
+        })
+        resetLedgerPreparedOutput()
+        resetShowInternalVerificationPopup()
+        sendFlowRouter.set(new SendFlowRouter(undefined))
+        closeDrawer()
+        openPopup({
+            id: PopupId.SendFlow,
+            overflow: true,
+        })
+    }
 </script>
 
-<div class="flex flex-col justify-between bg-white p-6 gap-4 border border-solid border-gray-200 rounded-xl">
-    <div class="flex justify-between">
+<contact-address-card class="flex flex-col justify-between bg-white p-6 gap-4 border border-solid border-gray-200 rounded-xl">
+    <contact-address-head class="flex justify-between">
         <div class="flex items-center gap-2">
             <NetworkIcon networkId={NetworkId.Testnet} />
             <Text fontSize="text-16" fontWeight={FontWeight.semibold}>{networkId}</Text>
@@ -26,27 +63,23 @@
             <MeatballMenuButton onClick={modal?.toggle} />
             <Modal bind:this={modal} position={{ right: '0' }} classes="mt-1.5">
                 <div class="flex flex-col">
-                    <MenuItem icon={IconEnum.Edit} title={'Edit network addresses'} onClick={() => {}} />
-                    <MenuItem icon={IconEnum.Delete} title={'Remove network'} onClick={() => {}} variant="error" />
+                    <MenuItem icon={IconEnum.Edit} title={'Edit network addresses'} onClick={() => onEditNetworkAddressesClick(networkId)} />
+                    <MenuItem icon={IconEnum.Delete} title={'Remove network'} onClick={() => onRemoveNetworkClick(networkId)} variant="error" />
                 </div>
             </Modal>
         </contact-address-menu>
-    </div>
+    </contact-address-head>
     {#each Object.values(contactAddressMap) as contactAddress}
-        <div class="flex justify-between items-center">
-            <div class="flex flex-col">
-                <Text overrideColor classes="text-gray-600" fontWeight={FontWeight.medium}>
+        <contact-address-item class="flex justify-between items-center gap-4">
+            <button type="button" class="flex flex-col flex-1 min-w-0 truncate" on:click={() => onAddressClick(contactAddress.address)}>
+                <Text overrideColor classes="text-gray-600 text-left w-full truncate" fontWeight={FontWeight.medium}>
                     {contactAddress.addressName}
                 </Text>
-                <button type="button" on:click={() => onAddressClick(contactAddress.address)}>
-                    <Text fontSize="text-16" fontWeight={FontWeight.medium}>
-                        {truncateString(contactAddress.address, 9, 9)}
-                    </Text>
-                </button>
-            </div>
-            <icon-container class="flex text-blue-500">
-                <Button size={ButtonSize.Small}>Send</Button>
-            </icon-container>
-        </div>
+                <Text fontSize="text-16" fontWeight={FontWeight.medium}>
+                    {truncateString(contactAddress.address, 9, 9)}
+                </Text>
+            </button>
+            <Button size={ButtonSize.Small} onClick={() => onSendClick(contactAddress.address)}>Send</Button>
+        </contact-address-item>
     {/each}
-</div>
+</contact-address-card>
