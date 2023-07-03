7f5c6b229495d4a84f17a07398fc0ecec8c70344
Jean Ribeiro <contact@jeanribeiro.dev>
2023-07-02 23:44:24 -0300

    feat: adds color in ContactCard

--- packages/desktop/components/ContactCard.svelte
+++ packages/desktop/components/ContactCard.svelte
@@ -8,7 +8,8 @@
 </script>
 
 <ClickableTile classes="bg-white border border-solid border-gray-200 dark:border-transparent" onClick={onCardClick}>
-    <div class="w-full flex flex-col gap-5">
+    <div class="w-full flex gap-2">
+        <span class="h-5 w-5 rounded-full" style="background-color: {contact.color}"></span>
         <Text type={TextType.h4} fontWeight={FontWeight.semibold} classes="truncate">
             {contact.name}
         </Text>
