a2f094c31516d94c4aa2ad38bdf2faffe54c5f72
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-07-07 13:47:11 -0500

    move files back

--- packages/shared/src/components/atoms/index.js
+++ packages/shared/src/components/atoms/index.js
@@ -6,6 +6,7 @@ export * from './labels'
 export * from './pills'
 export * from './tiles'
 
+export { default as AmountBox } from './AmountBox.svelte'
 export { default as BalanceSummaryRow } from './BalanceSummaryRow.svelte'
 export { default as BalanceSummarySection } from './BalanceSummarySection.svelte'
 export { default as ColoredCircle } from './ColoredCircle.svelte'
