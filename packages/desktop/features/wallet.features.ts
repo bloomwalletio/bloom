bd21a66cafb97a44e599b3ca3b7d24eae4ebc3f7
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-07-10 17:25:20 -0500

    chore: enable WalletConnect

--- packages/desktop/features/wallet.features.ts
+++ packages/desktop/features/wallet.features.ts
@@ -12,7 +12,7 @@ const walletFeatures: IWalletFeatures = {
         },
     },
     walletConnect: {
-        enabled: false,
+        enabled: true,
     },
     contacts: {
         enabled: false,
