7a872f49c9b136b6d52787f1cff53378c42b9bca
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-07-11 11:50:24 -0500

    Fix type checking

--- packages/shared/src/lib/core/contact/utils/validateContactNetworkSelection.ts
+++ packages/shared/src/lib/core/contact/utils/validateContactNetworkSelection.ts
@@ -1,7 +1,7 @@
 import { DestinationNetwork } from '@core/layer-2/enums'
 
 export function validateContactNetworkSelection(networkId: string): void {
-    if (!networkId || !Object.values(DestinationNetwork).includes(networkId)) {
+    if (!networkId || !Object.values(DestinationNetwork).includes(networkId as DestinationNetwork)) {
         throw new Error('Invalid network selection input')
     }
 }
