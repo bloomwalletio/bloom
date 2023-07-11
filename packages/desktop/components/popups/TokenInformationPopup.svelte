9e6a7de81f58c6ec79829d8a029249339f47a646
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-07-11 03:13:44 -0500

    feat: add untracking functionality (#203)
    * add untracking functionality
    
    * fix reactivity issue
    
    * add import to persisted assets
    
    * update branch
    
    * code quality improvements
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/desktop/components/popups/TokenInformationPopup.svelte
+++ packages/desktop/components/popups/TokenInformationPopup.svelte
@@ -12,7 +12,6 @@
         NewTransactionType,
     } from '@core/wallet'
     import { openPopup, PopupId, updatePopupProps } from '@desktop/auxiliary/popup'
-    import features from '@features/features'
     import {
         Button,
         Text,
@@ -29,6 +28,8 @@
 
     export let asset: IAsset
     export let activityId: string = undefined
+
+    $: showAssetActionsMenuButton = asset.standard === TokenStandard.Irc30 || asset.standard === TokenStandard.Erc20
 
     function onSkipClick(): void {
         unverifyAsset(asset.id, NotVerifiedStatus.Skipped)
@@ -67,7 +68,7 @@
         })
         sendFlowRouter.set(new SendFlowRouter(undefined, SendFlowRoute.SelectRecipient))
         openPopup({
-            id: features.wallet.newSendFlow.enabled ? PopupId.SendFlow : PopupId.SendForm,
+            id: PopupId.SendFlow,
             overflow: true,
         })
     }
@@ -95,7 +96,7 @@
                     />
                 {/if}
             </div>
-            {#if asset.standard === TokenStandard.Irc30}
+            {#if showAssetActionsMenuButton}
                 <AssetActionsButton {asset} />
             {/if}
         </div>
