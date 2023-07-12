41b1f577c8bd3237a5702d38d592fcc1e15b2443
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-07-12 16:35:00 -0500

    Cleanup contact address validation function

--- packages/shared/src/lib/core/contact/utils/validateContactAddress.ts
+++ packages/shared/src/lib/core/contact/utils/validateContactAddress.ts
@@ -1,13 +1,8 @@
-import { get } from 'svelte/store'
-
-import { selectedAccount } from '@core/account/stores'
 import { localize } from '@core/i18n'
 import { DestinationNetwork } from '@core/layer-2/enums'
-import { DEFAULT_CHAIN_CONFIGURATIONS } from '@core/network/constants'
-import { getActiveNetworkId } from '@core/network/utils/getNetworkId'
 import { getNetworkHrp } from '@core/profile/actions'
-import { getActiveProfilePersistedAccountData } from '@core/profile/stores'
 import { isValidBech32AddressAndPrefix, validateEthereumAddress } from '@core/utils/crypto'
+import { isAccountAddress } from '@core/wallet/utils'
 
 import { ContactManager } from '../classes'
 
@@ -22,21 +17,16 @@ export function validateContactAddress(address: string, networkId: string): void
             if (!isValidBech32AddressAndPrefix(address, getNetworkHrp())) {
                 throw new Error(localize('error.address.invalidBech32Format'))
             }
-            if (
-                address === getActiveProfilePersistedAccountData(get(selectedAccount)?.index as number)?.depositAddress
-            ) {
-                throw new Error(localize('error.address.belongsToAccount'))
-            }
             break
         case DestinationNetwork.ShimmerEvm:
         case DestinationNetwork.ShimmerEvmTestnet: {
             validateEthereumAddress(address)
-            const coinType = DEFAULT_CHAIN_CONFIGURATIONS[getActiveNetworkId()]?.coinType
-            if (address === get(selectedAccount)?.evmAddresses[coinType]) {
-                throw new Error(localize('error.address.belongsToAccount'))
-            }
             break
         }
+    }
+
+    if (isAccountAddress(address, networkId as DestinationNetwork)) {
+        throw new Error(localize('error.address.belongsToAccount'))
     }
 
     if (
