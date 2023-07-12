a3c650b47e36ce63031c0e0e8ebdff01c5c8af32
Nicole O'Brien <nicole.obrien@iota.org>
2023-07-12 16:08:34 +0100

    fix: remove unneeded validation check

--- packages/shared/src/lib/core/contact/utils/validateContactNote.ts
+++ packages/shared/src/lib/core/contact/utils/validateContactNote.ts
@@ -1,10 +1,6 @@
 import { CONTACT_NOTE_MAX_LENGTH } from '../constants'
 
 export function validateContactNote(note: string): void {
-    if (!note) {
-        throw new Error('Invalid note input')
-    }
-
     if (note.length > CONTACT_NOTE_MAX_LENGTH) {
         throw new Error('Note too long')
     }
