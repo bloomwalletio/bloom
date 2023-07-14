17b54a3a518211397ef4e1ec01a919b846258964
Tuditi <daviddetroch@pm.me>
2023-07-14 12:30:58 +0200

    fix: L2 transaction metadata decoding

--- packages/shared/src/lib/core/layer-2/utils/parseLayer2MetadataForTransfer.ts
+++ packages/shared/src/lib/core/layer-2/utils/parseLayer2MetadataForTransfer.ts
@@ -1,17 +1,19 @@
 import { Converter } from '@core/utils'
-import { ReadStream } from '@iota/util.js'
-import { NativeTokenAmount, TOKEN_ID_BYTE_LENGTH } from '@core/token'
+import { TOKEN_ID_BYTE_LENGTH } from '@core/token'
 import { NFT_ID_BYTE_LENGTH } from '@core/nfts/constants'
 import { ILayer2AssetAllowance, ILayer2TransferAllowanceMetadata } from '../interfaces'
 import { CONTRACT_FUNCTIONS, TARGET_CONTRACTS } from '../constants'
+import { Allowance } from '@core/layer-2/enums'
+import { ReadSpecialStream } from '@core/layer-2/classes'
 
+// Function to parse data from the L1 metadata, using the new encoding where the shimmer chainId is 1072
 export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2TransferAllowanceMetadata {
-    const readStream = new ReadStream(metadata)
-
+    const readStream = new ReadSpecialStream(metadata)
     const senderContract = readStream.readUInt32('senderContract')
     const targetContract = readStream.readUInt32('targetContract')
     const contractFunction = readStream.readUInt32('contractFunction')
-    const gasBudget = readStream.readUInt64('gasBudget')
+    // TODO: This is a temporary fix since now the gas is always 500000, when it varies, the length of the gas will change
+    const gasBudget = readStream.readUIntNSpecialEncoding('gasBudget', 3)
 
     const smartContractParameters = parseSmartContractParameters(readStream)
     const ethereumAddress = '0x' + smartContractParameters['a'].substring(4)
@@ -30,16 +32,16 @@ export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2Tra
     }
 }
 
-function parseSmartContractParameters(readStream: ReadStream): Record<string, string> {
-    const smartContractParametersAmount = readStream.readUInt32('parametersLength')
+function parseSmartContractParameters(readStream: ReadSpecialStream): Record<string, string> {
+    const smartContractParametersAmount = readStream.readUInt32SpecialEncoding('parametersLength')
     const smartContractParameters: Record<string, string> = {}
 
     for (let index = 0; index < smartContractParametersAmount; index++) {
-        const keyLength = readStream.readUInt16('keyLength')
-        const keyBytes = readStream.readBytes('keyValue', keyLength)
+        const keyLength = readStream.readUInt32SpecialEncoding('keyLength')
+        const keyBytes = readStream.readBytes('keyValue', Number(keyLength))
 
-        const valueLength = readStream.readUInt32('valueLength')
-        const valueBytes = readStream.readBytes('valueBytes', valueLength)
+        const valueLength = readStream.readUInt32SpecialEncoding('valueLength')
+        const valueBytes = readStream.readBytes('valueBytes', Number(valueLength))
 
         const key = Converter.bytesToUtf8(keyBytes)
         const value = Converter.bytesToHex(valueBytes)
@@ -50,34 +52,41 @@ function parseSmartContractParameters(readStream: ReadStream): Record<string, st
     return smartContractParameters
 }
 
-function parseAssetAllowance(readStream: ReadStream): ILayer2AssetAllowance {
-    const allowance = readStream.readUInt8('allowance')
-
-    if (allowance) {
-        const baseTokens = readStream.readUInt64('baseTokens').toString()
-        readStream.readUInt16('tokenBufferBytesLength')
-        const tokenAmount = readStream.readUInt16('tokenAmount')
-        const nativeTokens: NativeTokenAmount[] = []
-
-        for (let token = 0; token < tokenAmount; token++) {
-            const tokenId = Converter.bytesToHex(readStream.readBytes('tokenId', TOKEN_ID_BYTE_LENGTH))
-            const amount = readStream.readUInt256('tokenAmount').toString()
-            nativeTokens.push({ ID: [tokenId], amount })
-        }
-
-        const nftAmount = readStream.readUInt16('nftAmount')
-        const nfts: string[] = []
-        for (let nft = 0; nft < nftAmount; nft++) {
-            const nftId = Converter.bytesToHex(readStream.readBytes('nftId', NFT_ID_BYTE_LENGTH))
-            nfts.push(nftId)
-        }
-
-        return {
-            baseTokens,
-            nativeTokens,
-            nfts,
-        }
-    } else {
-        throw new Error('Smart contract call data does not set the asset allowance!')
+function parseAssetAllowance(readStream: ReadSpecialStream): ILayer2AssetAllowance {
+    const allowance = readStream.readUInt8('encodedAllowance')
+    const result: ILayer2AssetAllowance = {
+        baseTokens: '0',
+        nativeTokens: [],
+        nfts: [],
     }
+
+    switch (allowance) {
+        case Allowance.HasBaseTokens: {
+            // TODO: This is a temporary fix since now the base token is sent alone in the transfer (without native token and/or nfts)
+            const baseTokenLength = readStream.length() - readStream.getReadIndex()
+            result.baseTokens = readStream.readUIntNSpecialEncoding('baseTokenAmount', baseTokenLength).toString()
+            break
+        }
+
+        case Allowance.HasNativeTokens: {
+            readStream.readUInt32SpecialEncoding('amountOfDifferentTokens')
+            const tokenId = readStream.readBytes('tokenId', TOKEN_ID_BYTE_LENGTH)
+            const tokenAmount = readStream.readUInt32SpecialEncoding('nativeTokenAmountLength')
+            const nativeTokenAmount = readStream.readBytes('nativeTokenAmount', Number(tokenAmount))
+            result.nativeTokens.push({ ID: [Converter.bytesToHex(tokenId)], amount: nativeTokenAmount.toString() })
+            break
+        }
+
+        case Allowance.hasNFTs: {
+            readStream.readUInt16SpecialEncoding('nftAmount')
+            const nftIdBytes = readStream.readBytes('nftId', NFT_ID_BYTE_LENGTH)
+            const nftId = Converter.bytesToHex(nftIdBytes)
+            result.nfts.push(nftId)
+            break
+        }
+
+        default:
+            throw new Error('Smart contract call data does not set the asset allowance!')
+    }
+    return result
 }
