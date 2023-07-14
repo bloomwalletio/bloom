60fd71c7384c81d98a4083b85237c7d2f80e9c7f
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-07-14 06:15:28 -0500

    fix: add input validation for editing a contact (#256)
    * Change method of validating inputs
    
    * Add validation for other text inputs
    
    * Add validation for network selection
    
    * Delete temp file
    
    * Fix type checking
    
    * Disable feature flag
    
    * fix: minor fixes
    
    * fix: remove unneeded validation check
    
    * Add locales for error strings
    
    * Cleanup contact address validation function
    
    * Fix readonly behavior of component
    
    * Use locales in existing manager class
    
    * Disable feature flag
    
    * Remove unnecessary locales
    
    * Add validation to edit contact form
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>

--- packages/desktop/views/dashboard/contact-book/drawers/EditContactDrawer.svelte
+++ packages/desktop/views/dashboard/contact-book/drawers/EditContactDrawer.svelte
@@ -4,47 +4,49 @@
     import { Button, TextInput } from '@ui'
     import { DrawerTemplate } from '@components'
 
-    import { ContactManager, selectedContact } from '@core/contact'
+    import { ContactManager, selectedContact, validateContactName, validateContactNote } from '@core/contact'
     import { localize } from '@core/i18n'
     import { Router } from '@core/router'
-
     import { showAppNotification } from '@auxiliary/notification'
 
     export let drawerRouter: Router<unknown>
 
+    let nameInput, noteInput: TextInput
     let contactName = $selectedContact.name
     let contactNote = $selectedContact.note
-    let contactErrors = {
-        name: '',
-        note: '',
-    }
-
-    $: contactName, contactNote, resetErrors()
-
-    function resetErrors(): void {
-        contactErrors = {
-            name: '',
-            note: '',
-        }
-    }
 
     function updateContact(): void {
-        resetErrors()
         try {
-            const contact = { ...$selectedContact, name: contactName, note: contactNote }
-            ContactManager.validateContact(contact)
-            ContactManager.updateContact($selectedContact.id, contact)
-            showAppNotification({
-                type: 'success',
-                message: localize('notifications.updateContact.success'),
-                alert: true,
-            })
+            if (validate()) {
+                const contact = { ...$selectedContact, name: contactName, note: contactNote }
+                ContactManager.updateContact($selectedContact.id, contact)
+                showAppNotification({
+                    type: 'success',
+                    message: localize('notifications.updateContact.success'),
+                    alert: true,
+                })
 
-            drawerRouter.previous()
+                drawerRouter.previous()
+            }
         } catch (error) {
-            contactErrors.name = error.name
-            contactErrors.note = error.note
+            showAppNotification({
+                type: 'error',
+                alert: true,
+                message: error?.message ?? error,
+            })
         }
+    }
+
+    function validate(): boolean {
+        let handledError = false
+        for (const input of [nameInput, noteInput]) {
+            try {
+                input.validate()
+            } catch (err) {
+                handledError = true
+            }
+        }
+        return !handledError
     }
 </script>
 
@@ -54,16 +56,18 @@
 >
     <update-contact class="flex flex-col justify-between gap-4">
         <TextInput
+            bind:this={nameInput}
             bind:value={contactName}
             placeholder={localize('general.name')}
             label={localize('general.name')}
-            error={contactErrors.name}
+            validationFunction={validateContactName}
         />
         <TextInput
+            bind:this={noteInput}
             bind:value={contactNote}
             placeholder={localize('general.note')}
             label={localize('general.note')}
-            error={contactErrors.note}
+            validationFunction={validateContactNote}
         />
     </update-contact>
     <div slot="footer">
