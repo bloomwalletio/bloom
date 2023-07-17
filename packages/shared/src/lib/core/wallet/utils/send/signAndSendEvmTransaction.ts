998bf604d133ede3aafd096bf5fdbc517af6ce5e
Mark Nardi <nardimark7@gmail.com>
2023-07-17 08:47:28 +0200

    add ledger popup check for evm transactions

--- packages/shared/src/lib/core/wallet/utils/send/signAndSendEvmTransaction.ts
+++ packages/shared/src/lib/core/wallet/utils/send/signAndSendEvmTransaction.ts
@@ -2,6 +2,8 @@ import { buildBip32Path, updateSelectedAccount } from '@core/account'
 import { EvmTransactionData, signTransactionWithLedger } from '@core/layer-2'
 import { handleError } from '@core/error/handlers'
 import Web3 from 'web3'
+import { LedgerAppName } from '@core/ledger'
+import { checkActiveProfileAuth } from '@core/profile/actions'
 
 export async function signAndSendEvmTransaction(
     transaction: EvmTransactionData,
@@ -9,21 +11,27 @@ export async function signAndSendEvmTransaction(
     accountIndex: number,
     callback: () => void
 ): Promise<void> {
-    try {
-        updateSelectedAccount({ isTransferring: true })
+    await checkActiveProfileAuth(
+        async () => {
+            try {
+                updateSelectedAccount({ isTransferring: true })
 
-        const bip32 = buildBip32Path(60, accountIndex)
-        const signedTransaction = await signTransactionWithLedger(transaction, bip32)
+                const bip32 = buildBip32Path(60, accountIndex)
+                const signedTransaction = await signTransactionWithLedger(transaction, bip32)
 
-        if (signedTransaction) {
-            await provider?.eth.sendSignedTransaction(signedTransaction)
-            callback()
-        } else {
-            throw Error('No Signature provided')
-        }
-    } catch (err) {
-        handleError(err)
-    } finally {
-        updateSelectedAccount({ isTransferring: false })
-    }
+                if (signedTransaction) {
+                    await provider?.eth.sendSignedTransaction(signedTransaction)
+                    callback()
+                } else {
+                    throw Error('No Signature provided')
+                }
+            } catch (err) {
+                handleError(err)
+            } finally {
+                updateSelectedAccount({ isTransferring: false })
+            }
+        },
+        { stronghold: true, ledger: true },
+        LedgerAppName.Ethereum
+    )
 }
