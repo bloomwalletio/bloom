9a209deac615159cecd61d1c63f64c413068037a
Mark Nardi <nardimark7@gmail.com>
2023-07-18 16:01:50 +0200

    chore: refactor `TransactionSummary` (#252)
    * extract stardust summary
    
    * extract evm summary
    
    * split building, signing and sending into their own functions
    
    * fix inputs on loading
    
    * add ledger popup check for evm transactions
    
    * cleanup
    
    * extract asset section
    
    * fix: pr review changes
    
    Co-authored-by: MarkNerdi996 <MarkNerdi996@users.noreply.github.com>
    
    * extract send into own files
    
    * update types
    
    * remove redundant lines
    
    * fix: pr review comments for evm summary components
    
    Co-authored-by: MarkNerdi996 <MarkNerdi996@users.noreply.github.com>
    
    * fix: pr review comments
    
    * refactor: rename functions
    
    * fix: pr review comments
    
    * refactor: move functions to actions folder
    
    * fix: missing exports
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>
    Co-authored-by: MarkNerdi996 <MarkNerdi996@users.noreply.github.com>

---
+++ packages/desktop/views/dashboard/send-flow/views/components/EvmTransactionDetails.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { localize } from '@core/i18n'
+    import { NetworkIcon, Text, TooltipIcon } from '@ui'
+    import { NetworkId } from '@core/network'
+    import { getBaseToken } from '@core/profile'
+    import { formatTokenAmountBestMatch } from '@core/wallet'
+
+    export let destinationNetwork: string
+    export let gasBudget: number
+</script>
+
+<div class="border border-solid border-gray-200 dark:border-gray-700 rounded-lg">
+    {#if destinationNetwork}
+        <section class="key-value-box border-gray-200 dark:border-gray-700">
+            <Text>{localize('general.destinationNetwork')}</Text>
+            <div class="flex flex-row gap-2">
+                <!-- TODO: Add correct icon for L2 -->
+                <NetworkIcon networkId={NetworkId.Testnet} height={16} width={16} outlined={false} />
+                <Text color="gray-600">{destinationNetwork}</Text>
+            </div>
+        </section>
+    {/if}
+    {#if gasBudget}
+        <section class="key-value-box border-gray-200 dark:border-gray-700">
+            <div class="flex flex-row">
+                <Text>{localize('general.gasBudget')}</Text>
+                <TooltipIcon
+                    title={localize('general.gasBudget')}
+                    text={localize('tooltips.transactionDetails.outgoing.gasBudget')}
+                    width={15}
+                    height={15}
+                    classes="ml-1"
+                />
+            </div>
+            <Text color="gray-600">{formatTokenAmountBestMatch(gasBudget, getBaseToken())}</Text>
+        </section>
+    {/if}
+</div>
+
+<style lang="scss">
+    .key-value-box {
+        @apply flex flex-row justify-between p-4;
+        @apply border-b border-solid;
+
+        &:last-child {
+            @apply border-0;
+        }
+    }
+</style>
---
+++ packages/shared/src/lib/core/wallet/actions/send/sendOutputFromStardust.ts
@@ -0,0 +1,27 @@
+import { IAccountState } from '@core/account/interfaces'
+import { ledgerPreparedOutput } from '@core/ledger'
+import { checkActiveProfileAuth, getIsActiveLedgerProfile } from '@core/profile'
+import { Output } from '@core/wallet/types'
+import { validateSendConfirmation } from '@core/wallet/utils'
+import { signAndSendStardustTransaction } from './signAndSendStardustTransaction'
+
+export async function sendOutputFromStardust(
+    output: Output,
+    account: IAccountState,
+    callback: () => void
+): Promise<void> {
+    validateSendConfirmation(output)
+
+    if (getIsActiveLedgerProfile()) {
+        ledgerPreparedOutput.set(output)
+    }
+
+    await checkActiveProfileAuth(
+        async () => {
+            await signAndSendStardustTransaction(output, account)
+            callback()
+        },
+        { stronghold: true, ledger: false }
+    )
+    return
+}
