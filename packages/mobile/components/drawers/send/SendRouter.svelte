98fae85cfd50e703ab4350d562ff5467a0dbb70d
Mark Nardi <nardimark7@gmail.com>
2023-07-20 08:51:53 +0200

    revert mobile change

--- packages/mobile/components/drawers/send/SendRouter.svelte
+++ packages/mobile/components/drawers/send/SendRouter.svelte
@@ -74,9 +74,9 @@
     }
 
     function setStorageDeposit(preparedOutput: Output, surplus?: number): void {
-        const _storageDeposit = getStorageDepositFromOutput(preparedOutput)
-        const _giftedStorageDeposit = 0
-
+        const rawAmount = transactionData.type === NewTransactionType.TokenTransfer ? transactionData.rawAmount : '0'
+        const { storageDeposit: _storageDeposit, giftedStorageDeposit: _giftedStorageDeposit } =
+            getStorageDepositFromOutput(preparedOutput, rawAmount)
         if (surplus > _storageDeposit) {
             visibleSurplus = Number(surplus)
         }
