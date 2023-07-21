cf1b3c216966de47addbdaac9c27ea5ec7711675
Tuditi <daviddetroch@pm.me>
2023-07-21 18:18:00 +0200

    chore: use rs filter

--- packages/shared/src/lib/core/wallet/utils/outputs/getSerialNumberFromAliasOutput.ts
+++ packages/shared/src/lib/core/wallet/utils/outputs/getSerialNumberFromAliasOutput.ts
@@ -1,5 +1,4 @@
 import { getSelectedAccount } from '@core/account'
-import { OUTPUT_TYPE_ALIAS } from '@core/wallet/constants'
 import { convertBech32ToHexAddress } from '@core/wallet/utils/convertBech32ToHexAddress'
 import { IAliasOutput } from '@iota/types'
 
@@ -10,12 +9,7 @@ export async function getSerialNumberFromAliasOutput(aliasAddress: string): Prom
     }
     const aliasId = convertBech32ToHexAddress(aliasAddress)
 
-    // TODO: add filter options if updated to @iota/wallet.rc-41
-    const unspentOutputs = await account.unspentOutputs()
-
-    const aliasOutput = unspentOutputs.find(({ output }) => {
-        return output.type === OUTPUT_TYPE_ALIAS && output.aliasId === aliasId
-    })
+    const [aliasOutput] = await account.unspentOutputs({ aliasIds: [aliasId] })
 
     // If it's the first state transition of the alias address, the aliasId is 0x0.
     // So we set the foundry counter to 0.
