199940bc6e98078e112f8fcc2db76875b3c46407
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-07-25 16:14:33 -0500

    Open add contact drawer if no contacts

--- packages/desktop/views/dashboard/contact-book/contact-book-router.ts
+++ packages/desktop/views/dashboard/contact-book/contact-book-router.ts
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store'
 
+import { getActiveProfile } from '@core/profile'
 import { Router } from '@core/router'
+
 import { ContactBookRoute } from './contact-book-route.enum'
 
 export const contactBookRoute = writable<ContactBookRoute | null>(null)
@@ -8,6 +10,8 @@ export const contactBookRouter = writable<ContactBookRouter | null>(null)
 
 export class ContactBookRouter extends Router<ContactBookRoute> {
     constructor() {
-        super(ContactBookRoute.ContactList, contactBookRoute)
+        const hasContacts = Object.keys(getActiveProfile()?.contacts).length > 0
+        const initialRoute = hasContacts ? ContactBookRoute.ContactList : ContactBookRoute.AddContact
+        super(initialRoute, contactBookRoute)
     }
 }
