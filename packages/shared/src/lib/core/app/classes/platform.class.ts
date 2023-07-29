22947f8edcef53b077da765cd3bb39bcbd1186e9
Mark Nardi <nardimark7@gmail.com>
2023-07-29 16:04:29 +0200

    fix circular import

--- packages/shared/src/lib/core/app/classes/platform.class.ts
+++ packages/shared/src/lib/core/app/classes/platform.class.ts
@@ -1,5 +1,5 @@
 import { IPlatform } from '../interfaces'
-import { IS_MOBILE } from '../constants'
+import { IS_MOBILE } from '../constants/is-mobile.constant'
 
 // TODO: https://github.com/iotaledger/firefly/issues/5143
 export const Platform: IPlatform = window[IS_MOBILE ? '__CAPACITOR__' : '__ELECTRON__']
