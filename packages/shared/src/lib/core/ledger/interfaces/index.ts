5536b0b93b600e741ea2f1213911908c853988ee
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-08-01 07:28:17 -0500

    refactor: async Ledger API calls (#342)
    * Add pre-signing logic
    
    * Add post-signing logic
    
    * Delete unused files
    
    * Fix rejecting tx signing
    
    * Refactor function out of logic
    
    * Refactor calling async Ledger functions
    
    * Organize new code
    
    * Format and lint files
    
    * Fix type check
    
    * Use static class rather than store
    
    * Fix constant name
    
    * Empty
    
    * fixes
    
    ---------
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/shared/src/lib/core/ledger/interfaces/index.ts
+++ packages/shared/src/lib/core/ledger/interfaces/index.ts
@@ -1,3 +1,4 @@
-export * from './ledger.interface'
+export * from './ledger-api-bridge.interface'
+export * from './ledger-api-request-options.interface'
 export * from './ledger-connection-state.interface'
 export * from './ledger-nano-status-polling-configuration.interface'
