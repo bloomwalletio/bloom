d2f18c5d6d2b290145e63758fbc96fedc5fda28a
Mark Nardi <nardimark7@gmail.com>
2023-08-02 16:32:24 +0200

    improve import

--- packages/shared/src/lib/core/activities/types/persisted-evm-transaction.interface.ts
+++ packages/shared/src/lib/core/activities/types/persisted-evm-transaction.interface.ts
@@ -1,4 +1,4 @@
-import { EvmTransactionData } from '@core/layer-2'
+import { EvmTransactionData } from '@core/layer-2/types'
 import { TransactionReceipt } from 'web3-core'
 
 export type PersistedEvmTransaction = TransactionReceipt & EvmTransactionData
