9c3b9838496057b4ea9f06916b2123e2da66631f
Mark Nardi <nardimark7@gmail.com>
2023-08-09 15:57:16 +0200

    fix import order

--- packages/desktop/components/buttons/index.js
+++ packages/desktop/components/buttons/index.js
@@ -1,5 +1,5 @@
 export * from './menu-buttons'
 export * from './popup-buttons'
 
-export { default as NetworkDrawerButton } from './NetworkDrawerButton.svelte'
 export { default as ConnectedDAppsButton } from './ConnectedDAppsButton.svelte'
+export { default as NetworkDrawerButton } from './NetworkDrawerButton.svelte'
