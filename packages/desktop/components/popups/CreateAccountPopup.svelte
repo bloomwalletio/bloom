6409f5a41f03f7806299559109f165f6698fa139
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-08-12 11:16:15 -0500

    hotfix: `validateAccountName` imports (#429)
    * Fix broken imports
    
    * refactor: organise imports
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>

--- packages/desktop/components/popups/CreateAccountPopup.svelte
+++ packages/desktop/components/popups/CreateAccountPopup.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import { getRandomAccountColor, validateAccountName } from '@core/account'
-    import { tryCreateAdditionalAccount } from '@core/account/actions'
+    import { getRandomAccountColor } from '@core/account'
+    import { tryCreateAdditionalAccount, validateAccountName } from '@core/account/actions'
     import { handleError } from '@core/error/handlers/handleError'
     import { localize } from '@core/i18n'
     import { checkActiveProfileAuth } from '@core/profile/actions'
