ffaac432cc1420b90a13107dba41ee6ddbc1bdd0
Mark Nardi <nardimark7@gmail.com>
2023-08-13 23:32:04 +0200

    fix naming

--- packages/desktop/components/buttons/DappsButton.svelte
+++ packages/desktop/components/buttons/DappsButton.svelte
@@ -4,7 +4,7 @@
     import { DrawerId, closeDrawer, drawerState, openDrawer } from '@desktop/auxiliary/drawer'
     import { DappsDrawerRouter, dappsDrawerRouter, DappsDrawerRoute } from '@views/dashboard/dapps'
 
-    function onConnectedDappsClick(): void {
+    function onDappsClick(): void {
         if ($drawerState.active) {
             closeDrawer()
         } else {
@@ -17,7 +17,7 @@
 <connected-dapps-button class="block relative">
     <button
         class="connected-dapps-button hover:bg-gray-300 dark:hover:bg-gray-900 border-gray-400 dark:border-gray-700"
-        on:click={onConnectedDappsClick}
+        on:click={onDappsClick}
     >
         <Icon height={20} width={20} icon={IconEnum.Alias} classes="dark:text-white" />
     </button>
