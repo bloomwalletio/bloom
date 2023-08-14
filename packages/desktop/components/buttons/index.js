b34178dc02431c5db32fd2a5377206ffa1adf124
Mark Nardi <nardimark7@gmail.com>
2023-08-14 18:48:03 +0200

    enhancement: setup walletconnect drawer (#394)
    * setup wallet connect
    
    * rename
    
    * add translation
    
    * cleanup components
    
    * use correct icon
    
    * add popup to connect dapp
    
    * fix import order
    
    * update locales
    
    * fix locale
    
    * rename dApps to dapps
    
    * rename button
    
    * fix naming
    
    * Make changes per PR review
    
    ---------
    
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>

--- packages/desktop/components/buttons/index.js
+++ packages/desktop/components/buttons/index.js
@@ -1,4 +1,5 @@
 export * from './menu-buttons'
 export * from './popup-buttons'
 
+export { default as DappsButton } from './DappsButton.svelte'
 export { default as NetworkDrawerButton } from './NetworkDrawerButton.svelte'
---
+++ packages/desktop/views/dashboard/dapps/dapps-route.enum.ts
@@ -0,0 +1,3 @@
+export enum DappsDrawerRoute {
+    ConnectedDapps = 'connectedDapps',
+}
