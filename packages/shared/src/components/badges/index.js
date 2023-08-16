77fb4a7133aa104e3da57e2b7bcee7316e9632f6
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-16 23:48:30 +0100

    refactor replace NotificationBadge

--- packages/shared/src/components/badges/index.js
+++ packages/shared/src/components/badges/index.js
@@ -1,4 +1,3 @@
 export { default as NetworkIconBadge } from './NetworkIconBadge.svelte'
-export { default as NotificationBadge } from './NotificationBadge.svelte'
 export { default as StrongholdBadge } from './StrongholdBadge.svelte'
 export { default as VerificationBadge } from './VerificationBadge.svelte'
