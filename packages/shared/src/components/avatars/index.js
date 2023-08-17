f555fa4ec7e2efd00d58851d9e7ee08e0c66d761
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-08-17 04:40:32 -0500

    enhancement: use contact initials in contact icon (#450)
    * Add contact initials to contact icon
    
    * Add tiny size to profile picture:
    
    * Rename component to "Avatar"
    
    * Refactor avatar components
    
    * refactor: make default avatar size medium
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>

---
+++ packages/shared/src/components/avatars/index.js
@@ -0,0 +1,4 @@
+export { default as Avatar } from './Avatar.svelte'
+export { default as ContactAvatar } from './ContactAvatar.svelte'
+export { default as NftAvatar } from './NftAvatar.svelte'
+export { default as ProfileAvatar } from './ProfileAvatar.svelte'
