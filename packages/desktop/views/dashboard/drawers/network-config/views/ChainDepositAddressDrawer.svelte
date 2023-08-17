b3222fa6df92207a6ce68132a212be73a841082d
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-17 07:54:15 +0100

    enhancment: copy network address from card (#442)
    * fix: missing imports on network card
    
    * enhancement: use CopyableButton from UI kit
    
    * enhancement: use copyable component on network deposit address drawer
    
    * chore: update bloomwalletio to v0.6.22
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/desktop/views/dashboard/drawers/network-config/views/ChainDepositAddressDrawer.svelte
+++ packages/desktop/views/dashboard/drawers/network-config/views/ChainDepositAddressDrawer.svelte
@@ -6,7 +6,7 @@
     import { IIscpChainConfiguration, selectedChain } from '@core/network'
     import { Router } from '@core/router'
     import { NetworkConfigRoute } from '../'
-    import { AddressBox, FontWeight, QR, Text } from '@ui'
+    import { AddressBox, Copyable, FontWeight, QR, Text } from '@ui'
 
     export let drawerRouter: Router<NetworkConfigRoute>
 
@@ -22,10 +22,6 @@
             depositAddress = $selectedAccount.depositAddress
         }
     }
-
-    function onReceiveClick(): void {
-        addressBoxElement.copyAddress()
-    }
 </script>
 
 <DrawerTemplate
@@ -36,34 +32,34 @@
 >
     {#key depositAddress}
         <div class="w-full h-full flex items-center justify-center">
-            <button
-                class="flex flex-col px-4 py-4 space-y-2 rounded-xl cursor-pointer"
-                class:darkmode={$appSettings.darkMode}
-                on:click={onReceiveClick}
-            >
-                <inner-box class="flex flex-col space-y-6 pt-7 pb-6">
-                    <QR data={depositAddress} />
-                    <div class="flex flex-col space-y-1">
-                        <Text fontWeight={FontWeight.medium} color="gray-600" darkColor="white"
-                            >{localize('general.myAddress')}</Text
-                        >
-                        <AddressBox
-                            bind:this={addressBoxElement}
-                            clearBackground
-                            clearPadding
-                            address={depositAddress}
-                            fontSize="sm"
-                            isCopyable
-                        />
-                    </div>
-                </inner-box>
-            </button>
+            <Copyable value={depositAddress}>
+                <address-container
+                    class="flex flex-col px-4 py-4 space-y-2 rounded-xl cursor-pointer"
+                    class:darkmode={$appSettings.darkMode}
+                >
+                    <inner-box class="flex flex-col space-y-6 pt-7 pb-6">
+                        <QR data={depositAddress} />
+                        <div class="flex flex-col space-y-1">
+                            <Text fontWeight={FontWeight.medium} color="gray-600" darkColor="white"
+                                >{localize('general.myAddress')}</Text
+                            >
+                            <AddressBox
+                                bind:this={addressBoxElement}
+                                clearBackground
+                                clearPadding
+                                address={depositAddress}
+                                fontSize="sm"
+                            />
+                        </div>
+                    </inner-box>
+                </address-container>
+            </Copyable>
         </div>
     {/key}
 </DrawerTemplate>
 
 <style lang="scss">
-    button {
+    address-container {
         &:hover {
             @apply bg-blue-50;
             @apply border-gray-500;
