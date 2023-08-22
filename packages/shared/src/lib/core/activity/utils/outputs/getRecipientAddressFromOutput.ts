d43853912f43ab2129f18c764b3a020e5d1a900e
Tuditi <daviddetroch@pm.me>
2023-08-22 15:53:04 +0200

    chore: type updates

--- packages/shared/src/lib/core/activity/utils/outputs/getRecipientAddressFromOutput.ts
+++ packages/shared/src/lib/core/activity/utils/outputs/getRecipientAddressFromOutput.ts
@@ -1,8 +1,7 @@
-import { UnlockConditionType, AddressUnlockCondition } from '@iota/sdk/out/types'
-import { Output } from '@core/wallet/types'
+import { CommonOutput, UnlockConditionType, AddressUnlockCondition } from '@iota/sdk/out/types'
 import { getBech32AddressFromAddressTypes } from '@core/wallet/utils'
 
-export function getRecipientAddressFromOutput(output: Output): string | undefined {
+export function getRecipientAddressFromOutput(output: CommonOutput): string | undefined {
     for (const unlockCondition of output.unlockConditions) {
         if (unlockCondition.type === UnlockConditionType.Address) {
             const addressUnlockCondition = unlockCondition as AddressUnlockCondition
