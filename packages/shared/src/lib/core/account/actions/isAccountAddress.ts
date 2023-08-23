bc340a7e568eebba0ee358707f2bb4888a88ebc9
Mark Nardi <nardimark7@gmail.com>
2023-08-23 15:13:25 +0200

    cleanup validation logic

--- packages/shared/src/lib/core/account/actions/isAccountAddress.ts
+++ packages/shared/src/lib/core/account/actions/isAccountAddress.ts
@@ -1,6 +1,5 @@
 import { get } from 'svelte/store'
 import { activeAccounts } from '@core/profile/stores'
-import { network } from '@core/network/stores'
 import { NetworkId, isStardustNetwork } from '@core/network'
 
 export function isAccountAddress(address: string, destinationNetworkId: NetworkId): boolean {
@@ -12,21 +11,11 @@ export function isAccountAddress(address: string, destinationNetworkId: NetworkI
 }
 
 function isAccountBech32Address(address: string): boolean {
-    for (const account of get(activeAccounts)) {
-        if (account && account.depositAddress === address) {
-            return true
-        }
-    }
-    return false
+    return get(activeAccounts)?.some((account) => account && account.depositAddress === address)
 }
 
 function isAccountEvmAddress(address: string): boolean {
-    for (const account of get(activeAccounts) ?? []) {
-        for (const chain of get(network)?.getChains() ?? []) {
-            if (account && account.evmAddresses[chain.getConfiguration().coinType] === address) {
-                return true
-            }
-        }
-    }
-    return false
+    return get(activeAccounts)?.some((account) =>
+        Object.values(account.evmAddresses ?? {}).some((evmAddress) => evmAddress === address)
+    )
 }
