e957d2629134b3347ef1b273b9eb776fe6737fb9
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-24 14:05:35 +0100

    table on alias confirmation popup

--- packages/desktop/components/popups/AliasConfirmationPopup.svelte
+++ packages/desktop/components/popups/AliasConfirmationPopup.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+    import { Table } from '@bloomwalletio/ui'
     import { selectedAccount, updateSelectedAccount } from '@core/account/stores'
     import { processAndAddToActivities } from '@core/activity/utils'
     import { handleError } from '@core/error/handlers/handleError'
     import { localize } from '@core/i18n'
+    import { network } from '@core/network/stores'
     import { checkActiveProfileAuth, getBaseToken } from '@core/profile/actions'
+    import { formatTokenAmountPrecise } from '@core/token'
     import {
         EMPTY_HEX_ID,
         UNLOCK_CONDITION_GOVERNOR_ADDRESS,
@@ -11,10 +14,8 @@
         convertBech32ToHexAddress,
     } from '@core/wallet'
     import { closePopup } from '@desktop/auxiliary/popup'
-    import { Button, FontWeight, KeyValueBox, Text, TextType } from '@ui'
+    import { Button, FontWeight, Text, TextType } from '@ui'
     import { onMount } from 'svelte'
-    import { network } from '@core/network/stores'
-    import { formatTokenAmountPrecise } from '@core/token'
 
     export let _onMount: (..._: any[]) => Promise<void> = async () => {}
 
@@ -92,20 +93,26 @@
     <Text type={TextType.h3} fontWeight={FontWeight.semibold} classes="text-left">{localize('popups.alias.title')}</Text
     >
     <div class="w-full flex-col space-y-2">
-        <KeyValueBox
-            keyText={localize('general.storageDeposit')}
-            valueText={storageDeposit}
-            tooltipText={localize('tooltips.transactionDetails.incoming.storageDeposit')}
-        />
-        <KeyValueBox
-            keyText={localize('general.governorAddress')}
-            valueText={$selectedAccount.depositAddress}
-            isCopyable
-        />
-        <KeyValueBox
-            keyText={localize('general.stateControllerAddress')}
-            valueText={$selectedAccount.depositAddress}
-            isCopyable
+        <Table
+            items={[
+                {
+                    key: localize('general.storageDeposit'),
+                    value: storageDeposit,
+                    popover: { content: localize('tooltips.transactionDetails.incoming.storageDeposit') },
+                },
+                {
+                    key: localize('general.governorAddress'),
+                    value: $selectedAccount.depositAddress,
+                    truncate: { firstCharCount: 10, endCharCount: 10 },
+                    copyable: true,
+                },
+                {
+                    key: localize('popups.walletFinder.totalWalletBalance'),
+                    value: $selectedAccount.depositAddress,
+                    truncate: { firstCharCount: 10, endCharCount: 10 },
+                    copyable: true,
+                },
+            ]}
         />
     </div>
     <popup-buttons class="flex flex-row flex-nowrap w-full space-x-4">
