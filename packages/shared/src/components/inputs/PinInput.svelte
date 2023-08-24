e5b75656e713714ef3b6602750a9fc10434d3e25
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-24 09:26:58 +0100

    refactor: remove left over references to mobile (#487)
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/shared/src/components/inputs/PinInput.svelte
+++ packages/shared/src/components/inputs/PinInput.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { Error, Icon, Text } from '@ui'
     import { createEventDispatcher, onMount } from 'svelte'
-    import { IS_MOBILE, IS_ANDROID } from '@core/app/constants'
     import { isValidPin, PIN_LENGTH } from '@core/utils'
 
     const dispatch = createEventDispatcher()
@@ -108,20 +107,6 @@
         event.preventDefault()
     }
 
-    /**
-     * for android mobile we need both onkeydown and oninput
-     * event listeners to the input and handle the old and the new value.
-     * the auto-suggest feature or other event might follow
-     * the keydown event and invalidate it.
-     */
-    function changeHandlerHelper(event: InputEventInit, index: number): void {
-        if (!/^[0-9]$/.test(event.data)) {
-            inputs[index] = ''
-        } else {
-            inputElements[index + 1].focus()
-        }
-    }
-
     onMount(() => {
         if (autofocus) {
             focus()
@@ -148,35 +133,19 @@
         <div class="flex flex-row inputs-wrapper">
             <div class="input-wrapper absolute items-center w-full flex flex-row flex-no-wrap justify-between">
                 {#each inputs as input, i}
-                    {#if IS_MOBILE}
-                        <input
-                            bind:value={input}
-                            maxLength="1"
-                            id={`input-${i}`}
-                            type="tel"
-                            bind:this={inputElements[i]}
-                            class:active={!input || input.length === 0}
-                            class:glimpse
-                            {disabled}
-                            on:input={(event) => (IS_ANDROID ? changeHandlerHelper(event, i) : undefined)}
-                            on:keydown={changeHandler}
-                            on:contextmenu|preventDefault
-                        />
-                    {:else}
-                        <input
-                            bind:value={input}
-                            maxLength="1"
-                            id={`input-${i}`}
-                            type="text"
-                            bind:this={inputElements[i]}
-                            class:active={!input || input.length === 0}
-                            class:glimpse
-                            {disabled}
-                            on:keydown={changeHandler}
-                            on:contextmenu|preventDefault
-                            tabindex="-1"
-                        />
-                    {/if}
+                    <input
+                        bind:value={input}
+                        maxLength="1"
+                        id={`input-${i}`}
+                        type="text"
+                        bind:this={inputElements[i]}
+                        class:active={!input || input.length === 0}
+                        class:glimpse
+                        {disabled}
+                        on:keydown={changeHandler}
+                        on:contextmenu|preventDefault
+                        tabindex="-1"
+                    />
                 {/each}
             </div>
             <div
