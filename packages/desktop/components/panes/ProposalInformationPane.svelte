10a8fb899c656a1b6838d3bede174cae96fe9700
Jean Ribeiro <contact@jeanribeiro.dev>
2023-08-24 23:43:49 -0300

    chore: update ProposalInformationPane

--- packages/desktop/components/panes/ProposalInformationPane.svelte
+++ packages/desktop/components/panes/ProposalInformationPane.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
+    import { type IItems, Table } from '@bloomwalletio/ui'
     import { Pane, Text } from '@ui'
-    import { Table } from '@bloomwalletio/ui'
     import { formatDate, localize } from '@core/i18n'
-    import { DATE_FORMAT, IKeyValueBoxList, milestoneToDate, truncateString } from '@core/utils'
+    import { DATE_FORMAT, milestoneToDate, truncateString } from '@core/utils'
     import { networkStatus } from '@core/network/stores'
     import { ProposalStatus } from '@contexts/governance/enums'
     import { selectedProposal } from '@contexts/governance/stores'
+    import { IProposal } from '@contexts/governance'
+    import { INetworkStatus } from '@core/network/interfaces'
 
     export let classes: string = ''
-
-    let items: { key: string; value: string }[]
 
     interface IProposalDateData {
         propertyKey: 'votingOpens' | 'countingStarts' | 'countingEnds' | 'countingEnded'
@@ -45,39 +45,30 @@
         }
     }
 
-    let proposalInformation: IKeyValueBoxList
-    $: proposalInformation = {
-        ...(proposalDateData?.propertyKey && {
-            [proposalDateData.propertyKey]: {
-                data: formatDate(
-                    milestoneToDate($networkStatus.currentMilestone, proposalDateData.milestone),
+    const items: IItems[] = []
+
+    $: setItems($selectedProposal, $networkStatus)
+
+    function setItems(proposal: IProposal, networkStatus: INetworkStatus): void {
+        if (proposalDateData?.propertyKey) {
+            items.push({
+                key: localize(`views.governance.details.proposalInformation.${proposalDateData.propertyKey}`),
+                value: formatDate(
+                    milestoneToDate(networkStatus.currentMilestone, proposalDateData.milestone),
                     DATE_FORMAT
                 ),
-            },
-        }),
-        eventId: {
-            data: truncateString($selectedProposal?.id, 9, 9),
-            isCopyable: true,
-            copyValue: $selectedProposal?.id,
-        },
-        nodeUrl: { data: $selectedProposal?.nodeUrl, isCopyable: true },
-    }
-
-    $: items, setProposalDetailValues()
-
-    function setProposalDetailValues(): void {
-        items = []
-        for (const key in proposalInformation) {
-            items.push({
-                key: localize(`views.governance.details.proposalInformation.${key}`),
-                value: proposalInformation[key]?.data,
-                copyable: {
-                    popover: {
-                        content: localize('general.copiedToClipboard'),
-                    },
-                },
             })
         }
+        items.push({
+            key: localize('views.governance.details.proposalInformation.eventId'),
+            value: truncateString(proposal?.id, 9, 9),
+            copyable: true,
+        })
+        items.push({
+            key: localize('views.governance.details.proposalInformation.nodeUrl'),
+            value: proposal?.nodeUrl,
+            copyable: true,
+        })
     }
 </script>
 
