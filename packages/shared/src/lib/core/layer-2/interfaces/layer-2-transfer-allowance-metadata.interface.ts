ed2d23c9cc1b5a6a896543676012c9d38a4d74fb
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-08-28 10:51:44 -0500

    Fix type checker

--- packages/shared/src/lib/core/layer-2/interfaces/layer-2-transfer-allowance-metadata.interface.ts
+++ packages/shared/src/lib/core/layer-2/interfaces/layer-2-transfer-allowance-metadata.interface.ts
@@ -1,4 +1,4 @@
-import { ILayer2SmartContractCallData } from '@core/layer-2/interfaces/layer2-smart-contract-call-data.interface'
+import { ILayer2SmartContractCallData } from './layer-2-smart-contract-call-data.interface'
 
 export interface ILayer2TransferAllowanceMetadata extends ILayer2SmartContractCallData {
     ethereumAddress: string
