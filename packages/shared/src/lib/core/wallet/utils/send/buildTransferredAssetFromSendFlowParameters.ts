34284881d53d81ee0ad119eca62a076c79c21ae0
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-08-28 13:36:34 -0500

    Fix type checker

--- packages/shared/src/lib/core/wallet/utils/send/buildTransferredAssetFromSendFlowParameters.ts
+++ packages/shared/src/lib/core/wallet/utils/send/buildTransferredAssetFromSendFlowParameters.ts
@@ -1,12 +1,12 @@
 import { AssetType } from '@core/layer-2/enums'
 import { TransferredAsset } from '@core/layer-2/types'
 
-import { SendFlowParameters, TokenTransferData } from '../../types'
+import { SendFlowParameters, TokenSendFlowParameters, TokenTransferData } from '../../types'
 
 export function buildTransferredAssetFromSendFlowParameters(
     sendFlowParameters: SendFlowParameters
 ): TransferredAsset | undefined {
-    const { baseCoinTransfer, tokenTransfer } = sendFlowParameters
+    const { baseCoinTransfer, tokenTransfer } = sendFlowParameters as TokenSendFlowParameters
     const isBaseCoinTransfer = Boolean(baseCoinTransfer)
     const transfer: TokenTransferData = isBaseCoinTransfer ? baseCoinTransfer : tokenTransfer
     const { rawAmount, token } = transfer
