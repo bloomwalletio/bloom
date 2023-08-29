154bbfb25d8915745e8181cf0f1c611c599902ac
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-29 09:58:36 +0100

    improve comment

--- packages/shared/src/lib/core/layer-2/constants/fallback-estimated-gas.constant.ts
+++ packages/shared/src/lib/core/layer-2/constants/fallback-estimated-gas.constant.ts
@@ -1,13 +1,11 @@
 import { SendFlowType } from '@core/wallet'
 
 /**
- * We need a fallback estimated gas, since we cannot estimate calls to the magic contract.
+ * We need a fallback estimated gas, since we cannot estimate calls to the magic contract if there is no balance on the sender address.
  * For L1 -> L2 transfers we observed the following gas costs:
- *      Base Token Transfer: 22 000 glow
- *      Native Token Transfer: 63 000 glow
- *      NFT Transfer: 75 600 glow
- * Therefor we set the fallback estimated gas to 100 000 glow
- */
+ *      Base Token Transfer: < 24200 glow
+ *      Native Token Transfer: < 24350 glow
+ *      NFT Transfer: < 24700 glow */
 export const FALLBACK_ESTIMATED_GAS: { [key in SendFlowType]: number } = {
     [SendFlowType.BaseCoinTransfer]: 24200,
     [SendFlowType.TokenTransfer]: 24350,
