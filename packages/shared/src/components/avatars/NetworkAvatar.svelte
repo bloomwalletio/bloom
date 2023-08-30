bdd4b5624d7a271cc1f51f69889492aec8db9a4a
Nicole O'Brien <nicole@bloomwallet.io>
2023-08-30 22:21:43 +0100

    enhancement: make evm icon bigger

--- packages/shared/src/components/avatars/NetworkAvatar.svelte
+++ packages/shared/src/components/avatars/NetworkAvatar.svelte
@@ -24,7 +24,8 @@
     $: backgroundColor = isSupported ? AVATAR_BACKGROUND_COLOR[networkId as SupportedNetworkId] : 'gray-100'
     $: textColor = isSupported ? AVATAR_TEXT_COLOR[networkId as SupportedNetworkId] : 'gray-400'
     $: icon = isSupported ? NETWORK_ICON_SVG[networkId as SupportedNetworkId] : undefined
+    $: magnify = networkId === SupportedNetworkId.ShimmerEvm || networkId === SupportedNetworkId.ShimmerEvmTestnet
 </script>
 
 <!-- TODO: Add initials for not supported network IDs -->
-<Avatar {size} {backgroundColor} {textColor} {icon} />
+<Avatar {size} {backgroundColor} {textColor} {icon} {magnify} />
