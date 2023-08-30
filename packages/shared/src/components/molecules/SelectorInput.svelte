18133faa6f3dde081913f9de4bb8329781000ee7
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-08-30 15:09:18 -0500

    Change vertical padding for selector input

--- packages/shared/src/components/molecules/SelectorInput.svelte
+++ packages/shared/src/components/molecules/SelectorInput.svelte
@@ -101,7 +101,7 @@
                 {#each filteredOptions as option, index}
                     <button
                         on:click={() => onClick(option)}
-                        class="w-full flex flex-row flex-1 justify-between items-center px-2 py-3 rounded-lg hover:bg-blue-50 dark:hover:bg-gray-800 dark:hover:bg-opacity-20"
+                        class="w-full flex flex-row flex-1 justify-between items-center p-2 rounded-lg hover:bg-blue-50 dark:hover:bg-gray-800 dark:hover:bg-opacity-20"
                     >
                         <div class="flex flex-row gap-3 justify-start items-center flex-1">
                             <slot {option} {index}>
