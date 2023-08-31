dfc13fb10e843851e60a756c3a428fc835164635
Tuditi <daviddetroch@pm.me>
2023-08-31 15:02:43 +0200

    chore: add comments

--- packages/shared/test/setup.ts
+++ packages/shared/test/setup.ts
@@ -2,7 +2,9 @@
  * NOTE: This file is before creating additional setup for the testing
  * environment as needed (e.g. a particular object must be mocked or is mocked
  * everywhere).
+ * - reflect-metadata is needed for features from @iota/sdk which use reflection (decorators)
  */
+
 import 'reflect-metadata'
 import './mocks/api.mock'
 import './mocks/match-media.mock'
