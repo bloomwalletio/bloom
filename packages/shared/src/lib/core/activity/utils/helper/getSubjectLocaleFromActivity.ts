2df314d5dcf8a3adb1dc8ae8a898f8db5c2e4dfe
Mark Nardi <nardimark7@gmail.com>
2023-09-06 09:06:36 +0200

    get correct subject for evm addresses

--- packages/shared/src/lib/core/activity/utils/helper/getSubjectLocaleFromActivity.ts
+++ packages/shared/src/lib/core/activity/utils/helper/getSubjectLocaleFromActivity.ts
@@ -3,27 +3,12 @@ import { Activity } from '@core/activity/types'
 import { localize } from '@core/i18n'
 import { getLayer2NetworkFromAddress } from '@core/layer-2/actions'
 import { truncateString } from '@core/utils'
+import { getSubjectFromAddress } from '@core/wallet'
 import { SubjectType } from '@core/wallet/enums'
 import type { Subject } from '@core/wallet/types'
 
-export function getSubjectFromActivity(activity: Activity): Subject | undefined {
-    if (activity.parsedLayer2Metadata) {
-        return {
-            ...activity.subject,
-            ...(activity.subject?.type === SubjectType.Address && {
-                address: activity.parsedLayer2Metadata?.ethereumAddress,
-            }),
-        }
-    } else if (activity.subject?.type === SubjectType.Address) {
-        const network = getLayer2NetworkFromAddress(activity.subject.address)
-        return { ...activity.subject, address: network ?? activity.subject.address }
-    } else {
-        return activity.subject
-    }
-}
-
 export function getSubjectLocaleFromActivity(activity: Activity): string {
-    const { subject } = activity
+    const subject = getSubjectFromActivity(activity)
 
     if (activity.type === ActivityType.Basic && activity?.isShimmerClaiming) {
         return localize('general.shimmerGenesis')
@@ -32,11 +17,25 @@ export function getSubjectLocaleFromActivity(activity: Activity): string {
     } else if (subject?.type === SubjectType.Contact) {
         return truncateString(subject.contact?.name, 13, 0)
     } else if (subject?.type === SubjectType.Address) {
-        const address = activity?.parsedLayer2Metadata?.ethereumAddress ?? subject.address
-        const network = getLayer2NetworkFromAddress(address)
-
-        return network ?? truncateString(address, 6, 6)
+        const network = getLayer2NetworkFromAddress(subject.address)
+        return network ?? truncateString(subject.address, 6, 6)
     } else {
         return localize('general.unknownAddress')
+    }
+}
+
+function getSubjectFromActivity(activity: Activity): Subject | undefined {
+    if (activity.subject?.address && activity.parsedLayer2Metadata?.ethereumAddress) {
+        const network = getLayer2NetworkFromAddress(activity.subject.address)
+        if (network) {
+            return (
+                getSubjectFromAddress(activity.parsedLayer2Metadata.ethereumAddress, activity.destinationNetworkId) ??
+                activity.subject
+            )
+        } else {
+            return activity.subject
+        }
+    } else {
+        return activity.subject
     }
 }
