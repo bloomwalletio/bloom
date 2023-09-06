0454b557c139d4056844fc331f718195e20387eb
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-09-06 16:11:11 -0500

    Use alert box from UI kit

--- packages/desktop/components/popups/EnableLedgerBlindSigningPopup.svelte
+++ packages/desktop/components/popups/EnableLedgerBlindSigningPopup.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import { Icon as IconEnum } from '@auxiliary/icon'
+    import { Alert } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
     import { LedgerAppName, ledgerDeviceState } from '@core/ledger'
     import { closePopup } from '@desktop/auxiliary/popup'
-    import { Icon, Text, TextType } from '@ui'
+    import { Text, TextType } from '@ui'
     import { UiEventFunction } from 'shared/src/lib/core/utils'
 
     export let appName: LedgerAppName
@@ -20,14 +20,7 @@
 <Text type={TextType.h3} classes="mb-6">{localize('popups.enableLedgerBlindSigning.title')}</Text>
 
 <div class="w-full h-full space-y-2 flex flex-auto flex-col shrink-0">
-    <div class="bg-yellow-50 w-full h-full space-y-6 rounded-md px-6 py-4">
-        <span class="flex flex-row items-center space-x-4">
-            <Icon boxed height={18} width={18} icon={IconEnum.InfoFilled} classes="text-yellow-700" />
-            <Text type={TextType.p} fontSize="14" color="gray-700" darkColor="gray-700"
-                >{localize('popups.enableLedgerBlindSigning.info')}</Text
-            >
-        </span>
-    </div>
+    <Alert variant="warning" text={localize('popups.enableLedgerBlindSigning.info')} />
     <div>
         {#each STEPS as step}
             <Text type={TextType.p} fontSize="15" color="gray-600" classes="my-2">
