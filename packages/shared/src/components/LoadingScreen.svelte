50947de5d18fce69773cf03e332556e3bc92eb8a
Nicole O'Brien <nicole@bloomwallet.io>
2023-09-07 14:00:29 +0100

    fix: rename prop for progress

--- packages/shared/src/components/LoadingScreen.svelte
+++ packages/shared/src/components/LoadingScreen.svelte
@@ -8,6 +8,6 @@
 <div class={'flex flex-col justify-center align-center items-center w-full h-full'}>
     <Animation classes="h-64 w-64" animation="loading-desktop" loop={true} renderer="canvas" />
     <div class="flex-col justify-center items-center w-full max-w-lg">
-        <Progress size="lg" labelInside {progress} />
+        <Progress size="lg" showLabel {progress} />
     </div>
 </div>
