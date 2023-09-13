16ec462fc66957dba43d1f8d6c30b316995dc02a
Tuditi <daviddetroch@pm.me>
2023-09-13 09:19:00 +0200

    feat: add tag length validation

--- packages/shared/src/lib/core/wallet/utils/validateTag.ts
+++ packages/shared/src/lib/core/wallet/utils/validateTag.ts
@@ -1,8 +1,13 @@
 import { localize } from '@core/i18n'
+import { MAX_TAG_BYTES, getByteLengthOfString } from '@core/utils'
 import { RESERVED_TAG_KEYWORDS } from '../constants'
+import { TagLengthError } from '../errors'
 
 export function validateTag(tag: string): void {
-    console.log('inside validate')
+    if (getByteLengthOfString(tag) > MAX_TAG_BYTES) {
+        throw new TagLengthError()
+    }
+
     if (RESERVED_TAG_KEYWORDS.includes(tag)) {
         throw new Error(localize('error.send.reservedTagKeyword'))
     }
