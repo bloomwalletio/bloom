45b4a0aeea651b08b21f72ac44bb601893576e7e
Tuditi <45079109+Tuditi@users.noreply.github.com>
2023-09-13 22:25:06 +0200

    fix: validate tag (#681)
    * Hook up validation logic
    
    * chore: move errors around
    
    * feat: add tag length validation
    
    * fix: pass down error instead of validation
    
    * chore: remove redundant code
    
    * chore: remove validate function from OptionalInput
    
    * chore: minor type fixes
    
    * chore: cleanup closable input
    
    * fix: error is reset when validating
    
    * chore: cleanup
    
    * chore: cleanup NumberInput
    
    * chore: use validateTag function in deep link validation
    
    ---------
    
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>

--- packages/shared/src/components/inputs/NumberInput.svelte
+++ packages/shared/src/components/inputs/NumberInput.svelte
@@ -2,18 +2,20 @@
     import { TextInput } from '@ui'
     import { localize } from '@core/i18n'
 
-    export let inputElement: HTMLInputElement = undefined
+    export let inputElement: HTMLInputElement | undefined = undefined
 
     export let disabled = false
     export let hasFocus = false
     export let value: string
     export let isInteger: boolean
+    export let error: string = ''
 </script>
 
 <TextInput
     bind:inputElement
     bind:value
     bind:hasFocus
+    {error}
     {disabled}
     placeholder={localize('general.amount')}
     float={!isInteger}
