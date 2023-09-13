9aefb9a6e0bd998bad40e170326d6907cdf31f42
Tuditi <daviddetroch@pm.me>
2023-09-13 11:04:24 +0200

    chore: cleanup NumberInput

--- packages/shared/src/components/inputs/NumberInput.svelte
+++ packages/shared/src/components/inputs/NumberInput.svelte
@@ -2,27 +2,20 @@
     import { TextInput } from '@ui'
     import { localize } from '@core/i18n'
 
-    export let inputElement: HTMLInputElement = undefined
+    export let inputElement: HTMLInputElement | undefined = undefined
 
     export let disabled = false
     export let hasFocus = false
     export let value: string
     export let isInteger: boolean
     export let error: string = ''
-
-    let textInput: TextInput | undefined
-
-    export function validate(): void {
-        textInput?.validate()
-    }
 </script>
 
 <TextInput
-    bind:this={textInput}
     bind:inputElement
     bind:value
     bind:hasFocus
-    bind:error
+    {error}
     {disabled}
     placeholder={localize('general.amount')}
     float={!isInteger}
