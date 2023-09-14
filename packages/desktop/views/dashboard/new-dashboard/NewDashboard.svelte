3b447f158bdbe52ca15ac2e4886a3a9e36663a67
Mark Nardi <nardimark7@gmail.com>
2023-09-14 15:20:56 +0200

    ui: create rebranded tokenlist (#697)
    * setup
    
    * add initial list
    
    * fix content for tokenlist
    
    * Tokenlist
    
    * format
    
    * fix scroll
    
    * stuff
    
    * add locales

--- packages/desktop/views/dashboard/new-dashboard/NewDashboard.svelte
+++ packages/desktop/views/dashboard/new-dashboard/NewDashboard.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
     import { Pane } from '@ui'
     import { selectedAccount } from '@core/account/stores'
+    import TabSection from './tab-section/TabSection.svelte'
 </script>
 
 {#if $selectedAccount}
     <wallet-container
         class="w-full h-full flex flex-nowrap p-8 relative flex-1
-        bg-gray-50 dark:bg-gray-900 justify-center items-center"
+        bg-white dark:bg-gray-900 justify-center items-center"
     >
         {#key $selectedAccount?.index}
             <div class="h-full w-full flex flex-col gap-8">
                 <Pane>Balance section</Pane>
-                <Pane classes="flex-grow">Activity/Token section</Pane>
+                <Pane classes="flex-grow border border-solid border-gray-100">
+                    <TabSection />
+                </Pane>
             </div>
         {/key}
     </wallet-container>
