e775065febd01facfc0ce70da402d5159d08cf82
Mark Nardi <nardimark7@gmail.com>
2023-09-15 08:41:22 +0200

    fix date

--- packages/desktop/views/dashboard/new-dashboard/tab-section/activity/components/row-sections/ActivityActionSection.svelte
+++ packages/desktop/views/dashboard/new-dashboard/tab-section/activity/components/row-sections/ActivityActionSection.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import { Activity } from '@core/activity'
-    import { getFormattedTimeStamp } from '@core/i18n/utils'
+    import { formatDate } from '@core/i18n'
     import { FontWeight, Text } from '@ui'
 
     export let activity: Activity
 </script>
 
 <Text fontWeight={FontWeight.semibold} classes="text-start">{activity.action}</Text>
-<Text fontWeight={FontWeight.semibold} secondary classes="text-start">{getFormattedTimeStamp(activity.time)}</Text>
+<Text fontWeight={FontWeight.semibold} secondary classes="text-start"
+    >{formatDate(activity.time, { year: 'numeric', month: 'short', day: 'numeric' })}</Text
+>
