cf50b31418b5c481095135cc60dd7f540cc6c59e
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-09-18 15:58:41 -0500

    Fix profiles with no ShimmerEVM history

--- packages/desktop/views/dashboard/new-dashboard/panes/account-summary/components/AccountStardustNetworkSummary.svelte
+++ packages/desktop/views/dashboard/new-dashboard/panes/account-summary/components/AccountStardustNetworkSummary.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import { getNetwork, NetworkHealth, NetworkId } from '@core/network'
     import { IAccountState } from '@core/account'
-    import { getAccountTokensForSelectedAccount } from '@core/token/actions'
-    import { marketCoinPrices } from '@core/market/stores'
     import { formatTokenAmountBestMatch, ITokenWithBalance } from '@core/token'
     import { formatCurrency } from '@core/i18n'
     import { getMarketAmountFromTokenValue } from '@core/market/actions'
     import { ownedNfts } from '@core/nfts/stores'
+    import { selectedAccountTokens } from '@core/token/stores'
 
     import AccountNetworkSummary from './AccountNetworkSummary.svelte'
     import type { IAccountNetworkSummaryProps } from '../interfaces'
@@ -16,7 +15,7 @@
 
     function buildAccountStardustNetworkSummaryProps(): IAccountNetworkSummaryProps {
         const network = getNetwork()
-        const networkTokens = getAccountTokensForSelectedAccount($marketCoinPrices)?.[networkId]
+        const networkTokens = $selectedAccountTokens?.[networkId]
         const networkBaseCoin: ITokenWithBalance = networkTokens?.baseCoin
         const networkTokenBalance = formatTokenAmountBestMatch(networkBaseCoin.balance.total, networkBaseCoin.metadata)
         const networkFiatBalance = formatCurrency(
