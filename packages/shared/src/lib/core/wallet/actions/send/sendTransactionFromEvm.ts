6e5d0e17f674a37cc15738b242515d4d5a65dd3f
Mark Nardi <nardimark7@gmail.com>
2023-09-18 10:59:50 +0200

    generate hidden activity for native tokens

--- packages/shared/src/lib/core/wallet/actions/send/sendTransactionFromEvm.ts
+++ packages/shared/src/lib/core/wallet/actions/send/sendTransactionFromEvm.ts
@@ -6,10 +6,9 @@ import { IChain } from '@core/network'
 import { checkActiveProfileAuth } from '@core/profile/actions'
 import { signAndSendEvmTransaction } from './signAndSendEvmTransaction'
 import { generateActivityFromEvmTransaction } from '@core/activity/utils/generateActivityFromEvmTransaction'
-import { PersistedEvmTransaction, calculateAndAddPersistedBalanceChange } from '@core/activity'
-import { updateLayer2AccountBalanceForTokenOnChain } from '@core/layer-2/stores'
+import { PersistedEvmTransaction } from '@core/activity'
 import { getAddressFromAccountForNetwork } from '@core/account'
-import { BASE_TOKEN_ID } from '@core/token'
+import { updateL2BalanceWithoutActivity } from '../updateL2BalanceWithoutActivity'
 
 export async function sendTransactionFromEvm(
     transaction: EvmTransactionData,
@@ -42,26 +41,22 @@ export async function sendTransactionFromEvm(
                 addActivitiesToAccountActivitiesInAllAccountActivities(account.index, [activity])
 
                 if (getAddressFromAccountForNetwork(account, networkId) !== activity.subject?.address) {
-                    const tokenTransfer = activity.tokenTransfer ?? activity.baseTokenTransfer
-                    const delta =
-                        tokenTransfer.tokenId === BASE_TOKEN_ID
-                            ? (Number(tokenTransfer.rawAmount) + Number(activity?.transactionFee ?? 0)) * -1
-                            : Number(tokenTransfer.rawAmount) * -1
-                    const newBalance = updateLayer2AccountBalanceForTokenOnChain(
-                        account.index,
-                        networkId,
-                        tokenTransfer.tokenId,
-                        delta
-                    )
-                    await calculateAndAddPersistedBalanceChange(
-                        account.index,
-                        networkId,
-                        tokenTransfer.tokenId,
-                        newBalance,
-                        true
+                    // Currently only support outgoing transactions being added to activities so we can assume outgoing balance change
+                    if (activity.tokenTransfer) {
+                        await updateL2BalanceWithoutActivity(
+                            Number(activity.tokenTransfer.rawAmount),
+                            activity.tokenTransfer.tokenId,
+                            account,
+                            networkId
+                        )
+                    }
+                    await updateL2BalanceWithoutActivity(
+                        Number(activity.baseTokenTransfer.rawAmount) + Number(activity?.transactionFee ?? 0),
+                        activity.baseTokenTransfer.tokenId,
+                        account,
+                        networkId
                     )
                 }
-
                 if (callback && typeof callback === 'function') {
                     callback()
                 }
