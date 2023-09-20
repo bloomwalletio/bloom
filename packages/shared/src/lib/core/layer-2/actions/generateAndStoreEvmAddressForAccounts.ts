d39eb1d94c79db86ab3870ea36b296efe4aab445
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-09-20 02:30:45 -0500

    enhancement: add automatic EVM address generation for Stronghold profiles (#744)
    * Add automatic EVM address generation for Stronghold profiles
    
    * Add automatic EVM address generation when creating new accounts
    
    * Disable new dashboard
    
    * Add PR fixes 1/2
    
    * Add PR fixes 2/2
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

---
+++ packages/shared/src/lib/core/layer-2/actions/generateAndStoreEvmAddressForAccounts.ts
@@ -0,0 +1,40 @@
+import { IAccountState } from '@core/account/interfaces'
+import { Ledger } from '@core/ledger/classes'
+import { ProfileType } from '@core/profile/enums'
+import { api, getProfileManager } from '@core/profile-manager'
+import { updateActiveAccountPersistedData } from '@core/profile/actions'
+import { updateActiveAccount } from '@core/profile/stores'
+
+export async function generateAndStoreEvmAddressForAccounts(
+    profileType: ProfileType,
+    coinType: number,
+    ...accounts: IAccountState[]
+): Promise<void> {
+    for (const account of accounts ?? []) {
+        if (account.evmAddresses?.[coinType]) {
+            continue
+        }
+
+        const accountIndex = account.index
+
+        let evmAddress: string | undefined
+        if (profileType === ProfileType.Software) {
+            const manager = await api.getSecretManager(getProfileManager().id)
+            evmAddress = (
+                await manager.generateEvmAddresses({
+                    coinType,
+                    accountIndex,
+                    options: {
+                        internal: false,
+                    },
+                })
+            )?.[0]
+        } else {
+            evmAddress = await Ledger.generateEvmAddress(accountIndex, coinType)
+        }
+
+        const evmAddresses = { [coinType]: evmAddress }
+        updateActiveAccount(accountIndex, { evmAddresses })
+        updateActiveAccountPersistedData(accountIndex, { evmAddresses })
+    }
+}
