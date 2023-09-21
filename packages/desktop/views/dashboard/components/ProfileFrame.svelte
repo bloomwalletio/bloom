7db87b9e13034750d7c33540e46c335a3ae5392d
Mark Nardi <nardimark7@gmail.com>
2023-09-21 09:16:16 +0200

    update hover style

--- packages/desktop/views/dashboard/components/ProfileFrame.svelte
+++ packages/desktop/views/dashboard/components/ProfileFrame.svelte
@@ -9,24 +9,31 @@
     const { shouldOpenProfileModal } = $activeProfile
 </script>
 
-<button on:click={profileModal?.open}>
-    <Text>{$activeProfile.name}</Text>
-    <div class="relative">
-        <ProfileAvatar profile={$activeProfile} />
-        {#if !$shouldOpenProfileModal}
-            <Indicator size="sm" color="red" border="white" class="absolute top-0 right-0" />
-        {/if}
-    </div>
-</button>
+<profile-frame>
+    <button on:click={profileModal?.open}>
+        <Text>{$activeProfile.name}</Text>
+        <div class="relative">
+            <ProfileAvatar profile={$activeProfile} />
+            {#if !$shouldOpenProfileModal}
+                <Indicator size="sm" color="red" border="white" class="absolute top-0 right-0" />
+            {/if}
+        </div>
+    </button>
+</profile-frame>
 <ProfileActionsModal bind:modal={profileModal} />
 
 <style lang="postcss">
-    button {
+    profile-frame {
         @apply flex justify-between items-center;
         @apply w-full;
-        @apply px-7 py-4;
-        @apply hover:bg-purple-100;
+        @apply px-4 py-2;
         /* TODO: remove the hardcoded color when color system is in place */
         border-top: 1px solid #f1eef9;
+    }
+    button {
+        @apply flex justify-between items-center;
+        @apply w-full rounded;
+        @apply px-3 py-2;
+        @apply hover:bg-purple-100;
     }
 </style>
