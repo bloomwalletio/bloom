8be8f3ce1a2f2912c4196a1799b0b71f63b18528
Mark Nardi <nardimark7@gmail.com>
2023-09-21 16:00:59 +0200

    remove icon if only 1 address

--- packages/desktop/views/dashboard/new-dashboard/tab-section/activity/components/row-sections/ActivityAddressSection.svelte
+++ packages/desktop/views/dashboard/new-dashboard/tab-section/activity/components/row-sections/ActivityAddressSection.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Activity } from '@core/activity'
+    import { Activity, ActivityType } from '@core/activity'
     import AddressWithNetwork from '../AddressWithNetwork.svelte'
     import { Icon, IconName } from '@bloomwalletio/ui'
 
@@ -9,11 +9,15 @@
         (activity.sender &&
             activity.sender.address === activity.recipient?.address &&
             activity.sourceNetworkId === activity.destinationNetworkId) ||
-        activity.aliasId !== undefined
+        [ActivityType.Consolidation, ActivityType.Alias, ActivityType.Governance].includes(activity.type)
 </script>
 
-<div class="flex flex-row items-center">
-    <Icon name={IconName.Graph} size="md" color="gray-300" />
+<div class="h-full w-full flex flex-row items-center">
+    {#if (!activity.sender && !activity.recipient) || isSelfTransaction}
+        <div class="w-8" />
+    {:else}
+        <Icon name={IconName.Graph} size="md" color="gray-300" />
+    {/if}
 
     <div class="flex flex-col justify-between">
         {#if !activity.sender && !activity.recipient}
