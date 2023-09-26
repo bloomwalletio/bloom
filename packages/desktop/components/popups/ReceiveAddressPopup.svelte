1d2dfa8450f7850ffa4c37ba58e5475611d67397
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-09-26 15:26:00 -0500

    Get styling

--- packages/desktop/components/popups/ReceiveAddressPopup.svelte
+++ packages/desktop/components/popups/ReceiveAddressPopup.svelte
@@ -8,11 +8,17 @@
     $: receiveAddress = $selectedAccount.depositAddress
 </script>
 
-<receive-details class="w-full h-full space-y-6 flex flex-auto flex-col shrink-0">
-    <Text type="h5" textColor="text-brand">{localize('actions.receive')}</Text>
-    <div class="w-full flex flex-col items-center space-y-6">
+<receive-address-popup class="w-full h-full space-y-6 flex flex-auto flex-col shrink-0">
+    <Text type="h5" textColor="text-brand">{localize('popups.receiveAddress.title')}</Text>
+    <Text type="body2" textColor="text-secondary">{localize('popups.receiveAddress.body')}</Text>
+    <div
+        class="w-full py-6 space-y-6 flex flex-col items-center rounded-xl border border-solid border-stroke bg-surface-1"
+    >
+        <Text type="h6" textColor="text-brand"
+            >{localize('popups.receiveAddress.networkAddress', { networkName: 'Shimmer' })}</Text
+        >
         <QR data={receiveAddress} classes="w-1/2 h-1/2" />
         <AddressBox address={receiveAddress} clearBackground isCopyable />
     </div>
     <Button text={localize('actions.copyAddress')} on:click={() => setClipboard(receiveAddress)} />
-</receive-details>
+</receive-address-popup>
