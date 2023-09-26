67e4a2b7007a30ef2fc8cdadf0f90010ce1f3b85
Mark Nardi <nardimark7@gmail.com>
2023-09-26 18:39:08 +0200

    feat: add chainalias to agent id (#809)
    * add chainalias to agent id
    
    * update chainId and alias
    
    * cleanup generateAgentId function
    
    * revert chainId
    
    ---------
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/src/lib/core/layer-2/helpers/evmAddressToAgentId.ts
+++ packages/shared/src/lib/core/layer-2/helpers/evmAddressToAgentId.ts
@@ -1,6 +1,10 @@
+import { ChainType, IChain } from '@core/network'
 import { Converter } from '@iota/util.js'
 
-export function evmAddressToAgentId(evmStoreAccount: string): Uint8Array {
+export function evmAddressToAgentId(evmStoreAccount: string, chain: IChain): Uint8Array {
+    const chainConfiguration = chain.getConfiguration()
+    const chainAliasAddress = chainConfiguration.type === ChainType.Iscp ? chainConfiguration.aliasAddress : ''
+
     // This function constructs an AgentID that is required to be used with contracts
     // Wasp understands different AgentID types and each AgentID needs to provide a certain ID that describes it's address type.
     // In the case of EVM addresses it's ID 3.
@@ -9,8 +13,9 @@ export function evmAddressToAgentId(evmStoreAccount: string): Uint8Array {
     // Note: we need the evmStoreAccount to be in lower case,
     // otherwise fetching balances using the iscmagic contract will fail,
     // because evm addresses are case-insensitive but hexToBytes is not.
+    const chainAliasAddressBinary = Converter.hexToBytes(chainAliasAddress?.toLowerCase())
     const receiverAddrBinary = Converter.hexToBytes(evmStoreAccount?.toLowerCase())
-    const addressBytes = new Uint8Array([agentIDKindEthereumAddress, ...receiverAddrBinary])
+    const addressBytes = new Uint8Array([agentIDKindEthereumAddress, ...chainAliasAddressBinary, ...receiverAddrBinary])
 
     return addressBytes
 }
