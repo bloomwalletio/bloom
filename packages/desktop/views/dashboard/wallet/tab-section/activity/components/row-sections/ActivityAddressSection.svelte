2724a9e6164c4a9fa069e3bd6b1d95755d68103b
Mark Nardi <nardimark7@gmail.com>
2023-09-26 08:56:18 +0200

    update

--- packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/ActivityAddressSection.svelte
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/ActivityAddressSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { Activity, ActivityAction, ActivityType } from '@core/activity'
     import AddressWithNetwork from '../AddressWithNetwork.svelte'
+    import { appSettings } from '@core/app/stores'
 
     export let activity: Activity
 
@@ -10,6 +11,8 @@
             activity.sourceNetworkId === activity.destinationNetworkId) ||
         [ActivityType.Consolidation, ActivityType.Alias, ActivityType.Governance].includes(activity.type) ||
         [ActivityAction.Mint, ActivityAction.Burn].includes(activity.action)
+
+    $: svgColor = $appSettings.darkMode ? '#6F778A' : '#D1D5DB'
 </script>
 
 <div class="h-full w-full flex flex-row items-center">
@@ -18,13 +21,13 @@
     {:else}
         <svg width="32" height="30" viewBox="0 0 15 31" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g>
-                <circle cx="5" cy="5.25735" r="4" stroke="#d1d5db" stroke-width="2" />
+                <circle cx="5" cy="5.25735" r="4" stroke={svgColor} stroke-width="2" />
                 <path
                     d="M10.2426 21.2574L13.0711 24.0858C13.8521 24.8668 13.8521 26.1332 13.0711 26.9142L10.2426 29.7426"
-                    stroke="#d1d5db"
+                    stroke={svgColor}
                     stroke-width="2"
                 />
-                <path d="M5 9V21.2574C5 23.4665 6.79086 25.2574 9 25.2574H13" stroke="#d1d5db" stroke-width="2" />
+                <path d="M5 9V21.2574C5 23.4665 6.79086 25.2574 9 25.2574H13" stroke={svgColor} stroke-width="2" />
             </g>
         </svg>
     {/if}
