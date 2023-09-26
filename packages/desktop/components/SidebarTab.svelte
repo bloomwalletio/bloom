2d9e3689b28bf1de0be3668cf39741d575483853
Mark Nardi <nardimark7@gmail.com>
2023-09-26 11:11:27 +0200

    update chevron

--- packages/desktop/components/SidebarTab.svelte
+++ packages/desktop/components/SidebarTab.svelte
@@ -5,27 +5,15 @@
 
     export let tab: ISidebarTab = undefined
 
-    let showChevron = false
-
     $: isSelected = $dashboardRoute === tab?.route
-    $: color = showChevron || isSelected ? 'primary' : 'black'
+    $: color = isSelected ? 'primary' : 'text-primary'
 
     function onClick(): void {
-        _showChevron(false)
         tab?.onClick()
-    }
-
-    function _showChevron(show: boolean): void {
-        showChevron = show
     }
 </script>
 
-<button
-    on:mouseenter={() => _showChevron(true)}
-    on:mouseleave={() => _showChevron(false)}
-    class:selected={isSelected}
-    on:click={onClick}
->
+<button class:selected={isSelected} on:click={onClick}>
     <div class="flex flex-row relative space-x-4 pr-3">
         <Icon {color} name={tab?.icon} />
         {#if tab?.notificationType}
@@ -38,8 +26,8 @@
         {/if}
         <Text {color} weight="semibold">{tab.label}</Text>
     </div>
-    {#if showChevron}
-        <Icon color="primary" name={IconName.ChevronRight} />
+    {#if isSelected}
+        <Icon {color} name={IconName.ChevronRight} />
     {/if}
 </button>
 
@@ -47,12 +35,12 @@
     button {
         @apply flex flex-row flex-grow justify-between;
         @apply w-full;
-        @apply hover:bg-purple-100;
-        @apply text-purple-500;
+        @apply hover:bg-purple-100 dark:hover:bg-surface-2-dark;
+        @apply text-primary;
         @apply py-2.5 px-3 rounded-md;
 
         &.selected {
-            @apply bg-purple-100;
+            @apply bg-purple-100 dark:bg-surface-2-dark;
         }
     }
 </style>
