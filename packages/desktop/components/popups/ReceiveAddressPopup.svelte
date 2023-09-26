f7e975433aeff12e4b780b754279d9ec98ed51c3
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-09-26 16:56:46 -0500

    Get address generation flow working

--- packages/desktop/components/popups/ReceiveAddressPopup.svelte
+++ packages/desktop/components/popups/ReceiveAddressPopup.svelte
@@ -1,28 +1,52 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
     import { Button, Text } from '@bloomwalletio/ui'
-    import { QR, AddressBox, NetworkInput } from '@ui'
+    import { AddressBox, NetworkInput, QR } from '@ui'
     import { localize } from '@core/i18n'
     import { selectedAccount } from '@core/account/stores'
     import { setClipboard } from '@core/utils'
     import { ETHEREUM_COIN_TYPE, isEvmChain, isStardustNetwork, network, NetworkId } from '@core/network'
+    import { generateAndStoreEvmAddressForAccounts } from '@core/layer-2/actions'
+    import { activeProfile } from '@core/profile/stores'
+    import { checkActiveProfileAuth } from 'shared/src/lib/core/profile/actions'
+    import { LedgerAppName } from 'shared/src/lib/core/ledger'
 
-    let selectedNetworkId = $network.getMetadata().id
-    $: updateNetworkNameAndAddress(selectedNetworkId)
+    export let selectedNetworkId: NetworkId = $network.getMetadata().id
+    $: selectedNetworkId, updateNetworkNameAndAddress()
 
     let networkName: string
     let receiveAddress: string
-    function updateNetworkNameAndAddress(networkId: NetworkId): void {
-        if (isStardustNetwork(networkId)) {
+    function updateNetworkNameAndAddress(): void {
+        if (isStardustNetwork(selectedNetworkId)) {
             networkName = $network.getMetadata().name
             receiveAddress = $selectedAccount.depositAddress
-        } else if (isEvmChain(networkId)) {
-            networkName = $network.getChain(networkId).getConfiguration().name
-            receiveAddress = $selectedAccount.evmAddresses[ETHEREUM_COIN_TYPE]
+        } else if (isEvmChain(selectedNetworkId)) {
+            networkName = $network.getChain(selectedNetworkId)?.getConfiguration().name
+            receiveAddress = $selectedAccount.evmAddresses?.[ETHEREUM_COIN_TYPE]
+            if (!receiveAddress) {
+                void checkActiveProfileAuth(
+                    async () => {
+                        await generateAndStoreEvmAddressForAccounts(
+                            $activeProfile.type,
+                            ETHEREUM_COIN_TYPE,
+                            $selectedAccount
+                        )
+                        networkName = $network.getChain(selectedNetworkId)?.getConfiguration().name
+                        receiveAddress = $selectedAccount.evmAddresses?.[ETHEREUM_COIN_TYPE]
+                    },
+                    { ledger: true, stronghold: true, props: { selectedNetworkId } },
+                    LedgerAppName.Ethereum
+                )
+            }
         } else {
             networkName = undefined
             receiveAddress = undefined
         }
     }
+
+    onMount(() => {
+        updateNetworkNameAndAddress()
+    })
 </script>
 
 <receive-address-popup class="w-full h-full space-y-4 flex flex-auto flex-col shrink-0">
