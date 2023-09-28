f031f6be29b7048b5c5b735508eb17a8201d7aae
Nicole O'Brien <nicole@bloomwallet.io>
2023-09-28 13:56:38 +0100

    enhancement: improve breadcrumbs

---
+++ packages/desktop/views/dashboard/components/BreadcrumbButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Text } from '@bloomwalletio/ui'
+
+    export let disabled = false
+    export let text = ''
+
+    $: disabled = disabled
+</script>
+
+<button
+    on:click|stopPropagation
+    type="button"
+    class="flex flex-row justify-center items-center space-x-2 px-1.5 rounded-md"
+    {disabled}
+>
+    <slot name="button" />
+    {#if !$$slots.button}
+        <Text size="sm" weight="semibold" color="#1E1B4E">
+            {text}
+        </Text>
+    {/if}
+</button>
+
+<style lang="scss">
+    button {
+        &:hover:not(:disabled) {
+            @apply bg-purple-100;
+        }
+    }
+</style>
--- packages/desktop/views/dashboard/components/index.js
+++ packages/desktop/views/dashboard/components/index.js
@@ -1,2 +1,3 @@
+export { default as Breadcrumbs } from './Breadcrumbs.svelte'
 export { default as Navbar } from './Navbar.svelte'
 export { default as Sidebar } from './Sidebar.svelte'
