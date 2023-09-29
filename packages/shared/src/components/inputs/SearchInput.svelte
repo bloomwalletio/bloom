eecbdeae1d2763881d85804822cc4c485b08569b
Mark Nardi <nardimark7@gmail.com>
2023-09-29 08:53:52 +0200

    revert search input

--- packages/shared/src/components/inputs/SearchInput.svelte
+++ packages/shared/src/components/inputs/SearchInput.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { TextInput } from '@bloomwalletio/ui'
+    import { TextInput, Icon, FontWeight } from '@ui'
     import { localize } from '@core/i18n'
     import { debounce } from '@core/utils'
 
@@ -12,4 +12,14 @@
     })()
 </script>
 
-<TextInput bind:value={searchValue} label={localize('general.search')} />
+<TextInput
+    bind:value={searchValue}
+    placeholder={localize('general.search')}
+    fontSize="15"
+    clearPadding
+    containerClasses="py-2 px-3"
+    fontWeight={FontWeight.medium}
+    color="gray-500"
+>
+    <Icon slot="left" icon="search" classes="mr-2 text-gray-500 dark:text-white" />
+</TextInput>
