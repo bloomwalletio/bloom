92e81fd749c9055ab69d8c44be70bcd7808c7a46
Jean Ribeiro <contact@jeanribeiro.dev>
2023-09-29 10:09:34 -0300

    chore: change logo in about window

--- packages/desktop/lib/electron/preloads/about.preload.ts
+++ packages/desktop/lib/electron/preloads/about.preload.ts
@@ -12,12 +12,11 @@ interface AboutData {
 contextBridge.exposeInMainWorld('about', {
     getData: async (): Promise<AboutData> => {
         const data = await ipcRenderer.invoke('menu-data')
-        const stage = process.env.STAGE
 
         const aboutData: AboutData = {
             appName: productName,
             version: data.strings.version.replace('{version}', version),
-            iconPath: `./assets/logos/darkmode/${stage}_firefly_logo.svg`,
+            iconPath: './assets/logos/lightmode/bloom_logo.svg',
         }
 
         return aboutData
