7fc6cadaac27a97dd07d77b790b00fcac9e2af5b
Mark Nardi <nardimark7@gmail.com>
2023-10-02 09:08:20 +0200

    fix import

--- packages/shared/src/lib/auxiliary/deep-link/handlers/dapps/operations/handleDeepLinkAddWCConnectionOperation.ts
+++ packages/shared/src/lib/auxiliary/deep-link/handlers/dapps/operations/handleDeepLinkAddWCConnectionOperation.ts
@@ -1,7 +1,8 @@
 import { validateConnectionCodeUri } from '@auxiliary/wallet-connect/utils'
 import { pairWithNewDapp } from '@auxiliary/wallet-connect/actions'
 import { toggleDashboardDrawer } from '../../../../../../../../desktop/lib/auxiliary/drawer'
-import { DappConfigRoute, DashboardDrawerRoute } from '../../../../../../../../desktop/views/dashboard/drawers'
+import { DashboardDrawerRoute } from '../../../../../../../../desktop/views/dashboard/drawers/dashboard-drawer-route.enum'
+import { DappConfigRoute } from '../../../../../../../../desktop/views/dashboard/drawers/dapp-config/dapp-config-route.enum'
 
 export function handleDeepLinkAddWCConnectionOperation(pathnameParts: string[], searchString: string): void {
     const walletConnectUri = pathnameParts[1] + searchString
