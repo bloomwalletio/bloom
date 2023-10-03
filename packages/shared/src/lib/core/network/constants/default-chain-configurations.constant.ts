94e59fae4c1e3d84f641e1707bae51fbeb883978
Tuditi <45079109+Tuditi@users.noreply.github.com>
2023-10-03 17:11:54 +0200

    Fix: layer1 to layer2 (#894)
    * fix: update configuration values
    
    * fix: conditional encoding for agent ID
    
    * fix: address encoding
    
    * chore: cleanup
    
    * fix tests
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/shared/src/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/src/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -10,8 +10,9 @@ export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainC
         chainId: EvmChainId.ShimmerEvm,
         namespace: NetworkNamespace.Evm,
         coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.ShimmerEvm],
-        aliasAddress: '', // 'rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp',
-        iscpEndpoint: 'https://json-rpc.evm.shimmer.network',
+        aliasAddress: 'smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s',
+        iscpEndpoint:
+            'https://api.evm.shimmer.network/v1/chains/smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s',
         explorerUrl: 'https://explorer.evm.shimmer.network',
     },
     [SupportedNetworkId.Testnet]: {
@@ -21,7 +22,7 @@ export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainC
         chainId: EvmChainId.ShimmerEvmTestnet,
         namespace: NetworkNamespace.Evm,
         coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.ShimmerEvmTestnet],
-        aliasAddress: 'rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp',
+        aliasAddress: 'rms1pr75wa5xuepg2hew44vnr28wz5h6n6x99zptk2g68sp2wuu2karywgrztx3',
         iscpEndpoint: 'https://json-rpc.evm.testnet.shimmer.network',
         explorerUrl: 'https://explorer.evm.testnet.shimmer.network',
     },
