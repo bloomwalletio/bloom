4e0456679dd3de989eb75562fa3d043db70c38ce
Mark Nardi <nardimark7@gmail.com>
2023-10-03 09:40:54 +0200

    update amount pill

--- packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
@@ -2,7 +2,7 @@
     import { localize } from '@core/i18n'
     import { nftSearchTerm, ownedNfts, queriedNfts } from '@core/nfts/stores'
     import { Illustration, NftGallery, ReceiveButton, SearchInput } from '@ui'
-    import { Text, Avatar } from '@bloomwalletio/ui'
+    import { Text, Pill } from '@bloomwalletio/ui'
 </script>
 
 <div class="flex flex-col w-full h-full space-y-4">
@@ -10,13 +10,9 @@
         <div class="flex flex-row justify-between">
             <div class="flex flex-row text-left gap-2 items-center">
                 <Text type="h6">{localize('views.collectibles.gallery.title')}</Text>
-                <Avatar
-                    text={String($queriedNfts.length ?? '')}
-                    size="base"
-                    shape="square"
-                    textColor="secondary"
-                    backgroundColor="neutral/20"
-                />
+                <Pill color="neutral">
+                    <Text textColor="secondary">{String($queriedNfts.length ?? '')}</Text>
+                </Pill>
             </div>
             <div class="flex items-center" style="height: 40px">
                 <SearchInput bind:value={$nftSearchTerm} />
