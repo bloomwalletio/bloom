ec4375aa5b4a8fbd997e0a6c15c7a7da9443a8de
Mark Nardi <nardimark7@gmail.com>
2023-10-03 08:42:32 +0200

    feat: implement signing method in signmessagepopup (#877)
    * implement signing method for stronghold
    
    * fix: personal_sign rebase
    
    * chore: revert hex prefix
    
    * feat: return error to dApp
    
    * feat: check profile authentication before signing
    
    * ui: use rebrand elements
    
    * use constant
    
    ---------
    
    Co-authored-by: Tuditi <daviddetroch@pm.me>
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

---
+++ packages/shared/src/lib/core/stronghold/utils/getSignatureForStringWithStronghold.ts
@@ -0,0 +1,27 @@
+import { EvmChainId } from '@core/network/enums'
+import { getActiveProfile } from '@core/profile/stores'
+import { ECDSASignature, fromRpcSig } from '@ethereumjs/util'
+import type { Bip44 } from '@iota/sdk/out/types'
+import { api } from '@core/profile-manager'
+
+export async function getSignatureForStringWithStronghold(
+    text: string,
+    bip44Path: Bip44,
+    chainId?: EvmChainId
+): Promise<ECDSASignature> {
+    const manager = await api.getSecretManager(getActiveProfile()?.id)
+    const { signature } = await manager.signSecp256k1Ecdsa(text, bip44Path)
+
+    // Make Secp256k1Ecdsa into an Eip155Compatible Signature
+    const ecdsaSignature = fromRpcSig(signature)
+    if (chainId) {
+        ecdsaSignature.v = convertsVToEip155Compatible(ecdsaSignature.v, Number(chainId))
+    }
+    return ecdsaSignature
+}
+
+function convertsVToEip155Compatible(v: bigint, chainId: number): bigint {
+    const parity = Number(v) % 27
+    const newV = chainId * 2 + (35 + parity)
+    return BigInt(newV)
+}
