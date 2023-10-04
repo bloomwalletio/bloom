6cf9a7ac5032b388e4adea0806879119f8ffb18e
Mark Nardi <nardimark7@gmail.com>
2023-10-04 13:54:37 +0200

    fix colors

--- packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/ActivityActionSection.svelte
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/ActivityActionSection.svelte
@@ -3,68 +3,71 @@
     import { formatDate, localize } from '@core/i18n'
     import { Avatar, IconName } from '@bloomwalletio/ui'
     import { Text } from '@bloomwalletio/ui'
+    import { appSettings } from '@core/app/stores'
 
     export let activity: Activity
 
-    $: style = getActionStyle()
+    $: dark = $appSettings.darkMode
 
+    let style: { icon: IconName; color: string } | undefined
+    $: dark, (style = getActionStyle())
     function getActionStyle(): { icon: IconName; color: string } {
         const { type, isInternal, direction, action } = activity
 
         if (type === ActivityType.Basic && activity.isShimmerClaiming) {
             return {
                 icon: IconName.Receive,
-                color: 'purple',
+                color: 'brand',
             }
         }
         if (type === ActivityType.Governance) {
             return {
                 icon: IconName.Bank,
-                color: 'green',
+                color: dark ? 'neutral-1' : 'neutral-7',
             }
         } else if (type === ActivityType.Consolidation) {
             return {
-                icon: IconName.CoinSwap,
-                color: 'purple',
+                icon: IconName.Refresh,
+                color: dark ? 'neutral-1' : 'neutral-7',
             }
         } else if (action === ActivityAction.Mint) {
             return {
-                icon: IconName.Receive,
-                color: 'purple',
+                icon: IconName.Import,
+                color: 'success',
             }
         } else if (action === ActivityAction.Burn) {
             return {
-                icon: IconName.Send,
-                color: 'red',
+                icon: IconName.Refresh,
+                color: 'danger',
             }
         } else if (action === ActivityAction.InitialBalance) {
             return {
                 icon: IconName.Receive,
-                color: 'purple',
+                color: dark ? 'neutral-1' : 'neutral-7',
             }
         } else if (action === ActivityAction.Send || action === ActivityAction.BalanceChange) {
             if (isInternal) {
                 return {
                     icon: IconName.Refresh,
-                    color: 'purple',
+                    color: dark ? 'neutral-1' : 'neutral-7',
                 }
             }
             if (direction === ActivityDirection.Incoming || direction === ActivityDirection.SelfTransaction) {
                 return {
                     icon: IconName.Receive,
-                    color: 'purple',
+                    color: 'brand',
                 }
             }
             if (direction === ActivityDirection.Outgoing) {
                 return {
                     icon: IconName.Send,
-                    color: 'blue',
+                    color: 'info',
                 }
             }
         } else {
             return {
                 icon: IconName.HelpCircle,
-                color: 'gray',
+                color: dark ? 'neutral-1' : 'neutral-7',
             }
         }
     }
@@ -73,7 +76,7 @@
 <div class="text-start">
     <div class="flex flex-row items-center gap-2">
         <Avatar size="xxs" backgroundColor={style.color} icon={style.icon} textColor="primary" />
-        <Text customColor={style.color + '-500'}>{localize(getActivityTileTitle(activity))}</Text>
+        <Text customColor={style.color}>{localize(getActivityTileTitle(activity))}</Text>
     </div>
     <Text textColor="secondary">{formatDate(activity.time, { year: 'numeric', month: 'short', day: 'numeric' })}</Text>
 </div>
