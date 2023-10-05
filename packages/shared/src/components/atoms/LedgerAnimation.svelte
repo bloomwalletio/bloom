0a3051c3de4c35b0bb21350b4a40483b68af4b0d
Tuditi <daviddetroch@pm.me>
2023-10-05 10:42:59 +0200

    feat: remove ledger bg animation

--- packages/shared/src/components/atoms/LedgerAnimation.svelte
+++ packages/shared/src/components/atoms/LedgerAnimation.svelte
@@ -8,7 +8,6 @@
 </script>
 
 <ledger-animation class="w-full h-full relative flex {classes}">
-    <Animation animation="ledger-bg-desktop" classes="w-full h-full z-0 absolute {bgClasses}" />
     {#if animation}
         <Animation {animation} classes="z-0" />
     {:else if illustration}
