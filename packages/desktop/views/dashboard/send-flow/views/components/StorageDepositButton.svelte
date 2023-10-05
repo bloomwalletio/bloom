e6c89030e34f04a1e15fde8d2d3ad1de14787814
Tuditi <daviddetroch@pm.me>
2023-10-06 00:53:41 +0200

    chore: initial commit

--- packages/desktop/views/dashboard/send-flow/views/components/StorageDepositButton.svelte
+++ packages/desktop/views/dashboard/send-flow/views/components/StorageDepositButton.svelte
@@ -1,28 +1,33 @@
 <script lang="ts">
-    import { Icon as IconEnum } from '@auxiliary/icon'
+    import { Icon, IconName, Popover, Text, Toggle } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
     import { getBaseToken } from '@core/profile/actions'
     import { formatTokenAmountPrecise } from '@core/token'
-    import { FontWeight, Icon, Modal, Text, Toggle } from '@ui'
     import { fade } from 'svelte/transition'
 
     export let storageDeposit: number
     export let giftStorageDeposit: boolean
     export let disabled: boolean = false
 
-    let modal: Modal | undefined
+    let popover: Popover | undefined
+    let anchor: HTMLElement | undefined
 
     function toggleGiftStorageDeposit(): void {
         giftStorageDeposit = !giftStorageDeposit
-        modal?.close()
+        popover?.hide()
+    }
+
+    function onClick(): void {
+        popover?.show()
     }
 </script>
 
 <div class="relative">
     <button
+        bind:this={anchor}
+        on:click={onClick}
         class="flex items-center justify-center {disabled ? 'cursor-default' : 'cursor-pointer'}"
         {disabled}
-        on:click={modal?.open}
     >
         <div class="flex flex-row hover:text-blue-600 items-center">
             <Text
@@ -36,19 +41,17 @@
                     : formatTokenAmountPrecise(storageDeposit, getBaseToken())}
             </Text>
             {#if !disabled}
-                <Icon icon={IconEnum.ChevronDown} width="10" height="13" classes="text-blue-500 ml-1" />
+                <Icon name={IconName.ChevronDown} size="xxs" />
             {/if}
         </div>
     </button>
-    <Modal bind:this={modal} position={{ bottom: '30px', right: '0px' }} size="large">
-        <gift-storage-deposit-modal class="flex flex-row justify-between items-center p-4" in:fade={{ duration: 100 }}>
-            <div class="flex flex-col">
-                <Text fontWeight={FontWeight.medium}>{localize('popups.giftedStorageDeposit.tooltipTitle')}</Text>
-                <Text fontWeight={FontWeight.medium} color="gray-600" darkColor="gray-400"
-                    >{localize('popups.giftedStorageDeposit.tooltipDescription')}</Text
-                >
-            </div>
-            <Toggle color="green" active={giftStorageDeposit} onClick={toggleGiftStorageDeposit} />
-        </gift-storage-deposit-modal>
-    </Modal>
 </div>
+<Popover {anchor} bind:this={popover} placement="top-end" size="large">
+    <gift-storage-deposit-modal class="flex flex-row justify-between items-center p-4" in:fade={{ duration: 100 }}>
+        <div class="flex flex-col">
+            <Text>{localize('popups.giftedStorageDeposit.tooltipTitle')}</Text>
+            <Text>{localize('popups.giftedStorageDeposit.tooltipDescription')}</Text>
+        </div>
+        <Toggle label="giftStorageDeposit" checked={giftStorageDeposit} onClick={toggleGiftStorageDeposit} />
+    </gift-storage-deposit-modal>
+</Popover>
