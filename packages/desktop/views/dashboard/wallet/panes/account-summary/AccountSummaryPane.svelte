53bff86ce9e04b9359b6b0dda8bb7242e6dd332e
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-06 11:54:00 +0100

    fix: dashboard crash when no L2 chain

--- packages/desktop/views/dashboard/wallet/panes/account-summary/AccountSummaryPane.svelte
+++ packages/desktop/views/dashboard/wallet/panes/account-summary/AccountSummaryPane.svelte
@@ -7,12 +7,12 @@
     export let account: IAccountState
 
     const stardustNetworkId = getActiveNetworkId()
-    const evmChainNetworkId = $network.getChains()[0].getConfiguration().id
+    const evmChainNetworkId = $network.getChains()[0]?.getConfiguration()?.id
 </script>
 
 <Pane
     classes="
-        w-full flex shrink-0 grid grid-cols-3 
+        w-full flex shrink-0 grid {evmChainNetworkId ? 'grid-cols-3' : 'grid-cols-2'}
         bg-surface dark:bg-surface-dark 
         border border-solid border-stroke dark:border-stroke-dark 
         divide-x divide-solid divide-stroke dark:divide-stroke-dark 
@@ -21,5 +21,7 @@
 >
     <AccountSummary {stardustNetworkId} {evmChainNetworkId} />
     <AccountStardustNetworkSummary {account} networkId={stardustNetworkId} />
-    <AccountEvmChainSummary {account} networkId={evmChainNetworkId} />
+    {#if evmChainNetworkId}
+        <AccountEvmChainSummary {account} networkId={evmChainNetworkId} />
+    {/if}
 </Pane>
