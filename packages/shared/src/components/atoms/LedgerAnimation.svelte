ac07a324516efd7b0e28c5f47cf85228c14fd731
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-09 19:25:59 +0100

    revert  changes to the LedgerAnimation component

--- packages/shared/src/components/atoms/LedgerAnimation.svelte
+++ packages/shared/src/components/atoms/LedgerAnimation.svelte
@@ -1,26 +1,17 @@
 <script lang="ts">
-    import { Avatar } from '@bloomwalletio/ui'
     import { Animation, Illustration } from '@ui'
 
-    export let animation: string | undefined = undefined
-    export let illustration: string | undefined = undefined
+    export let animation: string = undefined
+    export let illustration: string = undefined
     export let classes: string = ''
-    export let avatar = false
+    export let bgClasses: string = ''
 </script>
 
-<ledger-animation class="h-full relative flex {classes}">
+<ledger-animation class="w-full h-full relative flex {classes}">
+    <Animation animation="ledger-bg-desktop" classes="w-full h-full z-0 absolute {bgClasses}" />
     {#if animation}
         <Animation {animation} classes="z-0" />
     {:else if illustration}
-        <div class="absolute">
-            <div class="relative top-30 left-37">
-                {#if avatar}
-                    <Avatar />
-                {:else}
-                    <Illustration illustration="ledger-pin" />
-                {/if}
-            </div>
-        </div>
-        <Illustration width={380} illustration="ledger-base" classes="z-0" />
+        <Illustration {illustration} classes="z-0" />
     {/if}
 </ledger-animation>
