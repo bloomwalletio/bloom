8a9b15d89445def70ef22079642b4b5a2383781b
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-09 14:07:30 +0100

    adjust column widths

--- packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
@@ -79,7 +79,7 @@
             @apply border-y border-solid border-stroke dark:border-stroke-dark;
 
             @apply grid;
-            grid-template-columns: 4fr 3fr 2fr 3fr;
+            grid-template-columns: 3fr 2fr 2fr 2fr;
         }
 
         info-section {
