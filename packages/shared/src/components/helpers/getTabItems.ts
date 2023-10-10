232c73ef76384a3b5f110234a9e3c25c23e24f93
Tuditi <daviddetroch@pm.me>
2023-10-10 17:40:03 +0200

    fix: locale

--- packages/shared/src/components/helpers/getTabItems.ts
+++ packages/shared/src/components/helpers/getTabItems.ts
@@ -3,5 +3,5 @@ import { KeyValue } from '../types'
 import { Tab } from '@ui/enums'
 
 export function getTabItems(items: Tab[]): KeyValue<string>[] {
-    return items.map((item) => ({ key: item, value: localize(item) }))
+    return items.map((item) => ({ key: item, value: localize(`general.${item}`) }))
 }
