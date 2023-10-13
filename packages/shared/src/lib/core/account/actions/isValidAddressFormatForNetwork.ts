48b5854d4225b7460900767c9023c3cc4dd0d037
Mark Nardi <nardimark7@gmail.com>
2023-10-13 12:07:44 +0200

    correctly validate bech32

--- packages/shared/src/lib/core/account/actions/isValidAddressFormatForNetwork.ts
+++ packages/shared/src/lib/core/account/actions/isValidAddressFormatForNetwork.ts
@@ -1,13 +1,15 @@
 import { NetworkId, isStardustNetwork } from '@core/network'
-import { isValidBech32AddressAndPrefix, validateEthereumAddress } from '@core/utils/crypto'
+import { validateBech32Address, validateEthereumAddress } from '@core/utils/crypto'
 import { getNetworkHrp } from '@core/profile/actions'
-import { localize } from '@core/i18n'
 
 export function isValidAddressFormatForNetwork(address: string, destinationNetworkId: NetworkId): string {
     if (isStardustNetwork(destinationNetworkId)) {
-        return isValidBech32AddressAndPrefix(address, getNetworkHrp())
-            ? ''
-            : localize('error.address.invalidBech32Format')
+        try {
+            validateBech32Address(getNetworkHrp(), address)
+            return ''
+        } catch (error) {
+            return error as string
+        }
     } else {
         try {
             validateEthereumAddress(address)
