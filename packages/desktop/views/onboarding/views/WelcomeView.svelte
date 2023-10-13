fc9ae24292bafc67903b998f943dcfc643b1d74e
Jean Ribeiro <contact@jeanribeiro.dev>
2023-10-13 20:45:31 -0300

    feat: adds WelcomeView background gradient

--- packages/desktop/views/onboarding/views/WelcomeView.svelte
+++ packages/desktop/views/onboarding/views/WelcomeView.svelte
@@ -81,6 +81,12 @@
     </illustration-container>
 </welcome-view>
 
+<bg-gradient-container>
+    <bg-gradient></bg-gradient>
+    <bg-gradient></bg-gradient>
+    <bg-gradient></bg-gradient>
+</bg-gradient-container>
+
 <style lang="scss">
     :global(welcome-view h1 strong) {
         background: linear-gradient(86deg, #c4b0ff 3.99%, #a82bdc 24.99%, #e65426 45.83%, #feb83a 69.6%);
@@ -133,5 +139,39 @@
         100% {
             opacity: 0.6;
         }
+    }
+
+    bg-gradient-container {
+        @apply absolute inset-0 flex items-center justify-center;
+        @apply dark:mix-blend-color-dodge;
+        @apply opacity-50 dark:opacity-20;
+        @apply pointer-events-none;
+        @apply blur-3xl;
+    }
+
+    bg-gradient:nth-of-type(1) {
+        @apply block absolute rounded-full opacity-50;
+        @apply -rotate-45;
+        @apply min-w-[18.75rem] min-h-[18.75rem];
+        @apply left-[-6.25rem];
+        @apply mix-blend-color;
+        background: conic-gradient(from 66deg at 51.51% 32.01%, #c395ff 70deg, #f4a5f6 286deg);
+        rotate: 100deg;
+    }
+
+    bg-gradient:nth-of-type(2) {
+        @apply block absolute rounded-full opacity-50;
+        @apply min-w-[18.75rem] min-h-[18.75rem];
+        @apply bottom-[-9.375rem];
+        @apply mix-blend-color;
+        background: conic-gradient(from 66deg at 51.51% 32.01%, #72aaff 70deg, #c395ff 178deg, #f4a5f6 286deg);
+    }
+
+    bg-gradient:nth-of-type(3) {
+        @apply block absolute rounded-full opacity-50;
+        @apply min-w-[18.75rem] min-h-[18.75rem];
+        @apply top-[-3.125rem] right-[-3.125rem];
+        @apply mix-blend-color;
+        background: conic-gradient(from 66deg at 51.51% 32.01%, #72aaff 70deg, #c395ff 178deg, #f4a5f6 286deg);
     }
 </style>
