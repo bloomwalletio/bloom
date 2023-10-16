d0923dda91925326d03e88c7b1e085e40b59e344
Jean Ribeiro <contact@jeanribeiro.dev>
2023-10-16 16:53:08 -0300

    enhancement: polish background gradient and make it a component

--- packages/desktop/views/onboarding/views/WelcomeView.svelte
+++ packages/desktop/views/onboarding/views/WelcomeView.svelte
@@ -12,6 +12,7 @@
     import { Illustration, Logo } from '@ui'
     import { LogoName } from '@auxiliary/logo/enums'
     import { onboardingRouter } from '../onboarding-router'
+    import BgGradient from '@views/components/BgGradient.svelte'
 
     let termsAccepted: boolean = false
     let flash: boolean = false
@@ -80,12 +81,7 @@
         <portal-glow></portal-glow>
     </illustration-container>
 </welcome-view>
-
-<bg-gradient-container>
-    <bg-gradient></bg-gradient>
-    <bg-gradient></bg-gradient>
-    <bg-gradient></bg-gradient>
-</bg-gradient-container>
+<BgGradient variant="spread" />
 
 <style lang="scss">
     :global(welcome-view h1 strong) {
@@ -110,7 +106,7 @@
         animation: portal-glow 5s ease-in-out infinite;
     }
 
-    :global(checkbox-container.flash check-box) {
+    :global(checkbox-container.flash button) {
         animation: flash 0.5s ease-in-out 3;
     }
 
@@ -139,39 +135,5 @@
         100% {
             opacity: 0.6;
         }
-    }
-
-    bg-gradient-container {
-        @apply absolute inset-0 flex items-center justify-center;
-        @apply dark:mix-blend-color-dodge;
-        @apply opacity-50 dark:opacity-20;
-        @apply pointer-events-none;
-        @apply blur-3xl;
-    }
-
-    bg-gradient:nth-of-type(1) {
-        @apply block absolute rounded-full opacity-50;
-        @apply -rotate-45;
-        @apply min-w-[18.75rem] min-h-[18.75rem];
-        @apply left-[-6.25rem];
-        @apply mix-blend-color;
-        background: conic-gradient(from 66deg at 51.51% 32.01%, #c395ff 70deg, #f4a5f6 286deg);
-        rotate: 100deg;
-    }
-
-    bg-gradient:nth-of-type(2) {
-        @apply block absolute rounded-full opacity-50;
-        @apply min-w-[18.75rem] min-h-[18.75rem];
-        @apply bottom-[-9.375rem];
-        @apply mix-blend-color;
-        background: conic-gradient(from 66deg at 51.51% 32.01%, #72aaff 70deg, #c395ff 178deg, #f4a5f6 286deg);
-    }
-
-    bg-gradient:nth-of-type(3) {
-        @apply block absolute rounded-full opacity-50;
-        @apply min-w-[18.75rem] min-h-[18.75rem];
-        @apply top-[-3.125rem] right-[-3.125rem];
-        @apply mix-blend-color;
-        background: conic-gradient(from 66deg at 51.51% 32.01%, #72aaff 70deg, #c395ff 178deg, #f4a5f6 286deg);
     }
 </style>
