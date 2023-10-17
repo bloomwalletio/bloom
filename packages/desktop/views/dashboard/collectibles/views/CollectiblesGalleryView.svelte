f3e0bb962bdebf18d71c98fb57e8f3f75bdce91b
Mark Nardi <nardimark7@gmail.com>
2023-10-17 09:30:14 +0200

    use correct avatar style

--- packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
@@ -2,7 +2,7 @@
     import { localize } from '@core/i18n'
     import { nftSearchTerm, ownedNfts, queriedNfts } from '@core/nfts/stores'
     import { NftGallery, SearchInput } from '@ui'
-    import { Avatar, Button, IconName, Text, Pill } from '@bloomwalletio/ui'
+    import { Button, Icon, IconName, Text, Pill } from '@bloomwalletio/ui'
     import { PopupId, openPopup } from '@desktop/auxiliary/popup'
     import { getActiveNetworkId } from '@core/network'
 
@@ -34,14 +34,18 @@
             <NftGallery nfts={$queriedNfts} />
         {:else}
             <div class="w-full h-full flex flex-col items-center justify-center gap-6">
-                <Avatar icon={IconName.Data} size="xxxl" customTextColor="primary-500" backgroundColor="surface" />
+                <empty-list-icon>
+                    <Icon name={IconName.Data} size="md" customColor="primary-500" />
+                </empty-list-icon>
                 <Text type="h6">{localize('views.collectibles.gallery.noResults')}</Text>
             </div>
         {/if}
     {:else}
         <div class="w-full h-full flex items-center justify-center grow-1">
             <div class="flex flex-col items-center gap-6">
-                <Avatar icon={IconName.Data} size="xxxl" customTextColor="primary-500" backgroundColor="surface" />
+                <empty-list-icon>
+                    <Icon name={IconName.Data} size="md" customColor="primary-500" />
+                </empty-list-icon>
                 <div class="flex flex-col items-center gap-4">
                     <Text type="h6">{localize('views.collectibles.gallery.emptyTitle')}</Text>
                     <Text textColor="secondary">{localize('views.collectibles.gallery.emptyDescription')}</Text>
@@ -51,3 +55,11 @@
         </div>
     {/if}
 </div>
+
+<style lang="scss">
+    empty-list-icon {
+        @apply flex items-center justify-center text-center;
+        @apply h-24 w-24 rounded-full shadow-elevation-4;
+        @apply bg-surface dark:bg-surface-dark;
+    }
+</style>
