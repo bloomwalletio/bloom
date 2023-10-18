7c822d3b0765d5aaf0670701d1675f7f9ca79f5c
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-18 14:17:40 +0100

    enhancement: show storage deposit on transaction summary for L2 -> L1
    Co-authored-by: MarkNerdi996 <MarkNerdi996@users.noreply.github.com>

--- packages/shared/src/lib/core/layer-2/constants/l1-to-l2-storage-deposit-buffer.constant.ts
+++ packages/shared/src/lib/core/layer-2/constants/l1-to-l2-storage-deposit-buffer.constant.ts
@@ -1,6 +1,6 @@
 import { SendFlowType } from '@core/wallet/enums'
 
 export const L2_TO_L1_STORAGE_DEPOSIT_BUFFER: { [key in SendFlowType]?: number } = {
-    [SendFlowType.TokenUnwrap]: 56_400,
+    [SendFlowType.TokenUnwrap]: 56_500,
     [SendFlowType.NftUnwrap]: 3_500,
 }
