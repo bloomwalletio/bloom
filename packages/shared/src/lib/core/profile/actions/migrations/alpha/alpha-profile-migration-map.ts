458a9226b999e066511f26f23019cddc40ebfb87
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-18 18:27:37 +0100

    fix: version index

--- packages/shared/src/lib/core/profile/actions/migrations/alpha/alpha-profile-migration-map.ts
+++ packages/shared/src/lib/core/profile/actions/migrations/alpha/alpha-profile-migration-map.ts
@@ -2,5 +2,5 @@ import { ProfileMigrationMap } from '../profile-migration-stage-map'
 import { alphaProfileMigration1 } from './alpha-profile-migration-1'
 
 export const ALPHA_PROFILE_MIGRATION_MAP: ProfileMigrationMap = {
-    1: alphaProfileMigration1,
+    0: alphaProfileMigration1,
 }
