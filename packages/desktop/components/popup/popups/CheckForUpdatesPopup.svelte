11be8fa859c22f20f7b12a72b13d1cadc1b869bd
Tuditi <daviddetroch@pm.me>
2023-10-18 17:13:58 +0200

    chore: rebrand checkForUpdates pop-up

--- packages/desktop/components/popup/popups/CheckForUpdatesPopup.svelte
+++ packages/desktop/components/popup/popups/CheckForUpdatesPopup.svelte
@@ -6,8 +6,8 @@
     import { formatDate, localize } from '@core/i18n'
     import { closePopup } from '@desktop/auxiliary/popup'
     import features from '@features/features'
-    import { Button, Text, TextType } from '@ui'
     import { onMount } from 'svelte'
+    import PopupTemplate from '../PopupTemplate.svelte'
 
     let hasAutoUpdate = false
 
@@ -17,11 +17,29 @@
     }
 
     function onVisitDownloadsClick(): void {
-        openUrlInBrowser('https://firefly.iota.org')
+        openUrlInBrowser('https://bloomwallet.io/')
     }
 
     function onCloseClick(): void {
         closePopup()
+    }
+
+    let continueButton
+    function setContinueButton(): void {
+        if (hasAutoUpdate && !$appVersionDetails.upToDate) {
+            continueButton = {
+                onClick: onDownloadClick,
+                disabled: $appUpdateState.busy,
+                text: localize('actions.updateBloom'),
+            }
+        } else if (!$appVersionDetails.upToDate) {
+            continueButton = {
+                onClick: onVisitDownloadsClick,
+                text: localize('actions.viewDownloads'),
+            }
+        } else {
+            return undefined
+        }
     }
 
     onMount(() => {
@@ -29,61 +47,51 @@
             checkForAppUpdate()
         }
         hasAutoUpdate = features.electron.autoUpdate[OS]?.enabled
+        setContinueButton()
     })
 </script>
 
-<Text type={TextType.h5} classes="mb-5">{localize('popups.appUpdate.title')}</Text>
-<div class="flex w-full flex-col space-y-6">
-    <div class="flex w-full flex-col space-y-5">
+<PopupTemplate
+    title={localize('popups.appUpdate.title')}
+    backButton={{
+        text: localize('actions.cancel'),
+        onClick: onCloseClick,
+    }}
+    {continueButton}
+>
+    <Table
+        items={[
+            {
+                key: localize('popups.appUpdate.installedVersion'),
+                value: $appVersionDetails.currentVersion,
+            },
+            {
+                key: localize('popups.appUpdate.stage'),
+                value: localize(`popups.appUpdate.${APP_STAGE}`),
+            },
+        ]}
+    />
+    {#if $appVersionDetails.upToDate}
+        <Alert variant="success" text={localize('popups.appUpdate.latestInstalled')} />
+    {:else}
         <Table
             items={[
                 {
-                    key: localize('popups.appUpdate.installedVersion'),
-                    value: $appVersionDetails.currentVersion,
+                    key: localize('popups.appUpdate.newVerion'),
+                    value: $appVersionDetails.newVersion,
                 },
                 {
-                    key: localize('popups.appUpdate.stage'),
-                    value: localize(`popups.appUpdate.${APP_STAGE}`),
+                    key: localize('popups.appUpdate.releasedAt'),
+                    value: formatDate($appVersionDetails.newVersionReleaseDate, {
+                        dateStyle: 'long',
+                        timeStyle: 'medium',
+                    }),
                 },
             ]}
         />
-        {#if $appVersionDetails.upToDate}
-            <Alert variant="success" text={localize('popups.appUpdate.latestInstalled')} />
-        {:else}
-            <Table
-                items={[
-                    {
-                        key: localize('popups.appUpdate.newVerion'),
-                        value: $appVersionDetails.newVersion,
-                    },
-                    {
-                        key: localize('popups.appUpdate.releasedAt'),
-                        value: formatDate($appVersionDetails.newVersionReleaseDate, {
-                            dateStyle: 'long',
-                            timeStyle: 'medium',
-                        }),
-                    },
-                ]}
-            />
-            <Alert
-                variant="info"
-                text={localize(`popups.appUpdate.${hasAutoUpdate ? 'updateAvailable' : 'updatesDisabled'}`)}
-            />
-        {/if}
-    </div>
-
-    <div class="flex flex-row justify-center w-full space-x-4">
-        <Button classes={$appVersionDetails.upToDate ? 'w-full' : 'w-1/2'} outline onClick={onCloseClick}>
-            {localize('actions.cancel')}
-        </Button>
-        {#if hasAutoUpdate && !$appVersionDetails.upToDate}
-            <Button classes="w-1/2" onClick={onDownloadClick} disabled={$appUpdateState.busy}>
-                {localize('actions.updateBloom')}
-            </Button>
-        {:else if !$appVersionDetails.upToDate}
-            <Button classes="w-1/2" onClick={onVisitDownloadsClick}>
-                {localize('actions.viewDownloads')}
-            </Button>
-        {/if}
-    </div>
-</div>
+        <Alert
+            variant="info"
+            text={localize(`popups.appUpdate.${hasAutoUpdate ? 'updateAvailable' : 'updatesDisabled'}`)}
+        />
+    {/if}
+</PopupTemplate>
