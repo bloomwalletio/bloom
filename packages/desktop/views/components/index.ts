da644f654581f951ce477927f818a1218e72f5be
Jean Ribeiro <contact@jeanribeiro.dev>
2023-10-18 11:26:38 -0300

    feat: adds Particles component (#1120)
    * feat: adds Particles component
    
    * fix: adds pointer-events-none
    
    * fix: Particles component

--- packages/desktop/views/components/index.ts
+++ packages/desktop/views/components/index.ts
@@ -1,2 +1,4 @@
+export { default as BgGradient } from './BgGradient.svelte'
 export { default as LoggedOutLayout } from './LoggedOutLayout.svelte'
 export { default as OnboardingLayout } from './OnboardingLayout.svelte'
+export { default as Particles } from './Particles.svelte'
