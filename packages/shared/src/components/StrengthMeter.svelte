4c0eb9d335264283057f97137267fe69c09b1e1a
Jean Ribeiro <contact@jeanribeiro.dev>
2023-10-19 18:54:26 -0300

    chore: update EncryptMnemonicView

--- packages/shared/src/components/StrengthMeter.svelte
+++ packages/shared/src/components/StrengthMeter.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-    import { Text } from '@bloomwalletio/ui'
+    import { Pill, Text } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
 
     export let strength: 0 | 1 | 2 | 3 | 4 | undefined = undefined
 
-    const STRENGTH_COLORS = ['neutral-4', 'danger-400', 'orange-400', 'warning-400', 'success-400']
+    const STRENGTH_COLORS = ['danger', 'danger', 'orange', 'warning', 'success']
     const STRENGTH_LEVELS = 4
 </script>
 
 {#if strength !== undefined}
     <strength-meter>
         <text-container>
-            <Text>{localize('general.passwordStrength')}:</Text>
-            <Text customColor={STRENGTH_COLORS[strength]} transform="uppercase">
-                {localize(`general.passwordStrength${strength}`)}
-            </Text>
+            <Text>{localize('general.passwordStrength')}</Text>
+            <Pill color={STRENGTH_COLORS[strength]}>
+                <Text textColor="current">
+                    {localize(`general.passwordStrength${strength}`).toLocaleUpperCase()}
+                </Text>
+            </Pill>
         </text-container>
-        <strength-block-container>
+        <strength-block-container class:ghost={strength === 0}>
             {#each Array(STRENGTH_LEVELS) as _, i}
-                <strength-block class="bg-{STRENGTH_COLORS[strength > i ? i + 1 : 0]}" />
+                <strength-block class="bg-{STRENGTH_COLORS[strength > i ? i + 1 : 0]}" class:ghost={strength <= i} />
             {/each}
         </strength-block-container>
     </strength-meter>
@@ -30,7 +32,7 @@
     }
 
     text-container {
-        @apply flex flex-row gap-1;
+        @apply flex flex-row items-center gap-3;
     }
 
     strength-block-container {
@@ -38,6 +40,10 @@
     }
 
     strength-block {
-        @apply rounded-[0.625rem] w-[1.375rem] h-1;
+        @apply rounded-[0.625rem] w-10 h-1;
+
+        &.ghost {
+            @apply opacity-30;
+        }
     }
 </style>
