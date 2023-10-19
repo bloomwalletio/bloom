f6ac9b614c4c750e30b4ae2b6a42377ae08e6da5
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-19 17:18:53 +0100

    refactor: rename file

--- packages/desktop/views/onboarding/views/create-from-mnemonic/views/index.js
+++ packages/desktop/views/onboarding/views/create-from-mnemonic/views/index.js
@@ -1,3 +1,3 @@
-export { default as VerifyMnemonicStatusView } from './VerifyMnemonicStatusView.svelte'
+export { default as VerifyMnemonicSuccessView } from './VerifyMnemonicSuccessView.svelte'
 export { default as VerifyMnemonicView } from './VerifyMnemonicView.svelte'
 export { default as ViewMnemonicView } from './ViewMnemonicView.svelte'
