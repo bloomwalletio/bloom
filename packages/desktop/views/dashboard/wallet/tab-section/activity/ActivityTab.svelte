25ab6608f19e6627171b3fd4eccf74c2d2028345
Mark Nardi <nardimark7@gmail.com>
2023-10-19 09:07:54 +0200

    fix padding on activity header

--- packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
@@ -74,7 +74,7 @@
 
         header-row {
             @apply w-full;
-            @apply px-5 py-2;
+            @apply pl-5 py-2 pr-7;
             @apply bg-surface-1 dark:bg-surface-1-dark;
             @apply border-y border-solid border-stroke dark:border-stroke-dark;
 
