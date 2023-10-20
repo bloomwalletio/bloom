870596d1efb4e32df3ba5a451eb908edeef33a11
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-10-20 10:52:56 -0500

    Rebrand profile settings

--- packages/desktop/views/dashboard/settings/views/profile/Currency.svelte
+++ packages/desktop/views/dashboard/settings/views/profile/Currency.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
+    import { IOption, SelectInput, Text } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
     import { MarketCurrency } from '@core/market'
     import { activeProfile, updateActiveProfileSettings } from '@core/profile/stores'
-    import type { IDropdownItem } from '@core/utils'
-    import { Dropdown, Text } from '@ui'
 
-    let currencyList: IDropdownItem<MarketCurrency>[]
-    $: currencyList = Object.values(MarketCurrency)
+    const options: IOption[] = Object.values(MarketCurrency)
         .map((currency) => ({ value: currency, label: currency.toUpperCase() }))
         .sort()
 
-    function onCurrencyChange(item: IDropdownItem<MarketCurrency>): void {
-        updateActiveProfileSettings({ marketCurrency: item.value })
+    let selected: IOption = options.find((option) => option.value === $activeProfile?.settings?.marketCurrency)
+    $: if (selected) onCurrencyChange(selected)
+
+    function onCurrencyChange(option: IOption): void {
+        updateActiveProfileSettings({ marketCurrency: option.value as MarketCurrency })
     }
 </script>
 
-<Text type="h4" classes="mb-3">{localize('views.settings.currency.title')}</Text>
-<Text type="p" secondary classes="mb-5">{localize('views.settings.currency.description')}</Text>
-<Dropdown
-    value={$activeProfile?.settings.marketCurrency}
-    items={currencyList}
-    sortItems
-    onSelect={onCurrencyChange}
-    enableTyping
-/>
+<Text type="body2" class="mb-2">{localize('views.settings.currency.title')}</Text>
+<Text type="base" textColor="secondary" class="mb-6">{localize('views.settings.currency.description')}</Text>
+<SelectInput bind:selected value={selected.value} {options} />
