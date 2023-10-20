eb053efe137b87a07b4265e78eb96767e47caeb1
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-20 15:11:20 +0100

    adjust colors and locals

--- packages/shared/src/components/StrengthMeter.svelte
+++ packages/shared/src/components/StrengthMeter.svelte
@@ -4,7 +4,7 @@
 
     export let strength: 0 | 1 | 2 | 3 | 4 | undefined = undefined
 
-    const STRENGTH_COLORS = ['danger', 'danger', 'orange', 'warning', 'success']
+    const STRENGTH_COLORS = ['danger', 'orange', 'warning', 'info', 'success']
     const STRENGTH_LEVELS = 4
 </script>
 
@@ -13,9 +13,7 @@
         <text-container>
             <Text>{localize('general.passwordStrength')}</Text>
             <Pill color={STRENGTH_COLORS[strength]}>
-                <Text textColor="current">
-                    {localize(`general.passwordStrength${strength}`).toLocaleUpperCase()}
-                </Text>
+                {localize(`general.passwordStrength${strength}`).toLocaleUpperCase()}
             </Pill>
         </text-container>
         <strength-block-container class:ghost={strength === 0}>
