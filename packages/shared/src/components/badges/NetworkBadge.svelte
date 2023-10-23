08c749f339566fd62e41af4f298502fbc716fdd4
Nicole O'Brien <nicole@bloomwallet.io>
2023-10-23 13:25:08 +0100

    ui: polish profile avatar with badge

--- packages/shared/src/components/badges/NetworkBadge.svelte
+++ packages/shared/src/components/badges/NetworkBadge.svelte
@@ -14,7 +14,7 @@
 
 <network-icon-badge
     bind:this={anchor}
-    class="block absolute -right-1 -bottom-1 rounded-full bg-white dark:bg-gray-900 p-0.5"
+    class="block absolute -right-1 -bottom-1 rounded-full bg-white dark:bg-gray-900 {size === 'md' ? 'p-1' : 'p-0.5'}"
 >
     <NetworkAvatar {size} {networkId} />
 </network-icon-badge>
