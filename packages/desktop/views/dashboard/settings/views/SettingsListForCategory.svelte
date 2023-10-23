b4a352ddf8dbb8ac9d989a7f4317f6193e6b1f65
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-10-23 11:40:19 -0500

    Adjust padding for settings category header

--- packages/desktop/views/dashboard/settings/views/SettingsListForCategory.svelte
+++ packages/desktop/views/dashboard/settings/views/SettingsListForCategory.svelte
@@ -23,7 +23,7 @@
         ) ?? []
 </script>
 
-<div class="flex flex-col p-8 space-y-5">
+<div class="flex flex-col px-8 pb-8 space-y-5">
     <Text type="h6">
         {localize(`views.settings.${category}.title`)}
     </Text>
