b8ca9ff810b881dc46a95bec821828dc11ce3197
Mark Nardi <nardimark7@gmail.com>
2023-10-24 02:20:26 +0200

    ui: add send nft activity for sent l2 nft (#1127)
    * create nft activity from evm transaction
    
    * fix address on L2->L2
    
    * fetch L2 nfts if not owned
    
    ---------
    
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>

--- packages/shared/src/lib/core/layer-2/utils/getTransferInfoFromTransactionData.ts
+++ packages/shared/src/lib/core/layer-2/utils/getTransferInfoFromTransactionData.ts
@@ -7,13 +7,14 @@ import { AbiDecoder, HEX_PREFIX } from '@core/utils'
 import { Erc20TransferMethodInputs, IscCallMethodInputs, IscSendMethodInputs } from '../interfaces'
 import { BASE_TOKEN_ID } from '@core/token/constants'
 import { IChain } from '@core/network'
+import { AssetType, TransferredAssetId } from '..'
 
 export function getTransferInfoFromTransactionData(
     transaction: PersistedEvmTransaction,
     address: string,
     networkId: NetworkId,
     chain: IChain
-): { tokenId: string; rawAmount: string; recipientAddress: string } | undefined {
+): { asset: TransferredAssetId; recipientAddress: string } | undefined {
     if (transaction.data) {
         const isErc20 = isTrackedTokenAddress(networkId, address)
         const isIscContract = address === ISC_MAGIC_CONTRACT_ADDRESS
@@ -30,13 +31,25 @@ export function getTransferInfoFromTransactionData(
             case 'call': {
                 const inputs = decoded.inputs as IscCallMethodInputs
                 const nativeToken = inputs?.allowance?.nativeTokens?.[0]
+                const nftId = inputs?.allowance?.nfts?.[0]
                 const agentId = inputs?.params.items?.find((item) => item.key === '0x61')?.value
 
                 if (nativeToken) {
                     return {
-                        tokenId: nativeToken.ID.data,
-                        rawAmount: nativeToken.amount,
-                        recipientAddress: HEX_PREFIX + agentId?.slice(4),
+                        asset: {
+                            type: AssetType.Token,
+                            tokenId: nativeToken.ID.data,
+                            rawAmount: nativeToken.amount,
+                        },
+                        recipientAddress: HEX_PREFIX + agentId?.substring(agentId.length - 40),
+                    }
+                } else if (nftId) {
+                    return {
+                        asset: {
+                            type: AssetType.Nft,
+                            nftId,
+                        },
+                        recipientAddress: HEX_PREFIX + agentId?.substring(agentId.length - 40),
                     }
                 } else {
                     return undefined
@@ -46,8 +59,11 @@ export function getTransferInfoFromTransactionData(
                 const inputs = decoded.inputs as Erc20TransferMethodInputs
 
                 return {
-                    tokenId: address,
-                    rawAmount: String(inputs._value),
+                    asset: {
+                        type: AssetType.Token,
+                        tokenId: address,
+                        rawAmount: String(inputs._value),
+                    },
                     recipientAddress: inputs._to,
                 }
             }
@@ -56,15 +72,21 @@ export function getTransferInfoFromTransactionData(
 
                 if (inputs.assets.baseTokens) {
                     return {
-                        tokenId: BASE_TOKEN_ID,
-                        rawAmount: inputs.assets.baseTokens,
+                        asset: {
+                            type: AssetType.BaseCoin,
+                            tokenId: BASE_TOKEN_ID,
+                            rawAmount: inputs.assets.baseTokens,
+                        },
                         recipientAddress: inputs.targetAddress.data,
                     }
                 } else if (inputs.assets.nativeTokens) {
                     const nativeToken = inputs.assets.nativeTokens[0]
                     return {
-                        tokenId: nativeToken.ID.data,
-                        rawAmount: nativeToken.amount,
+                        asset: {
+                            type: AssetType.Token,
+                            tokenId: nativeToken.ID.data,
+                            rawAmount: nativeToken.amount,
+                        },
                         recipientAddress: inputs.targetAddress.data,
                     }
                 }
@@ -76,8 +98,11 @@ export function getTransferInfoFromTransactionData(
         }
     } else {
         return {
-            tokenId: BASE_TOKEN_ID,
-            rawAmount: String(Number(transaction.value) / Number(WEI_PER_GLOW)),
+            asset: {
+                type: AssetType.BaseCoin,
+                tokenId: BASE_TOKEN_ID,
+                rawAmount: String(Number(transaction.value) / Number(WEI_PER_GLOW)),
+            },
             recipientAddress: address,
         }
     }
