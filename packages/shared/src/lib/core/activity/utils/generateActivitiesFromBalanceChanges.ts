6a729380c7b00e34ec880cd573e3393f84a25d6a
Mark Nardi <nardimark7@gmail.com>
2023-10-24 14:05:47 +0200

    generate balance change activities on login

--- packages/shared/src/lib/core/activity/utils/generateActivitiesFromBalanceChanges.ts
+++ packages/shared/src/lib/core/activity/utils/generateActivitiesFromBalanceChanges.ts
@@ -1,9 +1,9 @@
 import { IAccountState } from '@core/account'
-import { Activity } from '../types'
+import { Activity, INftBalanceChange, ITokenBalanceChange } from '../types'
 import { getBalanceChanges } from '../stores'
 import { get } from 'svelte/store'
-import { network } from '@core/network'
-import { generateTokenBalanceChangeActivity } from './evm'
+import { NetworkId, network } from '@core/network'
+import { generateNftBalanceChangeActivity, generateTokenBalanceChangeActivity } from './evm'
 
 export async function generateActivitiesFromBalanceChanges(account: IAccountState): Promise<Activity[]> {
     const activities: Activity[] = []
@@ -11,23 +11,62 @@ export async function generateActivitiesFromBalanceChanges(account: IAccountStat
     const chains = get(network)?.getChains() ?? []
     for (const chain of chains) {
         const networkId = chain.getConfiguration().id
-        const balanceChanges = getBalanceChanges(account.index, networkId)?.tokens
-        const tokenIds = balanceChanges ? Object.keys(balanceChanges) : []
+        const balanceChanges = getBalanceChanges(account.index, networkId)
 
-        for (const tokenId of tokenIds) {
-            for (const balanceChangeForAsset of balanceChanges[tokenId]) {
-                if (!balanceChangeForAsset.hidden) {
-                    const activity = await generateTokenBalanceChangeActivity(
-                        networkId,
-                        tokenId,
-                        balanceChangeForAsset,
-                        account
-                    )
-                    activities.push(activity)
-                }
+        const tokenActivites = await generateActivitiesFromTokenBalanceChanges(
+            account,
+            networkId,
+            balanceChanges.tokens
+        )
+        activities.push(...tokenActivites)
+        const nftActivites = await generateActivitiesFromNftBalanceChanges(account, networkId, balanceChanges.nfts)
+        activities.push(...nftActivites)
+    }
+
+    return activities
+}
+
+export async function generateActivitiesFromTokenBalanceChanges(
+    account: IAccountState,
+    networkId: NetworkId,
+    tokenBalanceChanges: {
+        [tokenId: string]: ITokenBalanceChange[]
+    }
+): Promise<Activity[]> {
+    const activities: Activity[] = []
+    const tokenIds = tokenBalanceChanges ? Object.keys(tokenBalanceChanges) : []
+    for (const tokenId of tokenIds) {
+        for (const balanceChangeForToken of tokenBalanceChanges[tokenId]) {
+            if (!balanceChangeForToken.hidden) {
+                const activity = await generateTokenBalanceChangeActivity(
+                    networkId,
+                    tokenId,
+                    balanceChangeForToken,
+                    account
+                )
+                activities.push(activity)
             }
         }
     }
+    return activities
+}
 
+export async function generateActivitiesFromNftBalanceChanges(
+    account: IAccountState,
+    networkId: NetworkId,
+    nftBalanceChanges: {
+        [nftId: string]: INftBalanceChange[]
+    }
+): Promise<Activity[]> {
+    const activities: Activity[] = []
+    const nftIds = nftBalanceChanges ? Object.keys(nftBalanceChanges) : []
+    for (const nftId of nftIds) {
+        for (const balanceChangeForNft of nftBalanceChanges[nftId]) {
+            if (!balanceChangeForNft.hidden) {
+                const activity = await generateNftBalanceChangeActivity(networkId, nftId, balanceChangeForNft, account)
+                activities.push(activity)
+            }
+        }
+    }
     return activities
 }
