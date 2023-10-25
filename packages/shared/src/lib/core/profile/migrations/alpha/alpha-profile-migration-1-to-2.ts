bd5ace6b6b60f1108a1b78f92abd1706b1bc387b
Mark Nardi <nardimark7@gmail.com>
2023-10-25 10:58:40 +0200

    add migration

---
+++ packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-1-to-2.ts
@@ -0,0 +1,30 @@
+import { ITokenBalanceChange, persistedBalanceChanges } from '@core/activity'
+import { NetworkId } from '@core/network'
+
+export async function alphaProfileMigration0To2(existingProfile: unknown): Promise<void> {
+    const profileId = (existingProfile as { id?: string })?.id
+    if (profileId) {
+        persistedBalanceChanges.update((state) => {
+            const balanceChangesFoProfile = state[profileId]
+
+            for (const accountId of Object.keys(balanceChangesFoProfile)) {
+                const balanceChangesFoProfileForAccount = state[profileId][accountId]
+
+                for (const networkId of Object.keys(balanceChangesFoProfileForAccount)) {
+                    const balanceChangesFoProfileForNetwork = balanceChangesFoProfileForAccount[
+                        networkId as NetworkId
+                    ] as unknown as {
+                        [tokenId: string]: ITokenBalanceChange[]
+                    }
+
+                    state[profileId][accountId][networkId as NetworkId] = {
+                        tokens: balanceChangesFoProfileForNetwork,
+                        nfts: {},
+                    }
+                }
+            }
+            return state
+        })
+        return Promise.resolve()
+    }
+}
