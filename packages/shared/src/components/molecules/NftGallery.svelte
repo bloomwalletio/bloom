b2191626bb306784bd284c41fe0681a72a698871
Jean Ribeiro <contact@jeanribeiro.dev>
2023-10-26 10:36:40 -0300

    fix: cut off shadows

--- packages/shared/src/components/molecules/NftGallery.svelte
+++ packages/shared/src/components/molecules/NftGallery.svelte
@@ -6,7 +6,10 @@
 </script>
 
 <div
-    class="grid overflow-scroll sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 2xl:gap-4 pb-1 pr-3 -mr-5"
+    class="overflow-scroll grid
+    sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5
+    gap-3 2xl:gap-4
+    pb-6 -mb-5 pl-4 -ml-4 pr-3 -mr-5"
 >
     {#each nfts as nft}
         <NftGalleryItem {nft} />
