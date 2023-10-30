bc063a782c96703157f896abf91d9c1cbeccd8e9
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-10-30 03:14:40 -0500

    enhancement: rebrand Ledger connection guide (#1237)
    * Get majority of rebranding done
    
    * Get device illustrations styled
    
    * Add new illustration
    
    * Make adjustments per PR review
    
    ---------
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    Co-authored-by: Tuditi <daviddetroch@pm.me>

---
+++ packages/desktop/views/onboarding/views/create-from-ledger/views/ledger-connection-guide/components/index.ts
@@ -0,0 +1,4 @@
+export { default as LedgerAppOpen } from './LedgerAppOpen.svelte'
+export { default as LedgerDeviceUnlocked } from './LedgerDeviceUnlocked.svelte'
+export { default as LedgerLiveNotOpen } from './LedgerLiveNotOpen.svelte'
+export { default as LedgerStillNotConnected } from './LedgerStillNotConnected.svelte'
