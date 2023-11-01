2d1dc2e4c2731adcb754154b6a3c3cdd39ce86b2
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-11-01 12:48:13 -0500

    Rebrand tile

--- packages/desktop/components/ContactCard.svelte
+++ packages/desktop/components/ContactCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import { Icon, IconName, Text } from '@bloomwalletio/ui'
-    import { ClickableTile, ContactAvatar } from '@ui'
+    import { Icon, IconName, Text, Tile } from '@bloomwalletio/ui'
+    import { ContactAvatar } from '@ui'
     import { UiEventFunction } from '@core/utils'
     import { IContact } from '@core/contact'
 
@@ -8,16 +8,14 @@
     export let onCardClick: UiEventFunction
 </script>
 
-<ClickableTile
-    classes="flex justify-between gap-2 items-center bg-white border border-solid border-gray-200 dark:border-transparent"
-    onClick={onCardClick}
->
-    <ContactAvatar {contact} />
-    <div class="flex w-full text-left overflow-hidden">
-        <Text type="base" truncate>
-            {contact.name}
-        </Text>
+<Tile border onClick={onCardClick}>
+    <div class="flex w-full justify-between gap-2 items-center">
+        <ContactAvatar {contact} />
+        <div class="flex w-full text-left overflow-hidden">
+            <Text type="base" truncate>
+                {contact.name}
+            </Text>
+        </div>
+        <Icon name={IconName.ChevronRight} textColor="secondary" />
     </div>
-
-    <Icon name={IconName.ChevronRight} textColor="secondary" />
-</ClickableTile>
+</Tile>
