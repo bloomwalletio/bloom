8c0f75d9f786a43957255b50839b7180ae9b0629
Mark Nardi <nardimark7@gmail.com>
2023-11-02 12:20:20 +0100

    add details drawer

---
+++ packages/desktop/views/dashboard/drawers/dapp-config/views/DappDetailsDrawer.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import { selectedDapp } from '@auxiliary/wallet-connect/stores'
+    import { DrawerTemplate } from '@components/drawers'
+    import { Link, Table, Text } from '@bloomwalletio/ui'
+    import { Router } from '@core/router'
+    import { onMount } from 'svelte'
+    import { DappConfigRoute } from '../dapp-config-route.enum'
+    import { localize } from '@core/i18n'
+    import { openUrlInBrowser } from '@core/app/utils'
+    import { DappActionsMenu } from '@components/menus'
+
+    export let drawerRouter: Router<unknown>
+
+    const localeKey = 'views.dashboard.drawers.dapps.details'
+
+    onMount(() => {
+        if (!$selectedDapp) {
+            drawerRouter.goTo(DappConfigRoute.ConnectedDapps)
+        }
+    })
+</script>
+
+<DrawerTemplate {drawerRouter}>
+    <div slot="header" class="flex flex-row items-center w-full justify-between">
+        <Text type="h6">{localize(`${localeKey}.title`)}</Text>
+        <DappActionsMenu {drawerRouter} dapp={$selectedDapp} />
+    </div>
+    <div class="w-full h-full">
+        <section>
+            <div class="flex flex-row gap-3">
+                <img class="dapp-icon" src={$selectedDapp.metadata.icons[0]} alt={$selectedDapp.metadata.name} />
+                <div class="flex flex-col items-start">
+                    <Text type="body1" fontWeight="bold">{$selectedDapp.metadata.name}</Text>
+                    <Link
+                        text={$selectedDapp.metadata.url}
+                        on:click={() => openUrlInBrowser($selectedDapp.metadata.url)}
+                    />
+                </div>
+            </div>
+            {#if $selectedDapp.metadata}
+                <Table
+                    orientation="vertical"
+                    items={[
+                        {
+                            key: localize(`${localeKey}.description`),
+                            value: $selectedDapp.metadata.description ?? undefined,
+                        },
+                        {
+                            key: localize(`${localeKey}.verifyUrl`), // TODO: instead of showing this url, show a boolean if it is verified or not
+                            value: $selectedDapp.metadata.verifyUrl ?? undefined,
+                            copyable: true,
+                        },
+                    ]}
+                />
+            {/if}
+        </section>
+    </div>
+</DrawerTemplate>
+
+<style lang="scss">
+    section {
+        @apply border border-solid border-stroke dark:border-stroke-dark;
+        @apply rounded-xl;
+        @apply flex flex-col gap-4 p-6;
+    }
+
+    .dapp-icon {
+        @apply w-10 h-10 rounded-full;
+    }
+</style>
--- packages/desktop/views/dashboard/drawers/dapp-config/views/index.ts
+++ packages/desktop/views/dashboard/drawers/dapp-config/views/index.ts
@@ -1,3 +1,4 @@
 export { default as ConfirmConnectionDrawer } from './ConfirmConnectionDrawer.svelte'
 export { default as ConnectedDappsDrawer } from './ConnectedDappsDrawer.svelte'
+export { default as DappDetailsDrawer } from './DappDetailsDrawer.svelte'
 export { default as InputConnectionCodeDrawer } from './InputConnectionCodeDrawer.svelte'
