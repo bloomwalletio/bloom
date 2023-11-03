aed5adcfa3fe798b5a35d0b44af3c817bc0ab379
Tuditi <daviddetroch@pm.me>
2023-11-03 07:47:46 +0100

    feat: add error handling

--- packages/shared/src/lib/core/layer-2/actions/setGasFee.ts
+++ packages/shared/src/lib/core/layer-2/actions/setGasFee.ts
@@ -1,4 +1,5 @@
 import { IAccountState } from '@core/account/interfaces'
+import { handleError } from '@core/error/handlers'
 import { getGasFeeForLayer1ToLayer2Transaction } from '@core/layer-2/actions'
 import { calculateMaxGasFeeFromTransactionData } from '@core/layer-2/utils'
 import { isEvmChain, getNetwork } from '@core/network'
@@ -10,34 +11,38 @@ import {
 } from '@core/wallet'
 
 export async function setGasFee(sendFlowParams: SendFlowParameters, account: IAccountState): Promise<void> {
-    if (sendFlowParams.gasFee) {
-        return
-    }
-
-    const sourceNetworkId = getNetworkIdFromSendFlowParameters(sendFlowParams)
-    if (!sourceNetworkId || !sendFlowParams.destinationNetworkId) {
-        throw new Error('Networks are not set in send flow parameters!')
-    }
-
-    let gasFee: number | undefined
-    if (isEvmChain(sourceNetworkId)) {
-        const chain = getNetwork()?.getChain(sourceNetworkId)
-        if (!chain) {
-            throw new Error('Chain is undefined!')
+    try {
+        if (sendFlowParams.gasFee) {
+            return
         }
 
-        try {
-            const txData = await createEvmTransactionFromSendFlowParameters(sendFlowParams, chain, account)
-            gasFee = txData ? Number(calculateMaxGasFeeFromTransactionData(txData)) : undefined
-        } catch (error) {
-            console.error(error)
+        const sourceNetworkId = getNetworkIdFromSendFlowParameters(sendFlowParams)
+        if (!sourceNetworkId || !sendFlowParams.destinationNetworkId) {
+            throw new Error('Networks are not set in send flow parameters!')
         }
-    } else if (isEvmChain(sendFlowParams.destinationNetworkId)) {
-        gasFee = await getGasFeeForLayer1ToLayer2Transaction(sendFlowParams)
-    }
 
-    updateSendFlowParameters({
-        type: sendFlowParams.type,
-        gasFee,
-    })
+        let gasFee: number | undefined
+        if (isEvmChain(sourceNetworkId)) {
+            const chain = getNetwork()?.getChain(sourceNetworkId)
+            if (!chain) {
+                throw new Error('Chain is undefined!')
+            }
+
+            try {
+                const txData = await createEvmTransactionFromSendFlowParameters(sendFlowParams, chain, account)
+                gasFee = txData ? Number(calculateMaxGasFeeFromTransactionData(txData)) : undefined
+            } catch (error) {
+                console.error(error)
+            }
+        } else if (isEvmChain(sendFlowParams.destinationNetworkId)) {
+            gasFee = await getGasFeeForLayer1ToLayer2Transaction(sendFlowParams)
+        }
+
+        updateSendFlowParameters({
+            type: sendFlowParams.type,
+            gasFee,
+        })
+    } catch (err) {
+        handleError(err)
+    }
 }
