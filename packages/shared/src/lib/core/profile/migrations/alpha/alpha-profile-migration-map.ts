1a2b94479e41d1e242070e4411f0f6d8d157b451
Tuditi <daviddetroch@pm.me>
2023-11-03 11:37:50 +0100

    fix: add profile migration

--- packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-map.ts
+++ packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-map.ts
@@ -1,8 +1,10 @@
 import { ProfileMigrationMap } from '../../types'
 import { alphaProfileMigration0To1 } from './alpha-profile-migration-0-to-1'
 import { alphaProfileMigration1To2 } from './alpha-profile-migration-1-to-2'
+import { alphaProfileMigration2To3 } from './alpha-profile-migration-2-to-3'
 
 export const ALPHA_PROFILE_MIGRATION_MAP: ProfileMigrationMap = {
     0: alphaProfileMigration0To1,
     1: alphaProfileMigration1To2,
+    2: alphaProfileMigration2To3,
 }
