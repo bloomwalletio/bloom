d3169f8c2242c1d08bb47fd871232d41847b4000
Mark Nardi <nardimark7@gmail.com>
2023-11-03 12:03:08 +0100

    rename Layer2 to L2

--- packages/shared/src/lib/core/layer-2/utils/fetchL2BalanceForAllAccounts.ts
+++ packages/shared/src/lib/core/layer-2/utils/fetchL2BalanceForAllAccounts.ts
@@ -1,11 +1,11 @@
 import { get } from 'svelte/store'
 import { activeAccounts } from '@core/profile/stores'
-import { fetchLayer2BalanceForAccount } from '../actions'
+import { fetchL2BalanceForAccount } from '../actions'
 
 export function fetchL2BalanceForAllAccounts(): void {
     for (const account of get(activeAccounts)) {
         try {
-            fetchLayer2BalanceForAccount(account)
+            fetchL2BalanceForAccount(account)
         } catch (err) {
             console.error(err)
         }
