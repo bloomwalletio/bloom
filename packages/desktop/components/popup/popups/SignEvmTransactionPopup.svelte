2e25f680ae1ccd22ffd6c12b48c3f51301716bd9
Tuditi <daviddetroch@pm.me>
2023-11-20 14:30:11 +0100

    feat: add signEvmTransactionPopup

--- packages/desktop/components/popup/popups/SignEvmTransactionPopup.svelte
+++ packages/desktop/components/popup/popups/SignEvmTransactionPopup.svelte
@@ -2,23 +2,30 @@
     import { localize } from '@core/i18n'
     import { closePopup } from '@desktop/auxiliary/popup'
     import { handleError } from '@core/error/handlers'
-    import { truncateString } from '@core/utils'
     import { IConnectedDapp } from '@auxiliary/wallet-connect/interface'
     import { CallbackParameters } from '@auxiliary/wallet-connect/types'
     import { signEvmTransaction } from '@core/wallet/actions'
-    import { Alert, Text } from '@bloomwalletio/ui'
+    import { Text } from '@bloomwalletio/ui'
     import { IAccountState } from '@core/account'
     import { selectedAccount } from '@core/account/stores'
     import { IChain } from '@core/network'
-    import { AccountLabel, TransactionAssetSection } from '@ui'
+    import { TransactionAssetSection } from '@ui'
     import { checkActiveProfileAuth } from '@core/profile/actions'
     import { LedgerAppName } from '@core/ledger'
     import PopupTemplate from '../PopupTemplate.svelte'
     import { showNotification } from '@auxiliary/notification/actions'
     import { EvmTransactionData } from '@core/layer-2/types'
     import { EvmTransactionDetails } from '@views/dashboard/send-flow/views/components'
-    import { calculateEstimatedGasFeeFromTransactionData, calculateMaxGasFeeFromTransactionData } from '@core/layer-2'
-    import { selectedAccountTokens } from '@core/token/stores'
+    import {
+        AssetType,
+        calculateEstimatedGasFeeFromTransactionData,
+        calculateMaxGasFeeFromTransactionData,
+    } from '@core/layer-2'
+    import { getTokenFromSelectedAccountTokens } from '@core/token/stores'
+    import { getTransferInfoFromTransactionData } from '@core/layer-2/utils/getTransferInfoFromTransactionData'
+    import { TokenTransferData } from '@core/wallet'
+    import { INft } from '@core/nfts'
+    import { getNftByIdFromAllAccountNfts } from '@core/nfts/actions'
 
     export let transaction: EvmTransactionData
     export let account: IAccountState
@@ -26,20 +33,61 @@
     export let dapp: IConnectedDapp | undefined
     export let callback: (params: CallbackParameters) => void
 
-    // console.log(1)
-    const { chainId, id, coinType } = chain.getConfiguration()
-    const baseCoin = $selectedAccountTokens?.[id].baseCoin
-    const baseCoinTransfer = {
-        rawAmount: transaction.value.toString(),
-        token: baseCoin,
-    }
-
-    $: address = truncateString(account.evmAddresses[coinType] ?? '', 8, 8)
+    const { chainId, id } = chain.getConfiguration()
 
     let isBusy = false
 
+    let nft: INft | undefined
+    let tokenTransfer: TokenTransferData | undefined
+    let baseCoinTransfer: TokenTransferData | undefined
+
+    setTokenTransfer()
+    function setTokenTransfer(): void {
+        const transactionInfo = getTransferInfoFromTransactionData(transaction, transaction.to as string, chain)
+        switch (transactionInfo?.asset.type) {
+            case AssetType.BaseCoin: {
+                const token = getTokenFromSelectedAccountTokens('0x0', id)
+                baseCoinTransfer = {
+                    rawAmount: transactionInfo?.asset?.rawAmount,
+                    token,
+                }
+                break
+            }
+            case AssetType.Token: {
+                const token = getTokenFromSelectedAccountTokens(
+                    '0x080ad771d46c28f267403a4af352ab2e278e5f1fe9b7ab5579106e98f826bc14640100000000',
+                    id
+                )
+                tokenTransfer = {
+                    rawAmount: transactionInfo?.asset?.rawAmount,
+                    token,
+                }
+                break
+            }
+            case AssetType.Nft: {
+                nft = getNftByIdFromAllAccountNfts($selectedAccount.index, transactionInfo.asset.nftId)
+                break
+            }
+            default: {
+                const token = getTokenFromSelectedAccountTokens(
+                    '0x080ad771d46c28f267403a4af352ab2e278e5f1fe9b7ab5579106e98f826bc14640100000000',
+                    id
+                )
+                tokenTransfer = {
+                    rawAmount: '12',
+                    token,
+                }
+            }
+        }
+    }
+
     async function onConfirmClick(): Promise<void> {
         await checkActiveProfileAuth(sign, { stronghold: false, ledger: false }, LedgerAppName.Ethereum)
+    }
+
+    function onCancelClick(): void {
+        callback({ error: 'User rejected' })
+        closePopup()
     }
 
     async function sign(): Promise<void> {
@@ -49,7 +97,7 @@
 
             showNotification({
                 variant: 'success',
-                text: localize('notifications.signMessage.success'),
+                text: localize('notifications.signTransaction.success'),
             })
             callback({ result })
         } catch (err) {
@@ -60,37 +108,23 @@
             closePopup()
         }
     }
-
-    function onCancelClick(): void {
-        callback({ error: 'User rejected' })
-        closePopup()
-    }
 </script>
 
 <PopupTemplate
-    title={localize('popups.signMessage.title')}
+    title={localize('popups.signTransaction.title')}
     backButton={{
         text: localize('actions.cancel'),
         onClick: onCancelClick,
     }}
     continueButton={{
-        text: localize('popups.signMessage.action'),
+        text: localize('popups.signTransaction.action'),
         onClick: onConfirmClick,
     }}
     busy={$selectedAccount?.isTransferring || isBusy}
 >
     <div class="space-y-5">
         <section class="relative flex flex-col border border-solid border-gray-200 rounded-xl p-6">
-            <Text textColor="secondary">{localize('general.message')}</Text>
-
-            <!-- <TransactionAssetSection {baseCoinTransfer} {...transactionAsset} /> -->
-            <TransactionAssetSection {baseCoinTransfer} />
-
-            <EvmTransactionDetails
-                destinationNetworkId={id}
-                estimatedGasFee={calculateEstimatedGasFeeFromTransactionData(transaction)}
-                maxGasFee={calculateMaxGasFeeFromTransactionData(transaction)}
-            />
+            <TransactionAssetSection {baseCoinTransfer} {tokenTransfer} {nft} />
             {#if dapp}
                 <div class="absolute flex flex-row justify-between" style="top: -12px; left: 18px;">
                     <div class="flex flex-row gap-1 bg-white dark:bg-gray-800 items-center px-2">
@@ -106,19 +140,10 @@
                 </div>
             {/if}
         </section>
-        <section class="flex flex-row justify-between items-center border border-solid border-gray-200 rounded-xl p-4">
-            <AccountLabel {account} />
-            <Text textColor="secondary">
-                {address}
-            </Text>
-        </section>
-        {#if dapp}
-            <Alert
-                variant="info"
-                text={localize('popups.signMessage.hint', { dappName: dapp.metadata?.name ?? 'Unkown' })}
-            />
-        {:else}
-            <Alert variant="warning" text={localize('popups.signMessage.warning')} />
-        {/if}
+        <EvmTransactionDetails
+            destinationNetworkId={id}
+            estimatedGasFee={calculateEstimatedGasFeeFromTransactionData(transaction)}
+            maxGasFee={calculateMaxGasFeeFromTransactionData(transaction)}
+        />
     </div>
 </PopupTemplate>
