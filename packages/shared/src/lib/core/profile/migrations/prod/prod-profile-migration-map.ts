8ae7c13fa01dc2392cb97c159d0714cb5b01da88
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-11-22 15:40:20 -0600

    fix: adjust logic for manually untracking tokens (#1454)
    * Add logic to avoid automatically tracking manually untracked tokens
    
    * Fix data items error
    
    * Fix lowercase issue
    
    * Fix store type for untracked tokens
    
    * Change data structure for tracked tokens
    
    * Hide activities rather than not generating them
    
    * Add prod migration
    
    * Change conditional logic
    
    * fix: track untracked logic
    
    * feat: add force sync button
    
    * Remove unneeded code
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>

--- packages/shared/src/lib/core/profile/migrations/prod/prod-profile-migration-map.ts
+++ packages/shared/src/lib/core/profile/migrations/prod/prod-profile-migration-map.ts
@@ -1,3 +1,7 @@
 import { ProfileMigrationMap } from '../../types'
 
-export const PROD_PROFILE_MIGRATION_MAP: ProfileMigrationMap = {}
+import { prodProfileMigration0To1 } from './prod-profile-migration-0-to-1'
+
+export const PROD_PROFILE_MIGRATION_MAP: ProfileMigrationMap = {
+    0: prodProfileMigration0To1,
+}
