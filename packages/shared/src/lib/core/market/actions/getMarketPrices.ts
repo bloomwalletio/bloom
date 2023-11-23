952a61d71425904a7e752ae2fe69e8d2b895f626
Nicole O'Brien <nicole@bloomwallet.io>
2023-11-23 18:16:10 +0000

    temp: initial coin gecko api class
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

--- packages/shared/src/lib/core/market/actions/getMarketPrices.ts
+++ packages/shared/src/lib/core/market/actions/getMarketPrices.ts
@@ -22,5 +22,60 @@ export async function getMarketCoinPrices(
         return <MarketCoinPrices>data
     } catch (err) {
         console.error(err)
+        throw err
+    }
+}
+
+function buildQueryParametersFromObject(obj: Record<string, string | number | boolean>): string {
+    return Object.keys(obj)
+        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+        .join('&')
+}
+
+enum CoinGeckoApiEndpoint {
+    SIMPLE_PRICE = 'simple/price',
+    COINS_LIST = 'coins/list',
+    COIN_DETAILS = 'coins',
+}
+export class CoinGeckoApi {
+    static async makeRequest<T>(endpoint: CoinGeckoApiEndpoint, queryParams?: string): Promise<T> {
+        try {
+            const response = await fetch(
+                `${MARKET_API_BASE_URL}/${endpoint}?${queryParams ?? ''}`,
+                DEFAULT_APPLICATION_JSON_REQUEST_OPTIONS
+            )
+            const data = await response.json()
+            return <T>data
+        } catch (err) {
+            console.error(err)
+            throw err
+        }
+    }
+
+    static async getMarketCoinPrices(ids: MarketCoinId[], vsCurrencies: MarketCurrency[]): Promise<MarketCoinPrices> {
+        const queryParams = buildQueryParametersFromObject({
+            ids: ids.join(','),
+            vs_currencies: vsCurrencies.join(','),
+        })
+        return this.makeRequest<MarketCoinPrices>(CoinGeckoApiEndpoint.SIMPLE_PRICE, queryParams)
+    }
+
+    static async getCoinsList(include_platform: boolean): Promise<Record<string, unknown>[]> {
+        const queryParams = buildQueryParametersFromObject({
+            include_platform,
+        })
+        return this.makeRequest<Record<string, unknown>[]>(CoinGeckoApiEndpoint.COINS_LIST, queryParams)
+    }
+
+    static async getFilteredCoinsList(): Promise<Record<string, unknown>[]> {
+        const coinsList = await this.getCoinsList(true)
+        return coinsList.filter((coin) => Object.keys(coin.platforms ?? {}).includes('shimmer_evm'))
+    }
+
+    static async getCoinDetails(id: string): Promise<Record<string, unknown>> {
+        return this.makeRequest<Record<string, unknown>>(
+            CoinGeckoApiEndpoint.COIN_DETAILS,
+            buildQueryParametersFromObject({ id })
+        )
     }
 }
