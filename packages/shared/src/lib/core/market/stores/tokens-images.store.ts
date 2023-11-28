23a6d65e35ab485fcd8b2bb37061b3128b53b28a
Jean Ribeiro <contact@jeanribeiro.dev>
2023-11-28 18:24:14 -0300

    fix tokensImages key

--- packages/shared/src/lib/core/market/stores/tokens-images.store.ts
+++ packages/shared/src/lib/core/market/stores/tokens-images.store.ts
@@ -1,6 +1,7 @@
 import { derived, Readable } from 'svelte/store'
 import { CoinGeckoCoinImage } from '../interfaces'
 import { coinGeckoTokensMetadata } from './coingecko-tokens-metadata'
+import { CoinGeckoNetworkId } from '../enums'
 
 export const tokensImages: Readable<Record<string, CoinGeckoCoinImage>> = derived(
     [coinGeckoTokensMetadata],
@@ -8,7 +9,7 @@ export const tokensImages: Readable<Record<string, CoinGeckoCoinImage>> = derive
         return Object.values($coinGeckoTokensMetadata).reduce(
             (images, token) => {
                 if (token?.image) {
-                    return { ...images, [token.id]: token.image }
+                    return { ...images, [token.platforms[CoinGeckoNetworkId.ShimmerEVM]]: token.image }
                 }
                 return images
             },
