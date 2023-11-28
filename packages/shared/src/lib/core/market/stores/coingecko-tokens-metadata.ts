10fe4461cc74d5d70d366cf869cb487e17e90e7d
Nicole O'Brien <nicole@bloomwallet.io>
2023-11-28 23:21:44 +0000

    fix: type coinGeckoTokensMetadata

--- packages/shared/src/lib/core/market/stores/coingecko-tokens-metadata.ts
+++ packages/shared/src/lib/core/market/stores/coingecko-tokens-metadata.ts
@@ -3,7 +3,7 @@ import { Writable } from 'svelte/store'
 import { CoinGeckoCoin } from '../interfaces'
 import { NetworkId } from '@core/network'
 
-export const coinGeckoTokensMetadata: Writable<Record<NetworkId, Record<string, CoinGeckoCoin>>> = persistent(
+export const coinGeckoTokensMetadata: Writable<{ [key in NetworkId]?: Record<string, CoinGeckoCoin> }> = persistent(
     'coinGeckoTokensMetadata',
     {}
 )
