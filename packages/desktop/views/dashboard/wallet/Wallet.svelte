d9d1528c2bd9e28404eab3b971bc42a79d0b221c
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-11-28 14:21:28 -0600

    Remove test code in Wallet component

--- packages/desktop/views/dashboard/wallet/Wallet.svelte
+++ packages/desktop/views/dashboard/wallet/Wallet.svelte
@@ -1,34 +1,8 @@
 <script lang="ts">
-    import { onMount } from 'svelte'
     import { Pane } from '@ui'
     import { selectedAccount } from '@core/account/stores'
-    import { ETHEREUM_COIN_TYPE, EvmExplorerApi, getNetwork } from '@core/network'
-    import { TokenStandard } from '@core/token'
     import { AccountSummaryPane } from './panes'
     import TabSection from './tab-section/TabSection.svelte'
-
-    async function helper(): Promise<void> {
-        try {
-            const address = $selectedAccount.evmAddresses[ETHEREUM_COIN_TYPE]
-            const networkId = getNetwork().getChains()[0].getConfiguration().id
-            const explorerApi = new EvmExplorerApi(networkId)
-            const addressAssets = await explorerApi.getAssetsForAddress(address, TokenStandard.Erc20)
-            /* eslint-disable no-console */
-            console.log('result: ', addressAssets)
-            addressAssets.forEach(async (asset) => {
-                const metadata = await explorerApi.getAssetMetadata(asset.token.address)
-                /* eslint-disable no-console */
-                console.log('metadata: ', metadata)
-            })
-        } catch (err) {
-            /* eslint-disable no-console */
-            console.log(err)
-        }
-    }
-
-    onMount(() => {
-        void helper()
-    })
 </script>
 
 {#if $selectedAccount}
