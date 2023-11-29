fe32aca62708ffbddf764f45c96bf386cade0133
Jean Ribeiro <contact@jeanribeiro.dev>
2023-11-28 21:01:50 -0300

    feat: include shimmerEvm tokens in getAndUpdateMarketPrices

--- packages/shared/src/lib/core/market/actions/getAndUpdateMarketPrices.ts
+++ packages/shared/src/lib/core/market/actions/getAndUpdateMarketPrices.ts
@@ -1,11 +1,18 @@
+import { get } from 'svelte/store'
 import { CoinGeckoApi } from '../apis'
 import { MarketCoinId, MarketCurrency } from '../enums'
-import { updateMarketCoinPrices } from '../stores'
+import { coinGeckoTokensMetadata, updateMarketCoinPrices } from '../stores'
+import { EvmChainId, NetworkId, NetworkNamespace } from '@core/network'
 
 export async function getAndUpdateMarketPrices(): Promise<void> {
     try {
+        const shimmerEvmNetworkId: NetworkId = `${NetworkNamespace.Evm}:${EvmChainId.ShimmerEvm}`
+        const shimmerEvmTokens = get(coinGeckoTokensMetadata)?.[shimmerEvmNetworkId] ?? {}
+        const shimmerEvmTokensAddresses = Object.keys(shimmerEvmTokens ?? {})
+        const shimmerEvmTokensIds =
+            shimmerEvmTokensAddresses.map((tokenAddress) => shimmerEvmTokens[tokenAddress].id) ?? []
         const marketPricesResponse = await CoinGeckoApi.getSimplePrices(
-            [MarketCoinId.Shimmer],
+            [MarketCoinId.Shimmer, ...shimmerEvmTokensIds],
             Object.values(MarketCurrency)
         )
         updateMarketCoinPrices(marketPricesResponse)
