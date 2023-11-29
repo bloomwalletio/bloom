3812ea1ee64b9abcdb0a3f8402bd1fbd548a3a6c
Nicole O'Brien <nicole@bloomwallet.io>
2023-11-29 23:15:27 +0000

    add explorer url

--- packages/shared/src/lib/core/network/constants/explorer-endpoints.constant.ts
+++ packages/shared/src/lib/core/network/constants/explorer-endpoints.constant.ts
@@ -2,6 +2,11 @@ import { ExplorerEndpoint, SupportedNetworkId } from '../enums'
 import { NetworkId } from '../types'
 
 export const EXPLORER_ENDPOINTS: Readonly<{ [key in NetworkId]?: { [key in ExplorerEndpoint]?: string } }> = {
+    [SupportedNetworkId.Iota]: {
+        [ExplorerEndpoint.Transaction]: ExplorerEndpoint.Transaction,
+        [ExplorerEndpoint.Nft]: ExplorerEndpoint.Nft,
+        [ExplorerEndpoint.Address]: 'addr',
+    },
     [SupportedNetworkId.Shimmer]: {
         [ExplorerEndpoint.Transaction]: ExplorerEndpoint.Transaction,
         [ExplorerEndpoint.Nft]: ExplorerEndpoint.Nft,
