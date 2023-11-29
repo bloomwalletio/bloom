f138ac5fa1ab2c80e7e90b3ec96e8b1c33b7e50c
Nicole O'Brien <nicole@bloomwallet.io>
2023-11-29 12:19:43 +0000

    refactor: cleanup getAndUpdateMarketPrices

--- packages/shared/src/lib/core/market/actions/getAndUpdateMarketPrices.ts
+++ packages/shared/src/lib/core/market/actions/getAndUpdateMarketPrices.ts
@@ -2,15 +2,12 @@ import { get } from 'svelte/store'
 import { CoinGeckoApi } from '../apis'
 import { MarketCoinId, MarketCurrency } from '../enums'
 import { coinGeckoTokensMetadata, updateMarketCoinPrices } from '../stores'
-import { EvmChainId, NetworkId, NetworkNamespace } from '@core/network'
+import { SupportedNetworkId } from '@core/network'
 
 export async function getAndUpdateMarketPrices(): Promise<void> {
     try {
-        const shimmerEvmNetworkId: NetworkId = `${NetworkNamespace.Evm}:${EvmChainId.ShimmerEvm}`
-        const shimmerEvmTokens = get(coinGeckoTokensMetadata)?.[shimmerEvmNetworkId] ?? {}
-        const shimmerEvmTokensAddresses = Object.keys(shimmerEvmTokens ?? {})
-        const shimmerEvmTokensIds =
-            shimmerEvmTokensAddresses.map((tokenAddress) => shimmerEvmTokens[tokenAddress].id) ?? []
+        const shimmerEvmTokens = Object.values(get(coinGeckoTokensMetadata)?.[SupportedNetworkId.ShimmerEvm] ?? {})
+        const shimmerEvmTokensIds = shimmerEvmTokens.map((token) => token.id)
         const marketPricesResponse = await CoinGeckoApi.getSimplePrices(
             [MarketCoinId.Shimmer, ...shimmerEvmTokensIds],
             Object.values(MarketCurrency)
