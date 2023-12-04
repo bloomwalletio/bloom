d33d4730523c676915c4f0ea60ab29271d97a01f
Mark Nardi <nardimark7@gmail.com>
2023-12-04 12:14:49 +0100

    feat: reject session proposal on closing the drawer (#1525)
    * update drawer props
    
    * reject session proposal
    
    * unselect dapp on close
    
    * fix closing on dapps details view
    
    * fix on back
    
    * fix on backs
    
    * cleanup
    
    * add timeout if no proposal arrives
    
    * Add onDestroy hook to clear timeout
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/desktop/views/dashboard/drawers/DashboardDrawerRouterView.svelte
+++ packages/desktop/views/dashboard/drawers/DashboardDrawerRouterView.svelte
@@ -5,7 +5,7 @@
     import { DashboardDrawerRoute } from './'
 </script>
 
-<Drawer>
+<Drawer onClose={$drawerState?.props?.onClose}>
     <div slot="contents" class="flex flex-col h-full">
         {#if $drawerState?.route === DrawerRoute.Dashboard}
             {#if $drawerState.id === DashboardDrawerRoute.NetworkConfig}
