8157cc46bcd34e1731f405b300de5738b83e6aa7
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-12-05 08:54:51 -0600

    Move ERC165 ABI

--- packages/shared/src/lib/core/layer-2/abis/erc721.abi.ts
+++ packages/shared/src/lib/core/layer-2/abis/erc721.abi.ts
@@ -1,37 +1,6 @@
 import { AbiType, StateMutabilityType } from '../enums'
 import { Abi } from '../types'
-
-const ERC165_ABI: Abi = [
-    // / @notice Query if a contract implements an interface
-    // / @param interfaceID The interface identifier, as specified in ERC-165
-    // / @dev Interface identification is specified in ERC-165. This function
-    // /  uses less than 30,000 gas.
-    // / @return `true` if the contract implements `interfaceID` and
-    // /  `interfaceID` is not 0xffffffff, `false` otherwise
-    // /
-    // / function supportsInterface(bytes4 interfaceID) external view returns(bool);
-    {
-        type: AbiType.Function,
-        name: 'supportsInterface',
-        inputs: [
-            {
-                internalType: 'bytes4',
-                name: 'interfaceId',
-                type: 'bytes4',
-            },
-        ],
-        outputs: [
-            {
-                internalType: 'bool',
-                name: '',
-                type: 'bool',
-            },
-        ],
-        constant: true,
-        payable: false,
-        stateMutability: StateMutabilityType.View,
-    },
-]
+import { ERC165_ABI } from './erc165.abi'
 
 const ERC721_BASE_ABI: Abi = [
     {
