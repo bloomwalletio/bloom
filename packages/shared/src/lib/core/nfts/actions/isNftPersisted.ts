22bde8406c10ed334234f9fec7ded50fe39dffd7
Tuditi <daviddetroch@pm.me>
2023-12-06 17:47:52 +0100

    fix: types

--- packages/shared/src/lib/core/nfts/actions/isNftPersisted.ts
+++ packages/shared/src/lib/core/nfts/actions/isNftPersisted.ts
@@ -2,5 +2,5 @@ import { get } from 'svelte/store'
 import { persistedNftForActiveProfile } from '../stores'
 
 export function isNftPersisted(nftId: string, tokenId: string): boolean {
-    return `${nftId}:${tokenId}` in get(persistedNftForActiveProfile) ?? {}
+    return `${nftId}:${tokenId}` in (get(persistedNftForActiveProfile) ?? {})
 }
