7b09a59f3f18c7ccba68efc8b444f5ba05c21f7e
Nicole O'Brien <nicole@bloomwallet.io>
2023-12-07 16:28:28 +0000

    fix: type

--- packages/shared/src/lib/core/layer-2/constants/base-token-contract-address.constant.ts
+++ packages/shared/src/lib/core/layer-2/constants/base-token-contract-address.constant.ts
@@ -1,6 +1,6 @@
 import { SupportedNetworkId } from '@core/network'
 
-export const BASE_TOKEN_CONTRACT_ADDRESS: Partial<Record<SupportedNetworkId, string>> = {
+export const BASE_TOKEN_CONTRACT_ADDRESS: { [id in SupportedNetworkId]?: string } = {
     [SupportedNetworkId.ShimmerEvm]: '0x1074010000000000000000000000000000000000',
     [SupportedNetworkId.TestnetEvm]: '0x1074010000000000000000000000000000000000',
 }
