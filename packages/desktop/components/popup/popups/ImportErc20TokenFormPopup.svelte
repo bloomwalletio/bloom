4c211150104a6c126f7be1b30813328905350008
Tuditi <daviddetroch@pm.me>
2023-12-13 15:23:51 +0100

    chore: remove duplicated function

--- packages/desktop/components/popup/popups/ImportErc20TokenFormPopup.svelte
+++ packages/desktop/components/popup/popups/ImportErc20TokenFormPopup.svelte
@@ -2,15 +2,15 @@
     import { TextInput } from '@bloomwalletio/ui'
     import { NetworkInput } from '@ui'
     import { localize } from '@core/i18n'
-    import { ETHEREUM_ADDRESS_LENGTH } from '@core/layer-2'
     import { getEvmTokenMetadata } from '@core/layer-2/utils'
     import { NetworkId } from '@core/network'
-    import { HEX_PREFIX, HEXADECIMAL_REGEXP } from '@core/utils'
+    import { validateEthereumAddress } from '@core/utils'
     import PopupTemplate from '../PopupTemplate.svelte'
     import { closePopup } from '@desktop/auxiliary/popup'
     import { showNotification } from '@auxiliary/notification'
     import { addNewTrackedTokenToActiveProfile } from '@core/wallet'
     import { IErc20Metadata, TokenTrackingStatus } from '@core/token'
+    import { handleError } from '@core/error/handlers'
 
     let busy = false
 
@@ -27,47 +27,29 @@
     async function onImportClick(): Promise<void> {
         busy = true
         const _tokenAddress = tokenAddress.toLowerCase()
-        if (validate(_tokenAddress)) {
-            try {
-                const erc20TokenMetadata = (await getEvmTokenMetadata(_tokenAddress, networkId)) as IErc20Metadata
-                if (erc20TokenMetadata) {
-                    addNewTrackedTokenToActiveProfile(
-                        networkId,
-                        _tokenAddress,
-                        erc20TokenMetadata,
-                        TokenTrackingStatus.ManuallyTracked
-                    )
-                    showNotification({
-                        variant: 'success',
-                        text: localize('popups.importErc20Token.success', {
-                            values: { tokenSymbol: erc20TokenMetadata.symbol },
-                        }),
-                    })
-                    closePopup()
-                }
-            } catch (err) {
-                console.error(err)
+        try {
+            validateEthereumAddress(_tokenAddress)
+
+            const erc20TokenMetadata = (await getEvmTokenMetadata(_tokenAddress, networkId)) as IErc20Metadata
+            if (erc20TokenMetadata) {
+                addNewTrackedTokenToActiveProfile(
+                    networkId,
+                    _tokenAddress,
+                    erc20TokenMetadata,
+                    TokenTrackingStatus.ManuallyTracked
+                )
                 showNotification({
-                    variant: 'error',
-                    text: localize('popups.importErc20Token.error'),
+                    variant: 'success',
+                    text: localize('popups.importErc20Token.success', {
+                        values: { tokenSymbol: erc20TokenMetadata.symbol },
+                    }),
                 })
+                closePopup()
             }
+        } catch (err) {
+            handleError(err)
         }
-
         busy = false
-    }
-
-    function validate(tokenAddress: string): boolean {
-        const hasHexPrefix = tokenAddress?.startsWith(HEX_PREFIX)
-        const isValidHex = HEXADECIMAL_REGEXP.test(tokenAddress)
-        const addressLength = tokenAddress?.substring(2)?.length
-
-        if (!hasHexPrefix || !isValidHex) {
-            tokenAddressError = localize('error.erc20Token.invalidAddressFormat')
-        } else if (addressLength !== ETHEREUM_ADDRESS_LENGTH) {
-            tokenAddressError = localize('error.erc20Token.invalidAddressLength')
-        }
-        return !tokenAddressError
     }
 </script>
 
