30c19927162ccd64357f10ed0e4837b8b5e6ab72
Nicole O'Brien <nicole@bloomwallet.io>
2023-12-15 15:57:59 +0000

    fix: dapp connection when drawer already open

--- packages/shared/src/lib/auxiliary/deep-link/handlers/dapps/operations/handleDeepLinkAddWCConnectionOperation.ts
+++ packages/shared/src/lib/auxiliary/deep-link/handlers/dapps/operations/handleDeepLinkAddWCConnectionOperation.ts
@@ -1,11 +1,14 @@
-import { rejectSession, validateConnectionCodeUri } from '@auxiliary/wallet-connect/utils'
 import { pairWithNewDapp } from '@auxiliary/wallet-connect/actions'
+import { rejectSession, validateConnectionCodeUri } from '@auxiliary/wallet-connect/utils'
+import { get } from 'svelte/store'
 import { toggleDashboardDrawer } from '../../../../../../../../desktop/lib/auxiliary/drawer'
-import { DashboardDrawerRoute } from '../../../../../../../../desktop/views/dashboard/drawers/dashboard-drawer-route.enum'
+import { dappConfigRouter } from '../../../../../../../../desktop/views/dashboard/drawers'
 import { DappConfigRoute } from '../../../../../../../../desktop/views/dashboard/drawers/dapp-config/dapp-config-route.enum'
+import { DashboardDrawerRoute } from '../../../../../../../../desktop/views/dashboard/drawers/dashboard-drawer-route.enum'
 
 export function handleDeepLinkAddWCConnectionOperation(searchParams: URLSearchParams): void {
     const walletConnectUri = searchParams.get('uri')
+    const $dappConfigRouter = get(dappConfigRouter)
 
     try {
         if (!walletConnectUri) {
@@ -15,12 +18,16 @@ export function handleDeepLinkAddWCConnectionOperation(searchParams: URLSearchPa
 
         pairWithNewDapp(walletConnectUri)
 
+        $dappConfigRouter?.reset()
+        $dappConfigRouter?.goTo(DappConfigRoute.ConnectionRequest)
         toggleDashboardDrawer({
             id: DashboardDrawerRoute.DappConfig,
             initialSubroute: DappConfigRoute.ConnectionRequest,
             props: { onClose: rejectSession },
         })
     } catch (err) {
+        $dappConfigRouter?.reset()
+        $dappConfigRouter?.goTo(DappConfigRoute.InputCode)
         toggleDashboardDrawer({
             id: DashboardDrawerRoute.DappConfig,
             initialSubroute: DappConfigRoute.InputCode,
