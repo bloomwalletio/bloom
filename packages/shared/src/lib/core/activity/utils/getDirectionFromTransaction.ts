a7edb6f83ab253a95a1a2b7e418a83fddd03b12f
Jean Ribeiro <contact@jeanribeiro.dev>
2023-12-20 18:21:40 -0300

    fix: set transaction direction to incoming if blockId is genesis

--- packages/shared/src/lib/core/activity/utils/getDirectionFromTransaction.ts
+++ packages/shared/src/lib/core/activity/utils/getDirectionFromTransaction.ts
@@ -1,15 +1,23 @@
-import { CommonOutput } from '@iota/sdk/out/types'
+import { CommonOutput, OutputData } from '@iota/sdk/out/types'
 import { IWrappedOutput } from '@core/wallet/interfaces'
 import { ActivityDirection } from '../enums'
 import { getRecipientAddressFromOutput } from './outputs'
+import { EMPTY_HEX_ID } from '@core/wallet'
 
 export function getDirectionFromTransaction(
     wrappedOutputs: IWrappedOutput[],
     incoming: boolean,
-    accountAddress: string
+    accountAddress: string,
+    inputs: OutputData[]
 ): ActivityDirection {
+    const isGenesis = inputs.some((input) => input.metadata?.blockId === EMPTY_HEX_ID)
+    if (isGenesis) {
+        return ActivityDirection.Incoming
+    }
     const containsOutput = wrappedOutputs.some((outputData) => {
         const recipientAddress = getRecipientAddressFromOutput(outputData.output as CommonOutput)
+
+        outputData.metadata?.blockId
 
         if (incoming) {
             return accountAddress === recipientAddress
