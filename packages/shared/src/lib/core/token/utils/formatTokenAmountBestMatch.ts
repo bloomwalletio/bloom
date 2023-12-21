5dfe459ef1544f5d6da4a4832200c9461829cb48
Nicole O'Brien <nicole@bloomwallet.io>
2023-12-21 15:52:09 +0000

    temp: initial formatTokenAmount refactor

--- packages/shared/src/lib/core/token/utils/formatTokenAmountBestMatch.ts
+++ packages/shared/src/lib/core/token/utils/formatTokenAmountBestMatch.ts
@@ -1,32 +1,70 @@
 import { formatNumber } from '@core/i18n'
-import { getIotaUnit, IOTA_UNIT_MAP } from '@core/utils'
-import { formatTokenAmountDefault } from './formatTokenAmountDefault'
 import { TokenMetadata } from '@core/token/types'
-import { TokenStandard } from '@core/token/enums'
 import { getUnitFromTokenMetadata } from '@core/token/utils'
 
 export function formatTokenAmountBestMatch(
     amount: number,
     tokenMetadata: TokenMetadata,
-    overrideDecimalPlaces?: number,
+    round = true,
     withUnit = true
 ): string {
-    const isBaseToken = tokenMetadata?.standard === TokenStandard.BaseToken
+    const formattedAmount = round ? roundAndFormatAmount(amount) : formatNumber(amount, 0, undefined, undefined, true)
+    const unit = getUnitFromTokenMetadata(tokenMetadata)
 
-    let amountWithoutUnit: string
-    let amountWithUnit: string
+    return withUnit && unit ? formattedAmount + unit  : formattedAmount
+}
 
-    if (isBaseToken && tokenMetadata?.useMetricPrefix) {
-        const metricUnit = getIotaUnit(amount)
-        const maxDecimals = overrideDecimalPlaces ?? IOTA_UNIT_MAP[metricUnit].decimalPlaces
-        const convertedAmount = amount / IOTA_UNIT_MAP[metricUnit].value
-        amountWithoutUnit = formatNumber(convertedAmount, 0, maxDecimals, undefined, true)
-        amountWithUnit = amountWithoutUnit + ' ' + metricUnit + tokenMetadata.unit
-    } else {
-        const unit = getUnitFromTokenMetadata(tokenMetadata)
-        amountWithoutUnit = !isNaN(amount) ? formatTokenAmountDefault(amount, tokenMetadata) : '0'
-        amountWithUnit = amountWithoutUnit + (unit ? ' ' + unit : '')
+export function formatTokenAmount(
+    amount: number,
+    tokenMetadata: TokenMetadata,
+    round = true,
+    withUnit = true
+): string {
+    const formattedAmount = round ? roundAndFormatAmount(amount) : formatNumber(amount, 0, undefined, undefined, true)
+    const unit = getUnitFromTokenMetadata(tokenMetadata)
+
+    return withUnit && unit ? formattedAmount + unit  : formattedAmount
+}
+
+
+export function roundAndFormatAmount(
+    amount: number,
+): string {
+    if (isNaN(amount)) {
+        return formatNumber(0, 0, 0, 0, true)
     }
 
-    return withUnit ? amountWithUnit : amountWithoutUnit
+    const stringAmount = amount.toString()
+    const integer = stringAmount.split('.')[0]
+    const decimals = stringAmount.split('.')[1]
+
+    if (decimals === undefined) {
+        return formatNumber(amount, 0, 0, 0, true)
+    } else if ( amount >= 1) {
+        // truncate decimals so max digits in in whole string is no greater than 12
+        const maxDigitsWithDecimals = 12
+        const integerLength = integer.length
+        const maxDecimals = Math.max(maxDigitsWithDecimals - integerLength, 0)
+
+        return formatNumber(amount, 0, maxDecimals, undefined, true)
+    } else {
+        if (decimals.length > 6) {
+            return '≈0'
+        } else {
+            // round to three significant digits
+            const indexFirstSignificantDigit = decimals.split('').findIndex((digit) => Number(digit) > 0)
+            const indexLastSignificantDigit = indexFirstSignificantDigit + 2
+            const roundedSignificantDigits = Number(decimals.slice(0, indexLastSignificantDigit + 1)).toFixed(indexLastSignificantDigit + 1)
+            const significantDigits = String(roundedSignificantDigits)
+            return getSignificantDigits(decimals)
+        }
+    }
+}
+
+function getSignificantDigits(decimals: string, limit: number = 3): string {
+    const indexFirstSignificantDigit = decimals.split('').findIndex((digit) => Number(digit) > 0)
+    const indexLastSignificantDigit = indexFirstSignificantDigit + 2
+    const roundedSignificantDigits = Number(decimals.slice(0, indexLastSignificantDigit + 1)).toFixed(indexLastSignificantDigit + 1)
+    const significantDigits = String(roundedSignificantDigits)
+    return significantDigits
 }
