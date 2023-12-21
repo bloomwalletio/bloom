086c5fdb63f185d2dbf1aeb5ba42629c1cf0517d
Jean Ribeiro <contact@jeanribeiro.dev>
2023-12-20 22:22:23 -0300

    fix: adds isActiveLedgerProfile condition to ledgerRaceConditionProtectionWrapper

--- packages/shared/src/lib/core/ledger/utils/ledgerRaceConditionProtectionWrapper.ts
+++ packages/shared/src/lib/core/ledger/utils/ledgerRaceConditionProtectionWrapper.ts
@@ -1,17 +1,18 @@
 import { isOnboardingLedgerProfile } from '@contexts/onboarding'
 import { get } from 'svelte/store'
 import { stopPollingLedgerDeviceState, pollLedgerDeviceState } from '../actions'
+import { isActiveLedgerProfile } from '@core/profile/stores'
 
 export async function ledgerRaceConditionProtectionWrapper(_function: () => unknown): Promise<void> {
     try {
-        if (get(isOnboardingLedgerProfile)) {
+        if (get(isOnboardingLedgerProfile) || get(isActiveLedgerProfile)) {
             stopPollingLedgerDeviceState()
         }
         await _function()
     } catch (err) {
         console.error(err)
     } finally {
-        if (get(isOnboardingLedgerProfile)) {
+        if (get(isOnboardingLedgerProfile) || get(isActiveLedgerProfile)) {
             pollLedgerDeviceState()
         }
     }
