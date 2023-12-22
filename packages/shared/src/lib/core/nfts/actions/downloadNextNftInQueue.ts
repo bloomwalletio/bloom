de04784478767e342aa148c90c261c41abb3c10c
Mark Nardi <nardimark7@gmail.com>
2023-12-22 12:10:39 +0100

    improve error handling

--- packages/shared/src/lib/core/nfts/actions/downloadNextNftInQueue.ts
+++ packages/shared/src/lib/core/nfts/actions/downloadNextNftInQueue.ts
@@ -8,21 +8,22 @@ export async function downloadNextNftInQueue(): Promise<void> {
         return
     }
 
+    const { nft, accountIndex } = nextDownload
+    if (!nft.downloadMetadata.downloadUrl || !nft.downloadMetadata.filePath) {
+        removeNftFromDownloadQueue(nft.id)
+        return
+    }
+
     try {
-        const { nft, accountIndex } = nextDownload
         downloadingNftId.set(nft.id)
-        if (nft.downloadMetadata.downloadUrl && nft.downloadMetadata.filePath) {
-            await Platform.downloadNft(
-                nft.downloadMetadata.downloadUrl,
-                nft.downloadMetadata.filePath,
-                nft.id,
-                accountIndex
-            )
-        } else {
-            throw new Error('Missing downloadUrl or filePath for nft')
-        }
+        await Platform.downloadNft(
+            nft.downloadMetadata.downloadUrl,
+            nft.downloadMetadata.filePath,
+            nft.id,
+            accountIndex
+        )
     } catch (err) {
         downloadingNftId.set(undefined)
-        removeNftFromDownloadQueue(get(downloadingNftId))
+        removeNftFromDownloadQueue(nft.id)
     }
 }
