de07f0fd9507ece0629f2a0b9c5b1a60bb9750fd
Nicole O'Brien <nicole@bloomwallet.io>
2024-01-02 13:07:34 +0000

    fix: remove max decimals from raw amount conversion
    Co-authored-by: Tuditi <Tuditi@users.noreply.github.com>

--- packages/shared/src/lib/core/token/utils/convertToRawAmount.ts
+++ packages/shared/src/lib/core/token/utils/convertToRawAmount.ts
@@ -1,7 +1,6 @@
 import { parseCurrency } from '@core/i18n'
 import { TokenStandard } from '@core/token/enums'
 import { TokenMetadata } from '@core/token/types'
-import { MAX_SUPPORTED_DECIMALS } from '@core/wallet/constants'
 
 export function convertToRawAmount(amount: string, tokenMetadata: TokenMetadata, unit?: string): bigint | undefined {
     if (amount) {
@@ -17,30 +16,19 @@ function convertToRawAmountFromMetadata(
     tokenMetadata: TokenMetadata,
     selectedUnit?: string
 ): bigint | undefined {
-    const [min] = bigIntMinAndMax(tokenMetadata.decimals, MAX_SUPPORTED_DECIMALS)
-
     if (tokenMetadata?.standard === TokenStandard.BaseToken) {
         if (!selectedUnit || selectedUnit === tokenMetadata.unit) {
-            return convertFloatToBigInt(amount, min)
+            return convertFloatToBigInt(amount, tokenMetadata.decimals)
         } else if (selectedUnit === tokenMetadata.subunit) {
             return BigInt(amount)
         } else {
             return undefined
         }
     } else if (tokenMetadata?.standard === TokenStandard.Irc30 || tokenMetadata?.standard === TokenStandard.Erc20) {
-        return convertFloatToBigInt(amount, min)
+        return convertFloatToBigInt(amount, tokenMetadata.decimals)
     } else {
         throw new Error('convertToRawAmountFromMetadata: Invalid token standard')
     }
-}
-
-function bigIntMinAndMax(...args: number[]): [min: number, max: number] {
-    return args.reduce(
-        ([min, max], e) => {
-            return [e < min ? e : min, e > max ? e : max]
-        },
-        [args[0], args[0]]
-    )
 }
 
 function convertFloatToBigInt(amount: number, maxDecimals: number): bigint {
