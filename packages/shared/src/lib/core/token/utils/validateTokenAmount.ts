b56209e74559fe92b302eb5ec5acbed7767c6d06
Tuditi <daviddetroch@pm.me>
2024-01-03 16:43:57 +0100

    chore: remove comments

--- packages/shared/src/lib/core/token/utils/validateTokenAmount.ts
+++ packages/shared/src/lib/core/token/utils/validateTokenAmount.ts
@@ -13,19 +13,13 @@ export function validateTokenAmount(
     }
     const amountAsFloat = parseCurrency(String(amount).replace('n', ''))
     const isAmountZeroOrNull = !Number(amountAsFloat)
-    // const requiresRawAmount =
-    // (token.metadata.standard === TokenStandard.BaseToken && unit === token.metadata.subunit) ||
-    //    token.metadata.decimals === 0
     const bigAmount = convertToRawAmount(amount, token.metadata, unit) ?? BigInt(0)
-
     // Zero value transactions can still contain metadata/tags
     let error = ''
     if (allowZeroOrNull && isAmountZeroOrNull) {
         return Promise.resolve(BigInt(0))
     } else if (isAmountZeroOrNull) {
         error = localize('error.send.amountInvalidFormat')
-        // } else if (requiresRawAmount && Number.parseInt(amount, 10).toString() !== amount) {
-        //    error = localize('error.send.amountNoFloat')
     } else if (bigAmount > BigInt(token?.balance?.available ?? 0)) {
         error = localize('error.send.amountTooHigh')
     } else if (bigAmount <= BigInt(0)) {
