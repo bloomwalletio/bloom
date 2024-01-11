884f301429b0afb3414c2f12b059818bf8841062
Mark Nardi <nardimark7@gmail.com>
2024-01-11 11:39:59 +0100

    chore: cleanup signature of isSettingVisible

--- packages/shared/src/lib/contexts/settings/utils/isSettingVisible.ts
+++ packages/shared/src/lib/contexts/settings/utils/isSettingVisible.ts
@@ -4,12 +4,11 @@ export function isSettingVisible(
     setting: ISetting,
     isEnabledInFeatures: boolean,
     isLoggedIn: boolean,
-    isSoftwareProfile: boolean,
     isLedgerProfile: boolean
 ): boolean {
     const loginChecked = !setting.requiresLogin || (setting.requiresLogin && isLoggedIn)
     const passedSoftwareCheck =
-        !setting.requiresSoftwareProfile || (setting.requiresSoftwareProfile && isSoftwareProfile)
+        !setting.requiresSoftwareProfile || (setting.requiresSoftwareProfile && !isLedgerProfile)
     const passedLedgerChecked = !setting.requiresLedgerProfile || (setting.requiresLedgerProfile && isLedgerProfile)
 
     return isEnabledInFeatures && loginChecked && passedLedgerChecked && passedSoftwareCheck
