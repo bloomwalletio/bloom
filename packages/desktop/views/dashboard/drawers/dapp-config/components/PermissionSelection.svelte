e23891afa72d11d1f6acda17f78da1f9b268afc2
Mark Nardi <nardimark7@gmail.com>
2024-01-16 15:50:28 +0100

    only show relevant permissions

--- packages/desktop/views/dashboard/drawers/dapp-config/components/PermissionSelection.svelte
+++ packages/desktop/views/dashboard/drawers/dapp-config/components/PermissionSelection.svelte
@@ -1,41 +1,57 @@
 <script lang="ts">
     import { ProposalTypes } from '@walletconnect/types'
     import { METHODS_FOR_PERMISSION } from '@auxiliary/wallet-connect/constants'
-    import { DappPermission } from '@auxiliary/wallet-connect/enums'
     import { onMount } from 'svelte'
     import Selection from './Selection.svelte'
     import { localize } from '@core/i18n'
     import { SupportedNamespaces } from '@auxiliary/wallet-connect/types'
+    import { Text } from '@bloomwalletio/ui'
+    import { getPermissionForMethod } from '@auxiliary/wallet-connect/utils'
 
     export let checkedMethods: string[]
     export let requiredNamespaces: ProposalTypes.RequiredNamespaces
+    export let optionalNamespaces: ProposalTypes.RequiredNamespaces
     export let persistedNamespaces: SupportedNamespaces | undefined = undefined
+    export let permissionSelections: { label: string; value: string; checked: boolean; required: boolean }[] = []
 
     const localeKey = 'views.dashboard.drawers.dapps.confirmConnection.permissions'
 
-    let permissionSelections: { label: string; value: string; checked: boolean; required: boolean }[] = []
     function setPermissionSelections(): void {
         const permissions: { label: string; value: string; checked: boolean; required: boolean }[] = []
-        const namespaces = Object.values(requiredNamespaces)
 
-        for (const permission of Object.values(DappPermission)) {
-            const supportedMethods = METHODS_FOR_PERMISSION[permission] ?? []
+        const checkedMethods: { [method: string]: boolean } = {}
+        const addedPermission: { [permission: string]: boolean } = {}
 
-            const isRequired = namespaces.some((namespace) => {
-                return supportedMethods.some((method) => namespace.methods.includes(method))
-            })
+        const methods = [
+            ...Object.values(requiredNamespaces).flatMap((namespace) =>
+                namespace.methods.map((method) => ({ method, required: true }))
+            ),
+            ...Object.values(optionalNamespaces).flatMap((namespace) =>
+                namespace.methods.map((method) => ({ method, required: false }))
+            ),
+        ]
+
+        for (const method of methods) {
+            if (checkedMethods[method.method]) {
+                continue
+            }
+            checkedMethods[method.method] = true
+
+            const permission = getPermissionForMethod(method.method)
+            if (!permission || addedPermission[permission]) {
+                continue
+            }
+            addedPermission[permission] = true
 
             const isChecked = persistedNamespaces
-                ? Object.values(persistedNamespaces).some((namespace) => {
-                      return supportedMethods.some((method) => namespace.methods.includes(method))
-                  })
+                ? Object.values(persistedNamespaces).some((namespace) => namespace.methods.includes(method.method))
                 : true
 
             permissions.push({
                 label: localize(`views.dashboard.drawers.dapps.confirmConnection.permissions.${String(permission)}`),
                 value: permission,
                 checked: isChecked,
-                required: isRequired,
+                required: method.required,
             })
         }
 
@@ -55,8 +71,18 @@
     })
 </script>
 
-<Selection
-    bind:selectionOptions={permissionSelections}
-    title={localize(`${localeKey}.title`)}
-    error={checkedMethods.length ? undefined : localize(`${localeKey}.empty`)}
-/>
+{#if permissionSelections.length}
+    <Selection
+        bind:selectionOptions={permissionSelections}
+        title={localize(`${localeKey}.title`)}
+        error={checkedMethods.length ? undefined : localize(`${localeKey}.empty`)}
+    />
+{:else}
+    <selection-component class="h-full flex flex-col gap-4">
+        <Text textColor="secondary">{localize(`${localeKey}.title`)}</Text>
+
+        <div class="w-full flex-grow flex justify-center items-center">
+            <Text type="body2">{localize(`${localeKey}.noPermissionsRequired`)}</Text>
+        </div>
+    </selection-component>
+{/if}
