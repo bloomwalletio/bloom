5fd2c5ae24cb0c2c1928a2693ed001a4e7ca287e
Tuditi <daviddetroch@pm.me>
2024-01-18 17:56:26 +0100

    chore: minor improvements

--- packages/shared/src/lib/core/layer-2/helpers/calculateGasFeeInGlow.ts
+++ packages/shared/src/lib/core/layer-2/helpers/calculateGasFeeInGlow.ts
@@ -1,10 +1,11 @@
 import { BigIntLike } from '@ethereumjs/util'
 import { WEI_PER_GLOW } from '../constants/wei.constants'
+import { Converter } from '@core/utils'
 
 export function calculateGasFeeInGlow(gasAmount: BigIntLike, gasPriceInWei: BigIntLike | undefined): bigint {
     if (gasAmount && gasPriceInWei) {
         const gasPriceInGlow = BigInt(Number(gasPriceInWei)) / WEI_PER_GLOW
-        return BigInt(Number(gasAmount) * Number(gasPriceInGlow))
+        return Converter.bigIntLikeToBigInt(gasAmount) * gasPriceInGlow
     } else {
         return BigInt(0)
     }
--- packages/shared/src/lib/core/layer-2/helpers/getEvmTransactionValueFromAmount.ts
+++ packages/shared/src/lib/core/layer-2/helpers/getEvmTransactionValueFromAmount.ts
@@ -2,5 +2,5 @@ import { HEX_PREFIX } from '@core/utils'
 import { WEI_PER_GLOW } from '../constants'
 
 export function getEvmTransactionValueFromAmount(amount: bigint): string {
-    return HEX_PREFIX + BigInt(amount * WEI_PER_GLOW).toString(16)
+    return HEX_PREFIX + (amount * WEI_PER_GLOW).toString(16)
 }
