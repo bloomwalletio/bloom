e236e7cde5fbc3b491a899cdf35e99365427e29d
Mark Nardi <nardimark7@gmail.com>
2024-01-22 14:12:00 +0100

    ui: add tabs to connected dapps (#1745)
    * add tabs to connected dapps
    
    * fix displayed dapps
    
    * make expired dapp slightly transparent
    
    * disable expired dapps

--- packages/shared/src/components/tiles/ClickableTile.svelte
+++ packages/shared/src/components/tiles/ClickableTile.svelte
@@ -9,11 +9,11 @@
 <Tile
     classes="
         {onClick ? 'hover:bg-gray-100 dark:hover:bg-gray-950 cursor-pointer' : 'pointer-events-none'}
-        {disabled ? 'opacity-50' : ''}
+        {disabled ? 'opacity-50 pointer-events-none' : ''}
         {classes}
     "
     {...$$restProps}
-    on:click={onClick}
+    on:click={() => !disabled && onClick?.()}
 >
     <slot />
 </Tile>
