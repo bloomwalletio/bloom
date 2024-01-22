63a23620926e039e42a24649f6a1b8a2db2e5e32
Mark Nardi <nardimark7@gmail.com>
2024-01-22 16:09:25 +0100

    add nftId to persisted nft

--- packages/shared/src/lib/core/nfts/utils/buildNftFromPersistedErc721Nft.ts
+++ packages/shared/src/lib/core/nfts/utils/buildNftFromPersistedErc721Nft.ts
@@ -2,13 +2,10 @@ import { DEFAULT_NFT_NAME, MimeType } from '@core/nfts'
 import { IErc721Nft, IPersistedErc721Nft } from '../interfaces'
 
 export function buildNftFromPersistedErc721Nft(nft: IPersistedErc721Nft, accountAddress: string): IErc721Nft {
-    const contractAddress = nft.contractMetadata.address.toLowerCase()
-    const id = nft.tokenId ? `${contractAddress}:${nft.tokenId}` : contractAddress
     const isSpendable = nft.ownerAddress === accountAddress
 
     return {
         ...nft,
-        id,
         isSpendable,
         name: nft.metadata?.name ?? nft.contractMetadata.name ?? DEFAULT_NFT_NAME,
         description: nft.metadata?.description,
