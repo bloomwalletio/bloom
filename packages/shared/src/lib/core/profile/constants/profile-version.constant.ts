d39a8c658ce30f20536b5066d5ee8facf21541c2
Mark Nardi <nardimark7@gmail.com>
2024-01-22 16:10:34 +0100

    update prod version

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -3,5 +3,5 @@ import { AppStage } from '@core/app/enums'
 export const PROFILE_VERSION: Record<AppStage, number> = {
     [AppStage.ALPHA]: 6,
     [AppStage.BETA]: 0,
-    [AppStage.PROD]: 2,
+    [AppStage.PROD]: 3,
 }
