c0290a00daa82945ec93b850eeb7bd6bcbda0e96
Nicole O'Brien <nicole@bloomwallet.io>
2024-01-26 20:11:40 +0000

    enhancement: reset campaign router on tab click (#1808)
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/desktop/views/dashboard/components/DashboardSidebar.svelte
+++ packages/desktop/views/dashboard/components/DashboardSidebar.svelte
@@ -13,6 +13,7 @@
     import { AutoUpdateToast, BackupToast, VersionToast } from './toasts'
     import { dashboardRoute } from '@core/router'
     import { StardustNetworkId } from '@core/network'
+    import { campaignsRouter } from '../campaigns'
 
     let expanded = true
     function toggleExpand(): void {
@@ -113,8 +114,9 @@
     function resetAllRouters(): void {
         $dashboardRouter.reset()
         $collectiblesRouter.reset()
-        $settingsRouter.reset()
+        $campaignsRouter.reset()
         $governanceRouter.reset()
+        $settingsRouter.reset()
     }
 </script>
 
