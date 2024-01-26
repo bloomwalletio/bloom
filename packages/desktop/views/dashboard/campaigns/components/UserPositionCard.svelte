cff0f597d8612ef321f8a749ac0c6edbad1cf74c
Mark Nardi <nardimark7@gmail.com>
2024-01-26 11:43:54 +0100

    add empty user position

--- packages/desktop/views/dashboard/campaigns/components/UserPositionCard.svelte
+++ packages/desktop/views/dashboard/campaigns/components/UserPositionCard.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
     import { Text } from '@bloomwalletio/ui'
-    import { ITideUserPosition } from '@core/tide/interfaces'
+    import { ITideLeaderboardItem } from '@core/tide/interfaces'
     import { Tile } from '@ui'
 
-    export let userPosition: ITideUserPosition
+    export let userPosition: ITideLeaderboardItem | undefined
 </script>
 
 <Tile classes="bg-surface-0 dark:bg-surface-0-dark border border-solid border-stroke dark:border-stroke-dark">
     <div class="w-full flex flex-row justify-between items-center p-2 gap-3 w-32">
         <div class="flex flex-row gap-3 items-center overflow-hidden">
             <div class="flex flex-col overflow-hidden">
-                <Text type="body2" truncate>My Position</Text>
-                <Text type="sm" textColor="secondary" truncate>
-                    Ranking: {userPosition.position}
-                </Text>
-                <Text type="sm" textColor="secondary" truncate>
-                    Points: {userPosition.xpEarned} xp
-                </Text>
-                <Text type="sm" textColor="secondary" truncate>
-                    Tasks Complete: {userPosition.tasksDone}
-                </Text>
-                <Text type="sm" textColor="secondary" truncate>
-                    Rewards Claimed: {userPosition.rewardClaimed}
-                </Text>
+                {#if userPosition}
+                    <Text type="body2" truncate>My Position</Text>
+                    <Text type="sm" textColor="secondary" truncate>
+                        Ranking: {userPosition.position}
+                    </Text>
+                    <Text type="sm" textColor="secondary" truncate>
+                        Points: {userPosition.xpGained} xp
+                    </Text>
+                    <Text type="sm" textColor="secondary" truncate>
+                        Tasks Complete: {userPosition.taskDone}
+                    </Text>
+                    <Text type="sm" textColor="secondary" truncate>
+                        Rewards Claimed: {userPosition.rewardClaimed}
+                    </Text>
+                {:else}
+                    <Text type="body1">You're not particitpating in this campaign yet</Text>
+                {/if}
             </div>
         </div>
     </div>
