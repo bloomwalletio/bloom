8082650fc039c96c7e640748dc4d747d5ea2658b
Nicole O'Brien <nicole@bloomwallet.io>
2024-01-26 10:41:00 +0000

    add initial campaign details layout (#1800)
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/desktop/views/dashboard/campaigns/components/index.ts
+++ packages/desktop/views/dashboard/campaigns/components/index.ts
@@ -1,2 +1,4 @@
 export { default as CampaignsGallery } from './CampaignsGallery.svelte'
 export { default as CampaignsGalleryItem } from './CampaignsGalleryItem.svelte'
+export { default as Leaderboard } from './Leaderboard.svelte'
+export { default as UserPositionCard } from './UserPositionCard.svelte'
--- packages/desktop/views/dashboard/campaigns/views/CampaignDetailsView.svelte
+++ packages/desktop/views/dashboard/campaigns/views/CampaignDetailsView.svelte
@@ -1,8 +1,75 @@
 <script lang="ts">
+    import { Text } from '@bloomwalletio/ui'
+    import { SupportedNetworkId } from '@core/network'
+    import { MimeType, Nft, NftStandard } from '@core/nfts'
+    import { TideApi } from '@core/tide/apis'
+    import { ITideUserPosition } from '@core/tide/interfaces'
+    import Pane from '@ui/atoms/Pane.svelte'
+    import { MediaPlaceholder } from '@ui/molecules'
+    import NftGalleryItem from '@ui/molecules/NftGalleryItem.svelte'
     import { onMount } from 'svelte'
     import Leaderboard from '../components/Leaderboard.svelte'
     import { campaignLeaderboards, addCampaignLeaderboard, selectedCampaign } from '@contexts/campaigns'
-    import { TideApi } from '@core/tide/apis'
+    import UserPositionCard from '../components/UserPositionCard.svelte'
+
+    const userPosition: ITideUserPosition = {
+        address: '0x9cb0f842bb6f827806f46cbbf62a494e6779bd08',
+        xpEarned: 333,
+        tasksDone: 6,
+        rewardClaimed: 1,
+        position: 1,
+    }
+
+    const userNft: Nft = {
+        id: '0x9cb0f842bb6f827806f46cbbf62a494e6779bd08:1',
+        type: MimeType.ImagePng,
+        networkId: SupportedNetworkId.ShimmerEvm,
+        name: 'MafiaBird #1',
+        isLoaded: true,
+        isSpendable: false,
+        standard: NftStandard.Erc721,
+        uri: 'https://tideprotocol.infura-ipfs.io/ipfs/QmZHMqGJ69SCofyoXtKaXxrLnoKBbGL92aC5NC79sk9Aks',
+        contractMetadata: {
+            standard: NftStandard.Erc721,
+            address: '0x9cb0f842bb6f827806f46cbbf62a494e6779bd08',
+            name: 'MafiaBirds',
+            symbol: 'MB',
+        },
+        tokenId: '1',
+        metadata: {
+            type: MimeType.ImagePng,
+            name: 'MafiaBird #1',
+            description:
+                'Are you ready to become a made bird and dive into The Magpie Mafia? 🐦🗡️ Enter the gang, climb your way up thru the ranks of a fun-oriented mafia-style organization, and be rewarded for your activity. 🔥🫡  By joining, you will earn your own nontransferable Magpie Mafia NFT that will level up based on your activity while acting as your way to access future Magpie Mafia functions, track your reputation, and more! 💣From here onwards, each time you’d like to engage in a campaign, join an event, or track your reputation, just show that you’re a member of The Magpie Mafia and join in on all the fun. 💰The beak speaks, the words of The Magpie Mafia carries with it authority and respect. 🪖',
+            image: 'https://tideprotocol.infura-ipfs.io/ipfs/QmZHMqGJ69SCofyoXtKaXxrLnoKBbGL92aC5NC79sk9Aks',
+            attributes: [
+                {
+                    trait_type: 'Body',
+                    value: 'Rocco',
+                },
+                {
+                    trait_type: 'Clothes',
+                    value: 'Harlem',
+                },
+                {
+                    trait_type: 'Background',
+                    value: 'Empire State Building',
+                },
+                {
+                    trait_type: 'Role',
+                    value: 'Ally',
+                },
+                {
+                    trait_type: 'Weapon',
+                    value: 'Bird\'s Knuckles',
+                },
+                {
+                    trait_type: 'XP',
+                    value: '65',
+                },
+            ],
+        },
+    }
 
     onMount(async () => {
         if (!$campaignLeaderboards[$selectedCampaign.projectId]?.[$selectedCampaign.id]) {
@@ -16,6 +83,36 @@
     })
 </script>
 
-{#if $campaignLeaderboards[$selectedCampaign.projectId]?.[$selectedCampaign.id]}
-    <Leaderboard leaderboardItems={$campaignLeaderboards[$selectedCampaign.projectId][$selectedCampaign.id]} />
-{/if}
+<div class="h-full flex flex-col gap-8">
+    <Pane
+        classes="
+            w-full flex-grow shrink-0 grid grid-cols-3
+            bg-surface dark:bg-surface-dark 
+            border border-solid border-stroke dark:border-stroke-dark 
+            divide-x divide-solid divide-stroke dark:divide-stroke-dark 
+            shadow-lg
+        "
+    >
+        <div class="min-w-full h-full object-cover">
+            <MediaPlaceholder size="md" />
+        </div>
+        <div class="col-span-2 p-4">
+            <Text type="h2" classes="whitespace-nowrap">{$selectedCampaign.title}</Text>
+            <Text type="body2">{$selectedCampaign.description}</Text>
+        </div>
+    </Pane>
+
+    <div class="grid grid-cols-7 gap-8 items-start">
+        <div class="col-span-5">
+            {#if $campaignLeaderboards[$selectedCampaign.projectId]?.[$selectedCampaign.id]}
+                <Leaderboard
+                    leaderboardItems={$campaignLeaderboards[$selectedCampaign.projectId][$selectedCampaign.id]}
+                />
+            {/if}
+        </div>
+        <div class="flex flex-col flex-grow gap-8 col-span-2">
+            <UserPositionCard {userPosition} />
+            <NftGalleryItem nft={userNft} />
+        </div>
+    </div>
+</div>
