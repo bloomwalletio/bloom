e31aa5d750f73d69f69b0ad24d13ebff9091bbb6
Mark Nardi <nardimark7@gmail.com>
2024-01-30 09:26:21 +0100

    add loading spinner to campaign fetch

--- packages/desktop/views/dashboard/campaigns/views/CampaignsGalleryView.svelte
+++ packages/desktop/views/dashboard/campaigns/views/CampaignsGalleryView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Button, IconName, Pill, Text } from '@bloomwalletio/ui'
+    import { Button, IconName, Pill, Spinner, Text } from '@bloomwalletio/ui'
     import { CollectiblesListMenu, EmptyListPlaceholder } from '@components'
     import { ICampaign, featuredCampaigns } from '@contexts/campaigns'
     import {
@@ -18,6 +18,7 @@
     import { TIDE_BASE_URL } from '@core/tide'
 
     const tideApi = new TideApi()
+    let loading = false
 
     const chainIds = getNetwork()
         .getChains()
@@ -47,11 +48,21 @@
         openUrlInBrowser(TIDE_BASE_URL)
     }
 
-    function fetchCampaigns(): void {
-        chainIds.forEach(async (chainId) => {
+    async function fetchCampaigns(): Promise<void> {
+        loading = true
+        const fetchCampaignsPromises = chainIds.map(async (chainId) => {
             const campaigns = (await tideApi.getCampaignsForChain(chainId)).campaigns
+
             addCampaignForChain(chainId, campaigns)
         })
+
+        try {
+            await Promise.all(fetchCampaignsPromises)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            loading = false
+        }
     }
 
     onMount(() => {
@@ -85,6 +96,10 @@
                 <EmptyListPlaceholder title={localize('views.campaign.gallery.noResults')} icon={IconName.Data} />
             </div>
         {/if}
+    {:else if loading}
+        <div class="w-full h-full flex flex-col items-center justify-center">
+            <Spinner size="lg" textColor="primary" />
+        </div>
     {:else}
         <div class="w-full h-full flex flex-col items-center justify-center grow-1 gap-6">
             <EmptyListPlaceholder
