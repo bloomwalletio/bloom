653fa91f4c6823090c9128d55373695155015f6f
Jean Ribeiro <contact@jeanribeiro.dev>
2024-02-02 16:28:35 -0300

    enhancement: check transak url

--- packages/desktop/views/dashboard/buy-sell/components/TransakConnectionPanel.svelte
+++ packages/desktop/views/dashboard/buy-sell/components/TransakConnectionPanel.svelte
@@ -14,7 +14,8 @@
     $: connectionStatus = getConnectionStatus(url)
 
     function getConnectionStatus(url: string): TransakConnectionStatus {
-        if (url.includes(TRANSAK_HOSTNAME)) {
+        const _url = new URL(url)
+        if (_url.hostname === TRANSAK_HOSTNAME) {
             return TransakConnectionStatus.Connected
         } else if (url) {
             return TransakConnectionStatus.Redirected
