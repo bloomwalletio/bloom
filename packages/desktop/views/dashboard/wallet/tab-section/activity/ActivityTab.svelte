f5c7703ede44a05e1c5418ed7578099aa8b7f53b
Mark Nardi <nardimark7@gmail.com>
2024-02-07 12:36:04 +0100

    ui: add date column to activity (#1872)
    * add date column to activity
    
    * fix: column widths
    
    ---------
    
    Co-authored-by: Nicole O'Brien <nicole@bloomwallet.io>

--- packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/ActivityTab.svelte
@@ -34,6 +34,7 @@
 <activity-tab>
     <header-row>
         <Text type="sm" fontWeight="medium" textColor="secondary">{localize('views.dashboard.activity.activity')}</Text>
+        <Text type="sm" fontWeight="medium" textColor="secondary">{localize('views.dashboard.activity.date')}</Text>
         <Text type="sm" fontWeight="medium" textColor="secondary">{localize('views.dashboard.activity.address')}</Text>
         <div class="text-end">
             <Text type="sm" fontWeight="medium" textColor="secondary">
@@ -83,7 +84,7 @@
             @apply border-y border-solid border-stroke dark:border-stroke-dark;
 
             @apply grid;
-            grid-template-columns: 3fr 2fr 2fr 2fr;
+            grid-template-columns: 3fr 1fr 2fr 2fr 1fr;
         }
 
         info-section {
--- packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/index.ts
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/index.ts
@@ -1,4 +1,5 @@
 export { default as ActivityAddressSection } from './ActivityAddressSection.svelte'
 export { default as ActivityAmountSection } from './ActivityAmountSection.svelte'
 export { default as ActivityAssetSection } from './ActivityAssetSection.svelte'
+export { default as ActivityDateSection } from './ActivityDateSection.svelte'
 export { default as ActivityFeeSection } from './ActivityFeeSection.svelte'
