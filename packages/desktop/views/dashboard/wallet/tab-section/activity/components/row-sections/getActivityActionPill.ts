d32e8790002e15f6240c03403f3d0879b460c3da
Mark Nardi <nardimark7@gmail.com>
2024-02-07 13:25:28 +0100

    add new pills to activity action section

---
+++ packages/desktop/views/dashboard/wallet/tab-section/activity/components/row-sections/getActivityActionPill.ts
@@ -0,0 +1,42 @@
+import { Activity, ActivityAsyncStatus, ActivityDirection } from '@core/activity'
+import { getTimeDifference } from '@core/utils/time'
+
+export function getActivityActionPill(
+    activity: Activity,
+    currentDate: Date
+): { type: 'timelock' | 'unclaimed' | 'expired'; timeDiff?: string } | undefined {
+    if (!activity?.asyncData?.asyncStatus) {
+        return undefined
+    }
+
+    const { asyncStatus, expirationDate, timelockDate } = activity.asyncData
+
+    switch (asyncStatus) {
+        case ActivityAsyncStatus.Claimed: {
+            return undefined
+        }
+        case ActivityAsyncStatus.Timelocked: {
+            if (activity.direction === ActivityDirection.Outgoing) {
+                if (expirationDate) {
+                    const timeDiff = getTimeDifference(expirationDate, currentDate)
+                    return { type: 'unclaimed', timeDiff }
+                } else {
+                    return undefined
+                }
+            } else {
+                const timeDiff = getTimeDifference(timelockDate, currentDate)
+                return { type: 'timelock', timeDiff }
+            }
+        }
+        case ActivityAsyncStatus.Unclaimed: {
+            const timeDiff = expirationDate ? getTimeDifference(expirationDate, currentDate) : undefined
+            return { type: 'unclaimed', timeDiff }
+        }
+        case ActivityAsyncStatus.Expired: {
+            return { type: 'expired' }
+        }
+        default: {
+            return undefined
+        }
+    }
+}
