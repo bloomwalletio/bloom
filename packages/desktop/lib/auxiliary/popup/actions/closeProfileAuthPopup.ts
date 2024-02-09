30cb19a19d1c2a6bd7b4d79b4e97817415b36af6
Tuditi <daviddetroch@pm.me>
2024-02-09 17:30:16 +0100

    chore: update locale

--- packages/desktop/lib/auxiliary/popup/actions/closeProfileAuthPopup.ts
+++ packages/desktop/lib/auxiliary/popup/actions/closeProfileAuthPopup.ts
@@ -5,7 +5,7 @@ import { localize } from '@core/i18n'
 export function closeProfileAuthPopup(options?: { forceClose?: boolean; callOnCancel?: boolean }): void {
     const props = get(profileAuthPopup).props
     if (options?.callOnCancel && 'function' === typeof props?.onCancel) {
-        props.onCancel(localize('error.stronghold.passwordCancelled'))
+        props.onCancel(localize('error.profile.authenticationInterrupted'))
     }
 
     updateProfileAuthProps({
