c981203762a41a105643e238ff9c86a599be5661
Tuditi <daviddetroch@pm.me>
2024-02-14 14:53:17 +0100

    chore: rebrand NodeAuthRequiredPopup

--- packages/desktop/components/popup/popups/NodeAuthRequiredPopup.svelte
+++ packages/desktop/components/popup/popups/NodeAuthRequiredPopup.svelte
@@ -1,54 +1,87 @@
 <script lang="ts">
-    import { Text, TextInput, TextType } from '@ui'
+    import { KeyValue } from '@ui'
     import type { IAuth } from '@iota/sdk'
     import { handleError } from '@core/error/handlers'
     import { localize } from '@core/i18n'
     import { closePopup } from '@desktop/auxiliary/popup'
-    import { Button } from '@bloomwalletio/ui'
+    import { TextInput, Tabs } from '@bloomwalletio/ui'
+    import PopupTemplate from '../PopupTemplate.svelte'
+    import { IError } from '@core/error'
 
     export let onSubmit: (auth: IAuth) => unknown = () => {}
 
+    enum Auth {
+        Credentials = 'credentials',
+        Jwt = 'jwt',
+    }
+
+    const tabs: KeyValue<string>[] = [
+        { key: Auth.Credentials, value: localize('popups.node.credentials') },
+        { key: Auth.Jwt, value: localize('popups.node.jwt') },
+    ]
+    let selectedTab = tabs[0]
+
     let isBusy = false
 
+    let username: string
+    let password: string
+
     let jwt: string
-    let jwtError: string
+    let jwtError: string | undefined
 
-    $: disabled = !jwt || isBusy
+    $: isJwtAuthentication = selectedTab.key === Auth.Jwt
+    $: disabled = isBusy || (isJwtAuthentication ? !jwt : !username || !password)
 
-    async function handleSubmit(): Promise<void> {
+    async function onConfirmClick(): Promise<void> {
         try {
             isBusy = true
-            const auth = { jwt }
+            const auth = isJwtAuthentication ? { jwt } : { basicAuthNamePwd: [username, password] as [string, string] }
             await onSubmit(auth)
             isBusy = false
         } catch (err) {
+            const error = err as IError
             isBusy = false
-            const authenticationError = err?.error?.match(/(jwt)/g)?.[0]
+            const authenticationError = error?.error?.match(/(jwt)/g)?.[0]
             switch (authenticationError) {
                 case 'jwt':
-                    jwtError = err.error
+                    jwtError = error?.error
                     break
                 default:
-                    handleError(err)
+                    handleError(error)
                     break
             }
         }
     }
+
+    function onCancelClick(): void {
+        closePopup()
+    }
 </script>
 
-<form id="node-auth-required" on:submit|preventDefault={handleSubmit}>
-    <Text type={TextType.h3} classes="mb-6">{localize('popups.nodeAuthRequired.title')}</Text>
-    <Text fontSize="15">{localize('popups.nodeAuthRequired.body')}</Text>
-    <div class="flex flex-col w-full space-y-5 mt-4">
-        <TextInput
-            bind:value={jwt}
-            bind:error={jwtError}
-            placeholder={localize('general.jwt')}
-            label={localize('general.jwt')}
-        />
-    </div>
-    <div class="flex w-full space-x-4 mt-6">
-        <Button variant="outlined" width="full" on:click={() => closePopup()} text={localize('actions.cancel')} />
-        <Button {disabled} busy={isBusy} type="submit" width="full" text={localize('actions.confirm')} />
-    </div>
-</form>
+<PopupTemplate
+    title={localize('popups.nodeAuthRequired.title')}
+    description={localize('popups.nodeAuthRequired.body')}
+    backButton={{
+        text: localize('actions.cancel'),
+        onClick: onCancelClick,
+    }}
+    continueButton={{
+        form: 'node-auth-required',
+        text: localize('actions.confirm'),
+        onClick: onConfirmClick,
+        disabled,
+    }}
+    busy={isBusy}
+>
+    <form id="node-auth-required" on:submit|preventDefault={onConfirmClick}>
+        <div class="flex flex-col w-full space-y-4">
+            <Tabs bind:selectedTab {tabs} />
+            {#if isJwtAuthentication}
+                <TextInput bind:value={jwt} error={jwtError} label={localize('popups.node.jwt')} />
+            {:else}
+                <TextInput bind:value={username} label={localize('general.username')} />
+                <TextInput bind:value={password} label={localize('general.password')} />
+            {/if}
+        </div>
+    </form>
+</PopupTemplate>
