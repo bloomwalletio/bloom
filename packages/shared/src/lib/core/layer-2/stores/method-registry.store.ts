f2b0b08f099ea8048d4d5ee04ad7eb6d0caddcc9
Mark Nardi <nardimark7@gmail.com>
2024-02-16 10:56:35 +0100

    add reading from cache

--- packages/shared/src/lib/core/layer-2/stores/method-registry.store.ts
+++ packages/shared/src/lib/core/layer-2/stores/method-registry.store.ts
@@ -1,3 +1,10 @@
-import { persistent } from '@core/utils'
+import { persistent } from '@core/utils/store'
 
-export const methodRegistry = persistent<{ [signature: string]: string }>('methodRegistry', {})
+export const methodRegistry = persistent<{ [fourBytePrefix: string]: string }>('methodRegistry', {})
+
+export function addMethodToRegistry(fourBytePrefix: string, signature: string): void {
+    methodRegistry.update((registry) => {
+        registry[fourBytePrefix] = signature
+        return registry
+    })
+}
--- packages/shared/src/lib/core/layer-2/utils/lookupMethodSignature.ts
+++ packages/shared/src/lib/core/layer-2/utils/lookupMethodSignature.ts
@@ -2,14 +2,27 @@ import Web3 from 'web3'
 import { REGISTRY_ABI } from '../abis/registry.abi'
 import { ETHEREUM_MAINNET_NODE } from '../constants'
 import { REGISTRY_CONTRACT_ADDRESS } from '../constants/registry-contract-address.constant'
+import { get } from 'svelte/store'
+import { addMethodToRegistry, methodRegistry } from '../stores/method-registry.store'
 
 const provider = new Web3(ETHEREUM_MAINNET_NODE)
 const registry = new provider.eth.Contract(REGISTRY_ABI, REGISTRY_CONTRACT_ADDRESS)
 
 export async function lookupMethodSignature(fourBytePrefix: string): Promise<string | undefined> {
-    const result = await registry.methods.entries(fourBytePrefix).call()
-    if (result) {
-        return result[0]
+    const methodName = get(methodRegistry)[fourBytePrefix]
+    if (methodName) {
+        return methodName
     }
-    return undefined
+
+    try {
+        const result = await registry.methods.entries(fourBytePrefix).call()
+        if (!result) {
+            return undefined
+        }
+
+        addMethodToRegistry(fourBytePrefix, result)
+        return result
+    } catch (error) {
+        return undefined
+    }
 }
