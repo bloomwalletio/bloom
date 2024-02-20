3f39dacd55eee0a2a8d08535d9da8e473b7a012c
Mark Nardi <nardimark7@gmail.com>
2024-02-20 15:39:29 +0100

    move proposals

--- packages/desktop/views/dashboard/governance/views/GovernanceDashboardView.svelte
+++ packages/desktop/views/dashboard/governance/views/GovernanceDashboardView.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import { IconName } from '@bloomwalletio/ui'
-    import { EmptyListPlaceholder, Proposals, ProposalsDetails } from '@components'
+    import { EmptyListPlaceholder, ProposalsDetails } from '@components'
     import { Pane } from '@ui'
     import { ManageVotingPowerPane } from '../components'
     import { localize } from '@core/i18n'
     import { registeredProposalsForSelectedAccount } from '@contexts/governance/stores'
+    import { ProposalList } from '@views/governance'
 </script>
 
 <governance-dashboard class="w-full h-full flex flex-nowrap p-8 relative flex-1 space-x-6">
@@ -17,7 +18,7 @@
     <span class="block w-0.5 h-full bg-gray-200 dark:bg-gray-800" />
     <div class="w-2/3">
         {#if Object.keys($registeredProposalsForSelectedAccount).length}
-            <Proposals />
+            <ProposalList />
         {:else}
             <div class="w-full h-full flex flex-col items-center justify-center">
                 <EmptyListPlaceholder
