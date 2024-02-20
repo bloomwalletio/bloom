4a0a6c2b68cc39e07367c860b092b6525d95aa2e
Mark Nardi <nardimark7@gmail.com>
2024-02-20 15:16:46 +0100

    move pane

--- packages/desktop/views/dashboard/governance/views/ProposalDetailsView.svelte
+++ packages/desktop/views/dashboard/governance/views/ProposalDetailsView.svelte
@@ -6,7 +6,7 @@
         TrackedParticipationOverview,
     } from '@iota/sdk/out/types'
     import { Alert, Button, Table } from '@bloomwalletio/ui'
-    import { ProposalDetailsMenu, ProposalInformationPane, ProposalQuestion } from '@components'
+    import { ProposalDetailsMenu, ProposalQuestion } from '@components'
     import { getVotingEvent } from '@contexts/governance/actions'
     import {
         clearParticipationEventStatusPoll,
@@ -35,7 +35,7 @@
     import { visibleSelectedAccountTokens } from '@core/token/stores'
     import { getBestTimeDuration, milestoneToDate } from '@core/utils'
     import { PopupId, openPopup } from '@desktop/auxiliary/popup'
-    import { ProposalStatusPill } from '@views/governance'
+    import { ProposalInformationPane, ProposalStatusPill } from '@views/governance'
     import { MarkdownBlock, Pane, Text, TextType } from '@ui'
     import { onDestroy, onMount } from 'svelte'
 
