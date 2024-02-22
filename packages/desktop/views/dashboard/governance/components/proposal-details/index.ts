0ed3a1b9dbc299bf454a872f62c124989a95e749
Jean Ribeiro <contact@jeanribeiro.dev>
2024-02-22 19:50:06 -0300

    refactor: ProposalQuestionListPane

--- packages/desktop/views/dashboard/governance/components/proposal-details/index.ts
+++ packages/desktop/views/dashboard/governance/components/proposal-details/index.ts
@@ -1,3 +1,4 @@
 export { default as ProposalAccountVotingPane } from './ProposalAccountVotingPane.svelte'
 export { default as ProposalDetailsPane } from './ProposalDetailsPane.svelte'
 export { default as ProposalInformationPane } from './ProposalInformationPane.svelte'
+export { default as ProposalQuestionListPane } from './ProposalQuestionListPane.svelte'
