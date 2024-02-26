87729a078892f223bca3737e1ae679e43c24c389
Mark Nardi <nardimark7@gmail.com>
2024-02-26 17:17:58 +0100

    fix metadata on nfts (#1996)

--- packages/shared/src/components/molecules/activity-info/NftMetadataInformation.svelte
+++ packages/shared/src/components/molecules/activity-info/NftMetadataInformation.svelte
@@ -11,52 +11,74 @@
     $: nft = getNftByIdFromAllAccountNfts($selectedAccountIndex, activity?.nftId)
 </script>
 
-{#if nft?.parsedMetadata}
-    {@const metadata = nft.metadata}
-    <Table
-        items={[
-            {
-                key: localize('general.standard'),
-                value:
-                    metadata.standard && metadata.version
-                        ? `${metadata.standard} - ${metadata.version}`
-                        : metadata.standard,
-            },
-            {
-                key: localize('general.type'),
-                value: metadata.type,
-                tooltip: localize('tooltips.transactionDetails.nftMetadata.type'),
-            },
-            {
-                key: localize('general.uri'),
-                value: metadata.uri,
-                copyable: true,
-            },
-            {
-                key: localize('general.description'),
-                value: metadata.description,
-            },
-            {
-                key: localize('general.issuerName'),
-                value: metadata.issuerName,
-                tooltip: localize('tooltips.transactionDetails.nftMetadata.issuerName'),
-            },
-            {
-                key: localize('general.collectionName'),
-                value: metadata.collectionName,
-            },
-            {
-                key: localize('general.metadata'),
-                value: metadata.standard !== NftStandard.Irc27 ? metadata : undefined,
-            },
-            {
-                key: localize('general.attributes'),
-                value: metadata.attributes,
-            },
-            {
-                key: localize('general.royalties'),
-                value: metadata.royalties,
-            },
-        ]}
-    />
+{#if nft?.metadata}
+    {#if nft.standard === NftStandard.Irc27}
+        {@const metadata = nft.metadata}
+        <Table
+            items={[
+                {
+                    key: localize('general.standard'),
+                    value:
+                        metadata.standard && metadata.version
+                            ? `${metadata.standard} - ${metadata.version}`
+                            : metadata.standard,
+                },
+                {
+                    key: localize('general.type'),
+                    value: metadata.type,
+                    tooltip: localize('tooltips.transactionDetails.nftMetadata.type'),
+                },
+                {
+                    key: localize('general.uri'),
+                    value: metadata.uri,
+                    copyable: true,
+                },
+                {
+                    key: localize('general.description'),
+                    value: metadata.description,
+                },
+                {
+                    key: localize('general.issuerName'),
+                    value: metadata.issuerName,
+                    tooltip: localize('tooltips.transactionDetails.nftMetadata.issuerName'),
+                },
+                {
+                    key: localize('general.collectionName'),
+                    value: metadata.collectionName,
+                },
+                {
+                    key: localize('general.attributes'),
+                    value: metadata.attributes,
+                },
+                {
+                    key: localize('general.royalties'),
+                    value: metadata.royalties,
+                },
+            ]}
+        />
+    {:else}
+        {@const metadata = nft.metadata}
+        <Table
+            items={[
+                {
+                    key: localize('general.type'),
+                    value: metadata.type,
+                    tooltip: localize('tooltips.transactionDetails.nftMetadata.type'),
+                },
+                {
+                    key: localize('general.image'),
+                    value: metadata.image,
+                    copyable: true,
+                },
+                {
+                    key: localize('general.description'),
+                    value: metadata.description,
+                },
+                {
+                    key: localize('general.attributes'),
+                    value: metadata.attributes,
+                },
+            ]}
+        />
+    {/if}
 {/if}
