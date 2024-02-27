70a7506eeb89d989151f7562ab74156e3949427c
Nicole O'Brien <nicole@bloomwallet.io>
2024-02-27 16:39:34 +0000

    feat: explorer api pagination (#2002)
    * refactor: query parameters on base api
    add: make paginated request on explorer api
    
    Co-authored-by: Tuditi <Tuditi@users.noreply.github.com>
    
    * fix: paginated request logic
    
    * refactor: move evm explorer to blockscout module
    
    * fix: imports
    
    * feat: add blockscout transaction interface
    
    * fix: correct queryParameters
    
    ---------
    
    Co-authored-by: Tuditi <Tuditi@users.noreply.github.com>
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    Co-authored-by: Tuditi <daviddetroch@pm.me>

---
+++ packages/shared/src/lib/auxiliary/blockscout/interfaces/index.ts
@@ -0,0 +1,4 @@
+export * from './blockscout-api.interface'
+export * from './blockscout-asset.interface'
+export * from './blockscout-asset-metadata.interface'
+export * from './blockscout-transaction.interface'
