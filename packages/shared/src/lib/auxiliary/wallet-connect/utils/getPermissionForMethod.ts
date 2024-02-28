558ad0194902d3f0f0ed645cf6e28bfa2d965f78
Mark Nardi <nardimark7@gmail.com>
2024-02-28 09:36:02 +0100

    fix type

--- packages/shared/src/lib/auxiliary/wallet-connect/utils/getPermissionForMethod.ts
+++ packages/shared/src/lib/auxiliary/wallet-connect/utils/getPermissionForMethod.ts
@@ -1,7 +1,7 @@
 import { METHODS_FOR_PERMISSION } from '../constants'
-import { DappPermission } from '../enums'
+import { DappPermission, RpcMethod } from '../enums'
 
-export function getPermissionForMethod(method: string): DappPermission | undefined {
+export function getPermissionForMethod(method: RpcMethod): DappPermission | undefined {
     for (const permission of Object.values(DappPermission)) {
         const supportedMethods = METHODS_FOR_PERMISSION[permission] ?? []
 
