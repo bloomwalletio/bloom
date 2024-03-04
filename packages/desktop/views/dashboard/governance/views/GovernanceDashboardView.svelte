27f45807b94d7deb8e545a173f8f4725cfaf715e
Nicole O'Brien <nicole@bloomwallet.io>
2024-03-04 17:01:38 +0000

    ui: polish title in governance dashboard
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

--- packages/desktop/views/dashboard/governance/views/GovernanceDashboardView.svelte
+++ packages/desktop/views/dashboard/governance/views/GovernanceDashboardView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { IconName } from '@bloomwalletio/ui'
+    import { IconName, Text, Pill } from '@bloomwalletio/ui'
     import { EmptyListPlaceholder } from '@components'
     import { Pane } from '@ui'
     import { ManageVotingPowerPane, ProposalList, ProposalListDetails } from '../components'
@@ -9,6 +9,14 @@
 
 <governance-dashboard class="w-full h-full flex flex-nowrap p-8 relative flex-1 space-x-6">
     <div class="w-1/3 flex flex-col space-y-4">
+        <div class="flex gap-2 pt-2">
+            <Text type="h6">{localize('views.governance.proposals.title')}</Text>
+            <Pill color="neutral">
+                <Text textColor="secondary">
+                    {String(Object.keys($registeredProposalsForSelectedAccount).length ?? '')}
+                </Text>
+            </Pill>
+        </div>
         <ManageVotingPowerPane />
         <Pane classes="p-6 h-fit">
             <ProposalListDetails />
