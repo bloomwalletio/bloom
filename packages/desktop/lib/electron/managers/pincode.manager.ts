6cbcc5a373b74f6c57fe500baff98f1c08aef6d3
Tuditi <daviddetroch@pm.me>
2024-03-05 10:49:34 +0100

    feat: use salt

--- packages/desktop/lib/electron/managers/pincode.manager.ts
+++ packages/desktop/lib/electron/managers/pincode.manager.ts
@@ -4,12 +4,12 @@ import type { IPincodeManager } from '@core/app'
 
 export default class PincodeManager implements IPincodeManager {
     public async set(key: string, pincode: string): Promise<void> {
-        const hash = this.hashValue(pincode)
+        const hash = this.hashValue(key, pincode)
         return ipcRenderer.invoke('keychain-set', key, hash)
     }
 
     public async verify(key: string, pincode: string): Promise<boolean> {
-        const hashedPin = this.hashValue(pincode)
+        const hashedPin = this.hashValue(key, pincode)
         const storedHash = await ipcRenderer.invoke('keychain-get', key)
         if (pincode === storedHash) {
             // Set pincode as a hash if it is stored in clear text (legacy implementation)
@@ -24,9 +24,9 @@ export default class PincodeManager implements IPincodeManager {
         return ipcRenderer.invoke('keychain-remove', key)
     }
 
-    private hashValue(value: string): string {
+    private hashValue(salt: string, value: string): string {
         const hash = createHash('sha256')
-        const hashedValue = hash.update(value).digest('base64')
+        const hashedValue = hash.update(`${salt}:${value}`).digest('base64')
         return hashedValue
     }
 }
