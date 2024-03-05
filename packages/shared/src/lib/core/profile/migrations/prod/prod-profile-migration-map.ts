2fd7705dc41fbfea1a4b1506c084a2b9110a5238
Mark Nardi <nardimark7@gmail.com>
2024-03-05 11:27:02 +0100

    add prod migration

--- packages/shared/src/lib/core/profile/migrations/prod/prod-profile-migration-map.ts
+++ packages/shared/src/lib/core/profile/migrations/prod/prod-profile-migration-map.ts
@@ -3,9 +3,11 @@ import { ProfileMigrationMap } from '../../types'
 import { prodProfileMigration0To1 } from './prod-profile-migration-0-to-1'
 import { prodProfileMigration1To2 } from './prod-profile-migration-1-to-2'
 import { prodProfileMigration2To3 } from './prod-profile-migration-2-to-3'
+import { prodProfileMigration3To4 } from './prod-profile-migration-3-to-4'
 
 export const PROD_PROFILE_MIGRATION_MAP: ProfileMigrationMap = {
     0: prodProfileMigration0To1,
     1: prodProfileMigration1To2,
     2: prodProfileMigration2To3,
+    3: prodProfileMigration3To4,
 }
