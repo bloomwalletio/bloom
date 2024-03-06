5b3b52cc29ef2b8775e51302f5749b07d35ba549
Tuditi <daviddetroch@pm.me>
2024-03-06 11:51:35 +0100

    fix: move menu out of governance list

--- packages/desktop/views/dashboard/governance/components/ProposalList.svelte
+++ packages/desktop/views/dashboard/governance/components/ProposalList.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-    import { Filter } from '@components'
     import { proposalFilter, registeredProposalsForSelectedAccount } from '@contexts/governance/stores'
     import { isVisibleProposal, sortProposals } from '@contexts/governance/utils'
-    import { SearchInput } from '@ui'
     import { ProposalCard } from './'
-    import ProposalListMenu from './ProposalListMenu.svelte'
 
     $: proposals = Object.values($registeredProposalsForSelectedAccount)
 
-    let searchTerm = ''
+    const searchTerm = ''
 
     $: visibleProposals = proposals
         .filter((proposal) => isVisibleProposal(proposal, $proposalFilter))
@@ -28,13 +25,6 @@
 </script>
 
 <proposals-container class="flex flex-col h-full">
-    <header-container class="flex justify-end items-center mb-4 h-9">
-        <div class="flex flex-row gap-5 items-center">
-            <SearchInput bind:value={searchTerm} />
-            <Filter filterStore={proposalFilter} />
-            <ProposalListMenu />
-        </div>
-    </header-container>
     <ul class="grid grid-cols-2 auto-rows-min gap-4 flex-1 overflow-y-scroll pr-3 -mr-5">
         {#each sortedProposals as proposal}
             <ProposalCard {proposal} />
