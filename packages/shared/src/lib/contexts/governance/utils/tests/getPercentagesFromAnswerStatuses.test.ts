f78d339db44d50d70a32d1b0d9c26295fe88d171
Tuditi <daviddetroch@pm.me>
2024-03-06 11:35:51 +0100

    chore: add test

--- packages/shared/src/lib/contexts/governance/utils/tests/getPercentagesFromAnswerStatuses.test.ts
+++ packages/shared/src/lib/contexts/governance/utils/tests/getPercentagesFromAnswerStatuses.test.ts
@@ -6,34 +6,63 @@ import type { IProposal } from '../../interfaces'
 describe('File: getPercentagesFromAnswerStatuses.ts', () => {
     describe('Function: getPercentagesFromAnswerStatuses', () => {
         const mockProposal = {
-            milestones: { ended: 10000 },
+            milestones: { ended: 100 },
         }
 
-        const ANSWER_STATUSES: AnswerStatus[] = [
-            {
-                value: 0,
-                current: 10,
-                accumulated: 1000,
-            },
-            {
-                value: 1,
-                current: 20,
-                accumulated: 2000,
-            },
-            {
-                value: 2,
-                current: 30,
-                accumulated: 3000,
-            },
-        ]
+        it('should return percentages with same projected votes as total votes from valid arguments and proposal', () => {
+            const answerStatuses: AnswerStatus[] = [
+                {
+                    value: 0,
+                    current: 10,
+                    accumulated: 1000,
+                },
+                {
+                    value: 1,
+                    current: 20,
+                    accumulated: 2000,
+                },
+                {
+                    value: 2,
+                    current: 30,
+                    accumulated: 3000,
+                },
+            ]
 
-        it('should return percentages with projected votes from valid arguments and proposal', () => {
             const expectedPercentages = {
                 0: { accumulated: '16.7%', projected: '16.7%' },
                 1: { accumulated: '33.3%', projected: '33.3%' },
                 2: { accumulated: '50%', projected: '50%' },
             }
-            expect(getPercentagesFromAnswerStatuses(ANSWER_STATUSES, mockProposal as IProposal)).toEqual(
+            expect(getPercentagesFromAnswerStatuses(answerStatuses, mockProposal as IProposal)).toEqual(
+                expectedPercentages
+            )
+        })
+
+        it('should return percentages with different projected votes as total votes from valid arguments and proposal', () => {
+            const answerStatuses: AnswerStatus[] = [
+                {
+                    value: 0,
+                    current: 400,
+                    accumulated: 1000,
+                },
+                {
+                    value: 1,
+                    current: 20,
+                    accumulated: 2000,
+                },
+                {
+                    value: 2,
+                    current: 30,
+                    accumulated: 3000,
+                },
+            ]
+
+            const expectedPercentages = {
+                0: { accumulated: '16.7%', projected: '80.5%' },
+                1: { accumulated: '33.3%', projected: '7.8%' },
+                2: { accumulated: '50%', projected: '11.7%' },
+            }
+            expect(getPercentagesFromAnswerStatuses(answerStatuses, mockProposal as IProposal)).toEqual(
                 expectedPercentages
             )
         })
