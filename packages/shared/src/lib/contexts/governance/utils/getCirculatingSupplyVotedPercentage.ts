f930e4ee2d7dc06b52a261ac580bb5d9708d13dc
Nicole O'Brien <nicole@bloomwallet.io>
2024-03-11 23:22:51 +0000

    fix: show quorum percentage during commencement phase

--- packages/shared/src/lib/contexts/governance/utils/getCirculatingSupplyVotedPercentage.ts
+++ packages/shared/src/lib/contexts/governance/utils/getCirculatingSupplyVotedPercentage.ts
@@ -16,21 +16,26 @@ export function getCirculatingSupplyVotedPercentage(
         return 0
     }
 
+    let totalEventVotes = participationEventStatus.questions[0].answers.reduce(
+        (total, answer) => (total += BigInt(answer.accumulated)),
+        BigInt(0)
+    )
+
     let milestoneCount: number
     if (currentMilestone < proposal.milestones.holding) {
+        totalEventVotes = participationEventStatus.questions[0].answers.reduce(
+            (total, answer) => (total += BigInt(answer.current)),
+            BigInt(0)
+        )
         milestoneCount = 1
     } else if (currentMilestone > proposal.milestones.ended) {
         milestoneCount = proposal.milestones.ended - proposal.milestones.holding
     } else {
         milestoneCount = currentMilestone - proposal.milestones.holding
     }
-    const totalEventVotes = participationEventStatus.questions[0].answers.reduce(
-        (total, answer) => (total += BigInt(answer.accumulated)),
-        BigInt(0)
-    )
     const maximumVotes = BigInt(circulatingSupply) * BigInt(milestoneCount)
 
-    const percentage = calculatePercentageOfBigInt(totalEventVotes, maximumVotes, 6)
+    const percentage = calculatePercentageOfBigInt(totalEventVotes, maximumVotes, 12)
 
     return percentage
 }
