019ec34e8d867fdd55e784be9480690d1002b0cf
Mark Nardi <nardimark7@gmail.com>
2024-03-11 15:58:49 +0100

    update type

--- packages/shared/src/lib/core/activity/types/evm/evm-balance-change-activity.type.ts
+++ packages/shared/src/lib/core/activity/types/evm/evm-balance-change-activity.type.ts
@@ -1,12 +1,9 @@
 import { EvmActivityType } from '@core/activity/enums/evm'
 import { TokenStandard } from '@core/token'
 import { NftStandard } from '@core/nfts'
-import { NetworkNamespace } from '@core/network'
-import { BaseActivity } from '../base-activity.type'
+import { BaseEvmActivity } from './base-evm-activity.type'
 
-export type EvmBalanceChangeActivity = BaseActivity & {
-    namespace: NetworkNamespace.Evm
-
+export type EvmBalanceChangeActivity = BaseEvmActivity & {
     type: EvmActivityType.BalanceChange
     tokenTransfer: {
         standard: TokenStandard.Irc30 | NftStandard.Irc27
