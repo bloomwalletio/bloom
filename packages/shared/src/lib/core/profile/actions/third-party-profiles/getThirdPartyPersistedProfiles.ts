5762c9813611fa15ef171909ccdfc088cf6da220
Nicole O'Brien <nicole@bloomwallet.io>
2024-03-12 10:08:23 +0000

    create getThirdPartyPersistedProfiles action

---
+++ packages/shared/src/lib/core/profile/actions/third-party-profiles/getThirdPartyPersistedProfiles.ts
@@ -0,0 +1,17 @@
+import { Platform } from '@core/app/classes'
+import { IThirdPartyPersistedProfile } from '@core/profile/interfaces'
+
+export async function getThirdPartyPersistedProfiles(appName: string): Promise<IThirdPartyPersistedProfile[]> {
+    const thirdPartyProfiles: IThirdPartyPersistedProfile[] = []
+    const thirdPartyData = await Platform.getThirdPartyData(appName)
+    if (!thirdPartyData) return []
+    const separator = String.fromCharCode(1)
+    Object.values(thirdPartyData).forEach(({ key, value }) => {
+        if (key.split(separator)[1] === 'profiles') {
+            const profiles = JSON.parse(value)
+            thirdPartyProfiles.push(profiles)
+            return
+        }
+    })
+    return thirdPartyProfiles
+}
