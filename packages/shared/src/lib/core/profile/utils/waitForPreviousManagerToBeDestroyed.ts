c7207f7eafcd123168f2a2af9a354a5c3539e95d
Mark Nardi <nardimark7@gmail.com>
2024-03-14 17:20:53 +0100

    cleanup manager desctruction

--- packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
+++ packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
@@ -1,17 +1,19 @@
 import { get } from 'svelte/store'
-import { sleep } from '@core/utils/os'
-import {
-    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL,
-    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT,
-} from '../constants'
+import { CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMOUT } from '../constants'
 import { isDestroyingManager } from '../stores'
 
 export async function waitForPreviousManagerToBeDestroyed(): Promise<void> {
-    for (let count = 0; count < CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT; count++) {
-        if (!get(isDestroyingManager)) {
-            return Promise.resolve()
-        }
-        await sleep(CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL)
-    }
-    return Promise.reject()
+    return new Promise((resolve, reject) => {
+        const intervalId = setInterval(() => {
+            if (!get(isDestroyingManager)) {
+                clearInterval(intervalId)
+                resolve()
+            }
+        }, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL)
+
+        setTimeout(() => {
+            clearInterval(intervalId)
+            reject()
+        }, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMOUT)
+    })
 }
