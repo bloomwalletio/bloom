0b7b1562b58b74746807626d45be0c981521ca81
Mark Nardi <nardimark7@gmail.com>
2024-03-19 16:37:43 +0100

    chore: cleanup manager destruction (#2142)
    * cleanup manager desctruction
    
    * Revert "cleanup manager desctruction"
    
    This reverts commit c7207f7eafcd123168f2a2af9a354a5c3539e95d.
    
    * make constant more readable
    
    ---------
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
+++ packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
@@ -1,13 +1,11 @@
 import { get } from 'svelte/store'
 import { sleep } from '@core/utils/os'
-import {
-    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL,
-    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT,
-} from '../constants'
+import { CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMEOUT } from '../constants'
 import { isDestroyingManager } from '../stores'
 
 export async function waitForPreviousManagerToBeDestroyed(): Promise<void> {
-    for (let count = 0; count < CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT; count++) {
+    const maxCount = CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMEOUT / CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL
+    for (let count = 0; count < maxCount; count++) {
         if (!get(isDestroyingManager)) {
             return Promise.resolve()
         }
