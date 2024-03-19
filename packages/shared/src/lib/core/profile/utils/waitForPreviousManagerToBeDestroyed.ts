bb8c07c48801723817ebd9423980d601581b6452
Mark Nardi <nardimark7@gmail.com>
2024-03-19 16:27:17 +0100

    Revert "cleanup manager desctruction"
    This reverts commit c7207f7eafcd123168f2a2af9a354a5c3539e95d.

--- packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
+++ packages/shared/src/lib/core/profile/utils/waitForPreviousManagerToBeDestroyed.ts
@@ -1,19 +1,17 @@
 import { get } from 'svelte/store'
-import { CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMOUT } from '../constants'
+import { sleep } from '@core/utils/os'
+import {
+    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL,
+    CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT,
+} from '../constants'
 import { isDestroyingManager } from '../stores'
 
 export async function waitForPreviousManagerToBeDestroyed(): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const intervalId = setInterval(() => {
-            if (!get(isDestroyingManager)) {
-                clearInterval(intervalId)
-                resolve()
-            }
-        }, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL)
-
-        setTimeout(() => {
-            clearInterval(intervalId)
-            reject()
-        }, CHECK_PREVIOUS_MANAGER_IS_DESTROYED_TIMOUT)
-    })
+    for (let count = 0; count < CHECK_PREVIOUS_MANAGER_IS_DESTROYED_MAX_COUNT; count++) {
+        if (!get(isDestroyingManager)) {
+            return Promise.resolve()
+        }
+        await sleep(CHECK_PREVIOUS_MANAGER_IS_DESTROYED_INTERVAL)
+    }
+    return Promise.reject()
 }
