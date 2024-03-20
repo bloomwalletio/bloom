094ee8c7a4c67e9e414889a710e2a50e30765438
Tuditi <daviddetroch@pm.me>
2024-03-20 18:07:39 +0100

    fix: correct testnet name

--- packages/shared/src/lib/auxiliary/third-party/constants/network-name-to-stardust-network-id-map.constant.ts
+++ packages/shared/src/lib/auxiliary/third-party/constants/network-name-to-stardust-network-id-map.constant.ts
@@ -5,5 +5,5 @@ export const NETWORK_NAME_TO_STARDUST_NETWORK_ID_MAP: Record<string, StardustNet
     iota: StardustNetworkId.Iota,
     [StardustNetworkName.Shimmer]: StardustNetworkId.Shimmer,
     [StardustNetworkName.Testnet]: StardustNetworkId.Testnet,
-    'testnet-1': StardustNetworkId.Testnet,
+    testnet: StardustNetworkId.Testnet,
 }
