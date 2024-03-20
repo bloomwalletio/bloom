338a082e58578451410cc6fe2b26a4106a79bfd4
Jean Ribeiro <contact@jeanribeiro.dev>
2024-03-20 13:00:02 -0300

    feat: adds Spinner when Transak window has not loaded yet (#2184)
    * WIP
    
    * feat: adds Spinner when Transak window has not loaded yet

--- packages/desktop/lib/electron/preloads/transak.preload.ts
+++ packages/desktop/lib/electron/preloads/transak.preload.ts
@@ -1,4 +1,33 @@
+import { ipcRenderer } from 'electron'
+
+/* Transak adds the style attribute in <html> with the theme color when it loads */
+function observeStyleChanges(targetNode: Element): { disconnect: () => void } {
+    const config = {
+        attributes: true,
+        attributeFilter: ['style'],
+    }
+
+    const callback = (mutationsList: MutationRecord[], observer: MutationObserver) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                void ipcRenderer.invoke('transak-loaded')
+                observer.disconnect()
+                break
+            }
+        }
+    }
+
+    const observer = new MutationObserver(callback)
+    observer.observe(targetNode, config)
+
+    return {
+        disconnect: () => observer.disconnect(),
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
+    observeStyleChanges(window.document.documentElement)
+
     const appRoot = window.document.getElementById('appRoot')
     if (appRoot) {
         appRoot.style.borderRadius = '16px'
--- packages/desktop/views/dashboard/buy-sell/components/TransakWindowPlaceholder.svelte
+++ packages/desktop/views/dashboard/buy-sell/components/TransakWindowPlaceholder.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { Platform, openUrlInBrowser } from '@core/app'
     import { Pane } from '@ui'
-    import { Button, Icon, IconName, Text } from '@bloomwalletio/ui'
+    import { Button, Icon, IconName, Spinner, Text } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
     import { DISCORD_URL } from '@contexts/settings/constants'
 
     let error: boolean = false
+    let loading: boolean = true
 
     Platform.onEvent('transak-not-loaded', () => (error = true))
+    Platform.onEvent('transak-loaded', () => (loading = false))
 
     function onButtonClick(): void {
         openUrlInBrowser(DISCORD_URL)
@@ -22,5 +24,7 @@
         <Text type="body1">{localize('views.buySell.error.title')}</Text>
         <Text textColor="secondary" align="center">{localize('views.buySell.error.description')}</Text>
         <Button on:click={onButtonClick} text={localize('actions.visitDiscord')} />
+    {:else if loading}
+        <Spinner size="lg" textColor="info" />
     {/if}
 </Pane>
