f62c1e02b91c40aa2a49253bcc71569e2dd103b5
Mark Nardi <nardimark7@gmail.com>
2024-03-21 17:13:25 +0100

    remove persisted transaction store

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -1,7 +1,7 @@
 import { AppStage } from '@core/app/enums'
 
 export const PROFILE_VERSION: Record<AppStage, number> = {
-    [AppStage.ALPHA]: 7,
+    [AppStage.ALPHA]: 8,
     [AppStage.BETA]: 0,
-    [AppStage.PROD]: 4,
+    [AppStage.PROD]: 5,
 }
