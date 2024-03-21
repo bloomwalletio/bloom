146e8f0478862172e8836e1f5fc79e3d1201decb
Tuditi <daviddetroch@pm.me>
2024-03-21 09:40:42 +0100

    chore: update imports

--- packages/shared/src/lib/auxiliary/wallet-connect/utils/getBloomError.ts
+++ packages/shared/src/lib/auxiliary/wallet-connect/utils/getBloomError.ts
@@ -1,15 +1,17 @@
 import { CallbackParameters } from '@auxiliary/wallet-connect/types'
+import { getSdkError } from '@walletconnect/utils'
 import { IError } from '@core/error/interfaces'
 
 export function getBloomError(err: unknown): Omit<CallbackParameters, 'result'> {
-   try {
-     return { error: getSdkError(err) }
-   } catch {
-    return {
-        error: {
-            message: (err as IError).message ?? 'Something went wrong',
-            code: 1000,
-        },
-     }
-   }
+    try {
+        // @ts-expect-error the error types aren't exported from @walletconnect
+        return { error: getSdkError(err) }
+    } catch {
+        return {
+            error: {
+                message: (err as IError).message ?? 'Something went wrong',
+                code: 1000,
+            },
+        }
+    }
 }
