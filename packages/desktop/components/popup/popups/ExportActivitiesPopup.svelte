fb86a39796597504dfabbeb36d5324745ad29a30
Mark Nardi <nardimark7@gmail.com>
2024-03-25 11:57:39 +0100

    update popup

--- packages/desktop/components/popup/popups/ExportActivitiesPopup.svelte
+++ packages/desktop/components/popup/popups/ExportActivitiesPopup.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import { localize } from '@core/i18n'
-    import { Logo } from '@ui'
-    import { Text } from '@bloomwalletio/ui'
     import { closePopup } from '@desktop/auxiliary/popup'
-    import { LogoName } from '@auxiliary/logo'
     import PopupTemplate from '../PopupTemplate.svelte'
 
     const busy = false
@@ -19,7 +16,7 @@
 
 <PopupTemplate
     title={localize('popups.exportActivities.title')}
-    description={localize('popups.exportActivities.bod')}
+    description={localize('popups.exportActivities.body')}
     backButton={{
         text: localize('actions.cancel'),
         onClick: onCancelClick,
@@ -31,15 +28,4 @@
         disabled: busy,
     }}
     {busy}
->
-    <div class="flex flex-col space-y-5">
-        <div class="w-full p-10 bg-surface-1 dark:bg-surface-1-dark rounded-xl flex justify-center content-center">
-            <Logo width="50%" logo={LogoName.Stronghold} />
-        </div>
-        <div class="w-full">
-            <Text textColor="secondary" align="justify">
-                {localize('popups.backupStronghold.body')}
-            </Text>
-        </div>
-    </div>
-</PopupTemplate>
+></PopupTemplate>
