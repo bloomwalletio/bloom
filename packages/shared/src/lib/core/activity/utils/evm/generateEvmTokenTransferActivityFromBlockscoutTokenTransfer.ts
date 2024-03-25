76fac8e655a8ab7f25c579f2c56a614d5dd6bd0b
Mark Nardi <nardimark7@gmail.com>
2024-03-25 12:07:17 +0100

    fix typecheck

--- packages/shared/src/lib/core/activity/utils/evm/generateEvmTokenTransferActivityFromBlockscoutTokenTransfer.ts
+++ packages/shared/src/lib/core/activity/utils/evm/generateEvmTokenTransferActivityFromBlockscoutTokenTransfer.ts
@@ -56,6 +56,10 @@ export async function generateEvmTokenTransferActivityFromBlockscoutTokenTransfe
         await getOrRequestTokenFromPersistedTokens(tokenId, networkId)
     }
 
+    if (!tokenId || !rawAmount) {
+        return
+    }
+
     const baseActivity: BaseEvmActivity = {
         namespace: NetworkNamespace.Evm,
 
