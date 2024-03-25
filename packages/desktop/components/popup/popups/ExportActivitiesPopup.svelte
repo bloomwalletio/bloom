678d2719d0ad27262fffde630650e15b15b0509d
Mark Nardi <nardimark7@gmail.com>
2024-03-25 14:32:51 +0100

    feat: add button to open pop up for managing activities export (#2200)
    * add export activities popup
    
    * improve popup types
    
    * implement activity action menu
    
    * update popup
    
    * add exportCsv feature flag
    
    * fix: code review
    
    ---------
    
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

---
+++ packages/desktop/components/popup/popups/ExportActivitiesPopup.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { localize } from '@core/i18n'
+    import { closePopup } from '@desktop/auxiliary/popup'
+    import PopupTemplate from '../PopupTemplate.svelte'
+
+    const busy = false
+
+    function onCancelClick(): void {
+        closePopup()
+    }
+
+    function onExportClick(): void {
+        // TODO: implement CSV export
+    }
+</script>
+
+<PopupTemplate
+    title={localize('popups.exportActivities.title')}
+    description={localize('popups.exportActivities.body')}
+    backButton={{
+        text: localize('actions.cancel'),
+        onClick: onCancelClick,
+        disabled: busy,
+    }}
+    continueButton={{
+        text: localize('actions.export'),
+        onClick: onExportClick,
+        disabled: busy,
+    }}
+    {busy}
+></PopupTemplate>
