c556c93a38f2a2e1d1f3afddf4fd78d21b921467
Nicole O'Brien <nicole@bloomwallet.io>
2024-03-26 16:14:29 +0000

    ui: polishing
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

--- packages/desktop/components/ImportProfileTile.svelte
+++ packages/desktop/components/ImportProfileTile.svelte
@@ -1,65 +1,61 @@
 <script lang="ts">
     import { IThirdPartyPersistedProfile } from '@auxiliary/third-party'
-    import { Icon, IconName, Text, Tile, Tooltip, getHexColorFromColor } from '@bloomwalletio/ui'
-    import { OnboardingNetworkType } from '@contexts/onboarding'
+    import { Icon, IconName, Pill, Text, Tile, Tooltip } from '@bloomwalletio/ui'
     import { localize } from '@core/i18n'
+    import { getNetworkIdFromOnboardingNetworkType } from '@core/network'
+    import { NetworkAvatar } from '@ui'
 
     export let profile: IThirdPartyPersistedProfile
     export let appName: string
     export let alreadyImported: boolean
     export let needsChrysalisToStardustDbMigration: boolean
-    export let disabled: boolean = alreadyImported || needsChrysalisToStardustDbMigration
     export let hidden: boolean = false
     export let selected: boolean = false
     export let onClick: () => unknown
 
-    const NETWORK_ICON_PROPS = {
-        [OnboardingNetworkType.Iota]: {
-            icon: IconName.Iota,
-            iconColor: '#ffffff',
-            backgroundColor: '#000000',
-        },
-        [OnboardingNetworkType.Shimmer]: {
-            icon: IconName.Shimmer,
-            iconColor: 'blue-900',
-            backgroundColor: 'shimmer',
-        },
-        [OnboardingNetworkType.Testnet]: {
-            icon: IconName.Beaker,
-        },
-    }
-
-    const background = getHexColorFromColor(NETWORK_ICON_PROPS[profile.network.id]?.backgroundColor ?? '#000000')
+    $: networkId = getNetworkIdFromOnboardingNetworkType(profile.network?.id)
 
     let iconElement: HTMLElement
 </script>
 
 {#if !hidden}
-    <Tile width="full" variant="outlined" {onClick} {disabled} {selected} surface={1} class="items-center">
+    <Tile
+        width="full"
+        variant="outlined"
+        onClick={!alreadyImported ? onClick : undefined}
+        disabled={needsChrysalisToStardustDbMigration}
+        {selected}
+        surface={1}
+        class="items-center"
+    >
         <div class="flex justify-center items-center">
-            <icon-container
-                class="w-12 h-12 flex justify-center items-center rounded-xl"
-                style:background-color={background}
-            >
-                <Icon
-                    name={NETWORK_ICON_PROPS[profile.network.id]?.icon}
-                    customColor={NETWORK_ICON_PROPS[profile.network.id]?.iconColor}
-                    size="base"
-                />
-            </icon-container>
+            {#if networkId}
+                <NetworkAvatar {networkId} networkName={profile.network.name} size="md" showTooltip />
+            {/if}
         </div>
         <div class="flex flex-col justify-center flex-1 ml-3">
-            <div class="flex gap-1">
+            <div class="flex gap-2 truncate">
                 <Text type="body2">{profile.name}</Text>
-                <Text type="body2" fontWeight="medium" textColor="secondary">● {appName}</Text>
+                <div>
+                    <Pill color="neutral" compact>{appName}</Pill>
+                </div>
             </div>
             <Text type="base" fontWeight="medium" textColor="secondary">
                 {profile.id}
             </Text>
         </div>
         {#if alreadyImported || needsChrysalisToStardustDbMigration}
-            <icon-container bind:this={iconElement} class="pointer-events-auto cursor-default">
-                <Icon name={alreadyImported ? IconName.SuccessCircle : IconName.WarningCircle} size="sm" />
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <icon-container
+                bind:this={iconElement}
+                on:click|stopPropagation={() => {}}
+                class="pointer-events-auto cursor-default"
+            >
+                <Icon
+                    name={alreadyImported ? IconName.SuccessCircle : IconName.WarningCircle}
+                    textColor={alreadyImported ? 'success' : 'warning'}
+                    size="sm"
+                />
             </icon-container>
         {/if}
     </Tile>
@@ -67,7 +63,7 @@
         <Tooltip
             anchor={iconElement}
             text={localize(
-                `views.importThirdPartyProfiles.warnings.${alreadyImported ? 'alreadyImported' : 'needsMigration'}`
+                `views.onboarding.importThirdPartyProfiles.warnings.${alreadyImported ? 'alreadyImported' : 'needsMigration'}`
             )}
             event="hover"
         />
