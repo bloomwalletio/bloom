783f358f8175e8236f3e6f8b3234560b4b8875e4
Mark Nardi <nardimark7@gmail.com>
2024-03-28 11:47:40 +0100

    fix cancel

--- packages/desktop/lib/electron/apis/electron.api.ts
+++ packages/desktop/lib/electron/apis/electron.api.ts
@@ -205,26 +205,31 @@ const electronApi: IPlatform = {
             })
     },
     async saveTextInFile(fileName: string, extension: string, content: string): Promise<void> {
-        return ipcRenderer
-            .invoke('show-save-dialog', {
-                properties: ['createDirectory', 'showOverwriteConfirmation'],
-                defaultPath: `${fileName}.${extension}`,
-                filters: [
-                    { name: `${extension.toUpperCase()} documents`, extensions: [extension] },
-                    { name: 'All Files', extensions: ['*'] },
-                ],
-            })
-            .then((result) => {
-                if (result.canceled) {
-                    return
-                }
+        return new Promise((resolve, reject) => {
+            void ipcRenderer
+                .invoke('show-save-dialog', {
+                    properties: ['createDirectory', 'showOverwriteConfirmation'],
+                    defaultPath: `${fileName}.${extension}`,
+                    filters: [
+                        { name: `${extension.toUpperCase()} documents`, extensions: [extension] },
+                        { name: 'All Files', extensions: ['*'] },
+                    ],
+                })
+                .then((result) => {
+                    if (result.canceled) {
+                        reject('Canceled by user')
+                        return
+                    }
 
-                try {
-                    fs.writeFileSync(result.filePath, content)
-                } catch (err) {
-                    console.error(err)
-                }
-            })
+                    try {
+                        fs.writeFileSync(result.filePath, content)
+                        resolve()
+                    } catch (err) {
+                        reject(err)
+                    }
+                })
+                .catch((err) => reject(err))
+        })
     },
     trackEvent(eventName: string, eventProperties?: unknown): Promise<void> | undefined {
         if (features.analytics.enabled) {
