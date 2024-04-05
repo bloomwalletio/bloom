121eaaa03caa215f4fedee6150abc2195c1304f8
Mark Nardi <nardimark7@gmail.com>
2024-04-05 12:14:46 +0200

    add migrations

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -1,7 +1,7 @@
 import { AppStage } from '@core/app/enums'
 
 export const PROFILE_VERSION: Record<AppStage, number> = {
-    [AppStage.ALPHA]: 11,
+    [AppStage.ALPHA]: 12,
     [AppStage.BETA]: 1,
-    [AppStage.PROD]: 6,
+    [AppStage.PROD]: 7,
 }
