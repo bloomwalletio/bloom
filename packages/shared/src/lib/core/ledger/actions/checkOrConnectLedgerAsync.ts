d70491627fd2b81edb1e8b503544c9a1507cec0a
Tuditi <daviddetroch@pm.me>
2024-04-05 14:32:48 +0200

    fix: prop name

--- packages/shared/src/lib/core/ledger/actions/checkOrConnectLedgerAsync.ts
+++ packages/shared/src/lib/core/ledger/actions/checkOrConnectLedgerAsync.ts
@@ -20,7 +20,7 @@ export function checkOrConnectLedgerAsync(
                 hideClose: true,
                 props: {
                     ledgerAppName,
-                    onSuccess: resolve,
+                    onContinue: resolve,
                     onCancel: reject,
                 },
             })
