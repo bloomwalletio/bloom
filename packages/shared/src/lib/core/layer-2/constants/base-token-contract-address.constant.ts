ed02e72c8c4ac710dcb20865dcd2f1ca6f03ba04
Mark Nardi <nardimark7@gmail.com>
2024-04-12 16:21:33 +0200

    fix circular dependency

--- packages/shared/src/lib/core/layer-2/constants/base-token-contract-address.constant.ts
+++ packages/shared/src/lib/core/layer-2/constants/base-token-contract-address.constant.ts
@@ -1,5 +1,5 @@
 import { EvmNetworkId } from '@core/network'
-import { SupportedEvmNetworkId } from '@core/network/constants'
+import { SupportedEvmNetworkId } from '@core/network/constants/supported-network-id.constant'
 
 export const BASE_TOKEN_CONTRACT_ADDRESS: Readonly<{ [id in EvmNetworkId]?: string }> = {
     [SupportedEvmNetworkId.ShimmerEvm]: '0x1074010000000000000000000000000000000000',
