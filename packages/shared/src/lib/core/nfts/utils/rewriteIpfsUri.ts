5a105769d9ce75b93356882390127e370f72dc4c
Mark Nardi <nardimark7@gmail.com>
2024-04-18 10:58:55 +0200

    rewrite ipfs gateway regarding

--- packages/shared/src/lib/core/nfts/utils/rewriteIpfsUri.ts
+++ packages/shared/src/lib/core/nfts/utils/rewriteIpfsUri.ts
@@ -1,7 +1,7 @@
 import { getActiveProfile } from '@core/profile/stores'
 
 export function rewriteIpfsUri(ipfsUri: string): string {
-    const ipfsGateway = getActiveProfile().settings.nfts.ipfsGateway
+    const ipfsGateway = getActiveProfile().settings.nfts.ipfsGateways?.filter((gateway) => gateway.isPrimary)[0]?.url
     const url = new URL(ipfsUri)
 
     return ipfsGateway + url.pathname.replace('//', '')
