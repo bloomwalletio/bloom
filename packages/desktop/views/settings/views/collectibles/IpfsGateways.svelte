1b8efe3ea5aee6f6ffe3d9ca5d22b68d27e65b91
Mark Nardi <nardimark7@gmail.com>
2024-04-18 08:26:59 +0200

    improve ipfsgateway table

--- packages/desktop/views/settings/views/collectibles/IpfsGateways.svelte
+++ packages/desktop/views/settings/views/collectibles/IpfsGateways.svelte
@@ -2,7 +2,7 @@
     import { localize } from '@core/i18n'
     import { activeProfile } from '@core/profile/stores'
     import SettingsSection from '../SettingsSection.svelte'
-    import { Pill, Text } from '@bloomwalletio/ui'
+    import { Button, IconName, Pill, Text } from '@bloomwalletio/ui'
     import { IpfsGatewayMenu } from './components'
 
     // function onIpfsGatewayChange(option: IOption): void {
@@ -14,35 +14,33 @@
     title={localize('views.settings.ipfsGateways.title')}
     description={localize('views.settings.ipfsGateways.description')}
 >
-    <ipfs-gateways-table class="max-h-80 flex flex-col overflow-auto">
-        {#each $activeProfile?.settings.nfts.ipfsGateways ?? [] as ipfsGateway}
-            <div class="flex flex-row items-center justify-between">
-                <div class="flex flex-row w-full items-center space-x-4 overflow-hidden">
-                    <Text truncate>
-                        {ipfsGateway.url}
-                    </Text>
-                    {#if ipfsGateway.isPrimary}
-                        <Pill color="info">
-                            {localize('general.primary').toLowerCase()}
-                        </Pill>
-                    {/if}
+    <div class="space-y-4">
+        <ipfs-gateways-table
+            class="max-h-80 flex flex-col overflow-auto w-1/2 divide-y divide-solid divide-stroke dark:divide-stroke-dark"
+        >
+            {#each $activeProfile?.settings.nfts.ipfsGateways ?? [] as ipfsGateway}
+                <div class="flex flex-row items-center justify-between p-2">
+                    <div class="flex flex-row w-full items-center space-x-4 overflow-hidden">
+                        <Text truncate>
+                            {ipfsGateway.url}
+                        </Text>
+                        {#if ipfsGateway.isPrimary}
+                            <Pill color="info">
+                                {localize('general.primary').toLowerCase()}
+                            </Pill>
+                        {/if}
+                    </div>
+                    <IpfsGatewayMenu {...ipfsGateway} />
                 </div>
-                <IpfsGatewayMenu {...ipfsGateway} />
-            </div>
-        {/each}
-    </ipfs-gateways-table>
+            {/each}
+        </ipfs-gateways-table>
+        <Button variant="text" icon={IconName.Plus} text="Add IPFS gateway" on:click={() => {}} />
+    </div>
 </SettingsSection>
 
 <style lang="postcss">
     ipfs-gateways-table {
         @apply border border-solid border-stroke dark:border-stroke-dark;
         @apply rounded-xl;
-        @apply p-2;
-    }
-
-    button {
-        @apply hover:bg-surface-2 dark:hover:bg-surface-2-dark;
-        @apply rounded-lg;
-        @apply p-2;
     }
 </style>
