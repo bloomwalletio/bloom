16db04877ad6067409264ab5f58ff392e2720255
Mark Nardi <nardimark7@gmail.com>
2024-04-19 09:14:41 +0200

    update ipfsgateways

--- packages/shared/src/lib/core/profile/constants/default-ipfs-gateways.constant.ts
+++ packages/shared/src/lib/core/profile/constants/default-ipfs-gateways.constant.ts
@@ -1,10 +1,10 @@
 export const DEFAULT_IPFS_GATEWAYS: { url: string; isPrimary?: boolean }[] = [
     {
-        url: 'https://ipfs.4everland.io/ipfs',
+        url: 'https://ipfs.4everland.io',
         isPrimary: true,
     },
     {
-        url: 'https://ipfs.io/ipfs',
+        url: 'https://ipfs.io',
         isPrimary: false,
     },
 ]
