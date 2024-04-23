2cb1c4e7beb0303816b261fba62f914bca775e56
Nicole O'Brien <nicole@bloomwallet.io>
2024-04-23 18:29:49 +0100

    fix: handle error

--- packages/shared/src/lib/core/wallet/actions/burnNft.ts
+++ packages/shared/src/lib/core/wallet/actions/burnNft.ts
@@ -1,6 +1,7 @@
 import { showNotification } from '@auxiliary/notification'
 import { getSelectedAccount, updateSelectedAccount } from '@core/account/stores/selected-account.store'
 import { processAndAddToActivities } from '@core/activity/actions'
+import { handleError } from '@core/error/handlers'
 import { localize } from '@core/i18n'
 import { getActiveNetworkId } from '@core/network'
 import { updateNftInAllAccountNftsForAccount } from '@core/nfts/actions'
@@ -28,6 +29,8 @@ export async function burnNft(nftId: string): Promise<void> {
             variant: 'success',
             text: localize('notifications.burnNft.success'),
         })
+    } catch (err) {
+        handleError(err)
     } finally {
         updateSelectedAccount({ isTransferring: false })
     }
