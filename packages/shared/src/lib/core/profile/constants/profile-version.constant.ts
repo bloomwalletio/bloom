9e3ce133cb435c4c375a41c37d5942d96d2566de
Mark Nardi <nardimark7@gmail.com>
2024-04-23 09:18:15 +0200

    add migrations

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -1,7 +1,7 @@
 import { AppStage } from '@core/app/enums'
 
 export const PROFILE_VERSION: Record<AppStage, number> = {
-    [AppStage.ALPHA]: 14,
+    [AppStage.ALPHA]: 15,
     [AppStage.BETA]: 1,
-    [AppStage.PROD]: 7,
+    [AppStage.PROD]: 6,
 }
---
+++ packages/shared/src/lib/core/profile/migrations/prod/prod-profile-migration-7-to-8.ts
@@ -0,0 +1,20 @@
+import { DappVerification } from '@auxiliary/wallet-connect/enums'
+import { persistDapp, persistedDappNamespaces } from '@auxiliary/wallet-connect/stores'
+import { IPersistedProfile } from '@core/profile/interfaces'
+import { get } from 'svelte/store'
+
+export function prodProfileMigration7To8(existingProfile: unknown): Promise<void> {
+    const profile = existingProfile as IPersistedProfile
+
+    const namespaces = get(persistedDappNamespaces)[profile.id] ?? {}
+    for (const dappUrl of Object.keys(namespaces)) {
+        persistDapp(dappUrl, DappVerification.Unknown, namespaces[dappUrl])
+    }
+
+    persistedDappNamespaces.update((state) => {
+        delete state[profile.id]
+        return state
+    })
+
+    return Promise.resolve()
+}
