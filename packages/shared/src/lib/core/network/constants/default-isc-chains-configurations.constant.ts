75c5a95a11d717cd6fd97427a5608b6e92de46f8
Mark Nardi <nardimark7@gmail.com>
2024-04-24 09:16:40 +0200

    fix default network configs

--- packages/shared/src/lib/core/network/constants/default-isc-chains-configurations.constant.ts
+++ packages/shared/src/lib/core/network/constants/default-isc-chains-configurations.constant.ts
@@ -1,32 +1,32 @@
 import { EvmNetworkType, NetworkNamespace, ChainId } from '../enums'
 import { IIscChainConfiguration } from '../interfaces'
-import { StardustNetworkId } from '../types'
+import { EvmNetworkId, StardustNetworkId } from '../types'
 import { DEFAULT_COIN_TYPE } from './default-coin-type.constant'
 import { SupportedNetworkId } from './supported-network-id.constant'
 
 export const DEFAULT_ISC_CHAINS_CONFIGURATIONS: Readonly<{ [id in StardustNetworkId]?: IIscChainConfiguration }> = {
-    [SupportedNetworkId.Shimmer]: {
-        id: SupportedNetworkId.ShimmerEvm,
+    [SupportedNetworkId.Shimmer as StardustNetworkId]: {
+        id: SupportedNetworkId.ShimmerEvm as EvmNetworkId,
         type: EvmNetworkType.Isc,
         name: 'Shimmer EVM',
         chainId: ChainId.ShimmerEvm,
         namespace: NetworkNamespace.Evm,
-        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.ShimmerEvm],
+        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.ShimmerEvm] ?? 0,
         aliasAddress: 'smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s',
         rpcEndpoint: 'https://json-rpc.evm.shimmer.network/',
         apiEndpoint: 'https://api.evm.shimmer.network/',
         explorerUrl: 'https://explorer.evm.shimmer.network/',
-    } as IIscChainConfiguration,
-    [SupportedNetworkId.Testnet]: {
-        id: SupportedNetworkId.TestnetEvm,
+    },
+    [SupportedNetworkId.Testnet as StardustNetworkId]: {
+        id: SupportedNetworkId.TestnetEvm as EvmNetworkId,
         type: EvmNetworkType.Isc,
         name: 'Testnet EVM',
         chainId: ChainId.TestnetEvm,
         namespace: NetworkNamespace.Evm,
-        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.TestnetEvm],
+        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.TestnetEvm] ?? 0,
         aliasAddress: 'rms1ppp00k5mmd2m8my8ukkp58nd3rskw6rx8l09aj35984k74uuc5u2cywn3ex',
         rpcEndpoint: 'https://json-rpc.evm.testnet.shimmer.network/',
         apiEndpoint: 'https://api.evm.testnet.shimmer.network/',
         explorerUrl: 'https://explorer.evm.testnet.shimmer.network/',
-    } as IIscChainConfiguration,
+    },
 }
--- packages/shared/src/lib/core/network/constants/default-l1-evm-network-configurations.constant.ts
+++ packages/shared/src/lib/core/network/constants/default-l1-evm-network-configurations.constant.ts
@@ -1,4 +1,4 @@
-import { ChainId } from '../enums'
+import { ChainId, EvmNetworkType, NetworkNamespace } from '../enums'
 import { IPureEvmNetworkConfiguration } from '../interfaces'
 import { EvmNetworkId } from '../types'
 import { DEFAULT_COIN_TYPE } from './default-coin-type.constant'
@@ -8,23 +8,27 @@ import { DEFAULT_EXPLORER_URLS } from './default-explorer-urls.constant'
 export const DEFAULT_L1_EVM_NETWORK_CONFIGURATION: Readonly<{
     [key in EvmNetworkId]: IPureEvmNetworkConfiguration
 }> = {
-    [SupportedNetworkId.Ethereum]: <IPureEvmNetworkConfiguration>{
+    [SupportedNetworkId.Ethereum as EvmNetworkId]: {
+        type: EvmNetworkType.PureEvm,
         name: 'Ethereum',
         symbol: 'ETH',
         ticker: 'ETH',
-        id: SupportedNetworkId.Ethereum,
+        id: SupportedNetworkId.Ethereum as EvmNetworkId,
         chainId: ChainId.Ethereum,
-        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.Ethereum],
+        namespace: NetworkNamespace.Evm,
+        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.Ethereum] ?? 0,
         explorerUrl: DEFAULT_EXPLORER_URLS[SupportedNetworkId.Ethereum],
         rpcEndpoint: 'https://ethereum-rpc.publicnode.com',
     },
-    [SupportedNetworkId.Sepolia]: <IPureEvmNetworkConfiguration>{
+    [SupportedNetworkId.Sepolia as EvmNetworkId]: {
+        type: EvmNetworkType.PureEvm,
         name: 'Sepolia Testnet',
         symbol: 'ETH',
         ticker: 'ETH',
-        id: SupportedNetworkId.Sepolia,
+        id: SupportedNetworkId.Sepolia as EvmNetworkId,
         chainId: ChainId.Sepolia,
-        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.Sepolia],
+        namespace: NetworkNamespace.Evm,
+        coinType: DEFAULT_COIN_TYPE[SupportedNetworkId.Sepolia] ?? 0,
         explorerUrl: DEFAULT_EXPLORER_URLS[SupportedNetworkId.Sepolia],
         rpcEndpoint: 'https://ethereum-sepolia-rpc.publicnode.com',
     },
