6a331e4fbb3852f10412c695e3f51741c6d9d34b
Nicole O'Brien <nicole@bloomwallet.io>
2024-04-26 10:09:58 +0100

    fix: tests

--- packages/shared/src/lib/core/nfts/constants/collectibles-tabs.constant.ts
+++ packages/shared/src/lib/core/nfts/constants/collectibles-tabs.constant.ts
@@ -3,5 +3,5 @@ import features from '@features/features'
 
 export const COLLECTIBLES_TABS: KeyValue<string>[] = [
     { key: 'collectibles', value: 'Collectibles' },
-    ...(features.collectibles.collections.enabled ? [{ key: 'collections', value: 'Collections' }] : []),
+    ...(features.collectibles?.collections.enabled ? [{ key: 'collections', value: 'Collections' }] : []),
 ]
