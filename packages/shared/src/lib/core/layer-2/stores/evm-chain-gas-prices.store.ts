ebb48d2ec20ea7aa9d7f3e077ff16e90815c3547
Mark Nardi <nardimark7@gmail.com>
2024-04-27 13:50:46 +0200

    remove remaininng store

--- packages/shared/src/lib/core/layer-2/stores/evm-chain-gas-prices.store.ts
+++
@@ -1,9 +0,0 @@
-import { get, writable } from 'svelte/store'
-import { NetworkId } from '@core/network/types'
-import { EvmChainGasPrices } from '../types'
-
-export const evmChainGasPrices = writable<EvmChainGasPrices>({})
-
-export function getEvmChainGasPrice(networkId: NetworkId): bigint | undefined {
-    return get(evmChainGasPrices)[networkId]
-}
