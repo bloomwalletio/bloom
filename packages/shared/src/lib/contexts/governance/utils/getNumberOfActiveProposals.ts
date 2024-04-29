b3c344afd176960aea24fd2674a658611a5e3a9a
Mark Nardi <nardimark7@gmail.com>
2024-04-29 08:51:56 +0200

    fix format

--- packages/shared/src/lib/contexts/governance/utils/getNumberOfActiveProposals.ts
+++ packages/shared/src/lib/contexts/governance/utils/getNumberOfActiveProposals.ts
@@ -8,7 +8,7 @@ export function getNumberOfActiveProposals(): number {
     const proposals = get(registeredProposalsForSelectedAccount)
     const currentMilestone = get(getL1Network().currentMilestone)
     const activeProposals = Object.values(proposals ?? {}).filter((proposal) => {
-        const { status } = getProposalWithStatus(proposal,currentMilestone)
+        const { status } = getProposalWithStatus(proposal, currentMilestone)
         return isProposalActive(status)
     })
     return activeProposals.length
