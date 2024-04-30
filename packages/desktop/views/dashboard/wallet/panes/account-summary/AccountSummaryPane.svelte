0c12d2a9107214e210718bf1f2108fc6c6fe52da
Mark Nardi <nardimark7@gmail.com>
2024-04-30 09:59:30 +0200

    fix: add fallback icon for networks (#2397)
    * add fallback icon for networks
    
    * fix network summary columns

--- packages/desktop/views/dashboard/wallet/panes/account-summary/AccountSummaryPane.svelte
+++ packages/desktop/views/dashboard/wallet/panes/account-summary/AccountSummaryPane.svelte
@@ -5,15 +5,18 @@
     import { networks } from '@core/network'
 
     export let account: IAccountState
+
+    const GRID_COLS = {
+        0: 'grid-cols-1',
+        1: 'grid-cols-2',
+        2: 'grid-cols-3',
+        3: 'grid-cols-4',
+    }
 </script>
 
 <Pane
     classes="
-        w-full flex shrink-0 grid {$networks.length > 0
-        ? $networks.length > 1
-            ? 'grid-cols-4'
-            : 'grid-cols-3'
-        : 'grid-cols-2'}
+        w-full flex shrink-0 grid {GRID_COLS[$networks.length] ?? GRID_COLS[3]}
         bg-surface dark:bg-surface-dark
         border border-solid border-stroke dark:border-stroke-dark
         divide-x divide-solid divide-stroke dark:divide-stroke-dark
