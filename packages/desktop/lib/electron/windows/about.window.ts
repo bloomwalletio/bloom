94c4dfa615d1119df7c9cb4f08cad1846c0e5d9a
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-02 23:06:43 +0100

    centre about window on main window

--- packages/desktop/lib/electron/windows/about.window.ts
+++ packages/desktop/lib/electron/windows/about.window.ts
@@ -7,6 +7,9 @@ import { APP_PATH } from '../constants/app-path.constant'
 const ABOUT_WINDOW_PRELOAD_FILE_NAME = 'about.preload.js'
 const ABOUT_WINDOW_HTML_FILE_NAME = 'about.html'
 
+const ABOUT_WINDOW_WIDTH = 380
+const ABOUT_WINDOW_HEIGHT = 230
+
 export abstract class AboutWindow {
     /**
      * Create about window
@@ -14,15 +17,17 @@ export abstract class AboutWindow {
      */
     static open(this: void): BrowserWindow {
         if (windows.about !== null) {
+            windows.about.setBounds(AboutWindow.calculatePosition())
             windows.about.focus()
             return windows.about
         }
 
         const preloadPath = path.join(APP_PATH, app.isPackaged ? 'public/build' : '', ABOUT_WINDOW_PRELOAD_FILE_NAME)
+        const htmlPath = path.join(APP_PATH, app.isPackaged ? 'public' : '..', ABOUT_WINDOW_HTML_FILE_NAME)
 
         windows.about = new BrowserWindow({
-            width: 380,
-            height: 230,
+            width: ABOUT_WINDOW_WIDTH,
+            height: ABOUT_WINDOW_HEIGHT,
             useContentSize: true,
             titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : 'hidden',
             /**
@@ -40,9 +45,8 @@ export abstract class AboutWindow {
                 ...DEFAULT_WEB_PREFERENCES,
                 preload: preloadPath,
             },
+            ...AboutWindow.calculatePosition(),
         })
-
-        const htmlPath = path.join(APP_PATH, app.isPackaged ? 'public' : '..', ABOUT_WINDOW_HTML_FILE_NAME)
 
         void windows.about.loadFile(htmlPath)
 
@@ -60,5 +64,19 @@ export abstract class AboutWindow {
             windows.about.close()
             windows.about = null
         }
+    }
+
+    static calculatePosition(): { x: number; y: number } {
+        const mainWindowBounds = BrowserWindow.getFocusedWindow()?.getBounds() ?? { x: 0, y: 0, width: 0, height: 0 }
+        const mainWindowCenterX = mainWindowBounds?.x + mainWindowBounds?.width / 2
+        const mainWindowCenterY = mainWindowBounds?.y + mainWindowBounds?.height / 2
+
+        const ABOUT_WINDOW_WIDTH = 380
+        const ABOUT_WINDOW_HEIGHT = 230
+
+        const x = mainWindowCenterX - ABOUT_WINDOW_WIDTH / 2
+        const y = mainWindowCenterY - ABOUT_WINDOW_HEIGHT / 2
+
+        return { x, y }
     }
 }
