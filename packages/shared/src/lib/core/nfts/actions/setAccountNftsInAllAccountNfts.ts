7142e108123ce8b5daf22979bea287cb15ebf887
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-02 10:18:42 +0100

    refactor: replace `setAccountNftsInAllAccountNfts` with `setNftsForAccount`

--- packages/shared/src/lib/core/nfts/actions/setAccountNftsInAllAccountNfts.ts
+++
@@ -1,9 +0,0 @@
-import { activeProfileNftsPerAccount } from '../stores'
-import { Nft } from '../interfaces'
-
-export function setAccountNftsInAllAccountNfts(accountIndex: number, accountNfts: Nft[]): void {
-    activeProfileNftsPerAccount.update((state) => {
-        state[accountIndex] = accountNfts
-        return state
-    })
-}
