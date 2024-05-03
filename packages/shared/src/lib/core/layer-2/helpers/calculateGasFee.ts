8300877c4b2a4de375b2459d4eadf93019da29fa
Mark Nardi <nardimark7@gmail.com>
2024-05-03 13:31:27 +0200

    use correct function to calculate gas

--- packages/shared/src/lib/core/layer-2/helpers/calculateGasFee.ts
+++ packages/shared/src/lib/core/layer-2/helpers/calculateGasFee.ts
@@ -1,16 +1,16 @@
 import { BigIntLike } from '@ethereumjs/util'
 import { Converter } from '@core/utils'
-import { getAmountForEvmTransaction } from './getEvmAmount'
+import { getAmountFromEvmTransaction } from './getEvmAmount'
 import { NetworkType } from '@core/network'
 
-export function calculateGasFeeInGlow(
+export function calculateGasFee(
     gasAmount: BigIntLike,
     gasPriceInWei: BigIntLike | undefined,
     networkType: NetworkType
 ): bigint {
     if (gasAmount && gasPriceInWei) {
         const totalPriceInWei = Converter.bigIntLikeToBigInt(gasAmount) * Converter.bigIntLikeToBigInt(gasPriceInWei)
-        const totalPriceInGlow = getAmountForEvmTransaction(totalPriceInWei, networkType)
+        const totalPriceInGlow = getAmountFromEvmTransaction(totalPriceInWei, networkType)
         return totalPriceInGlow
     } else {
         return BigInt(0)
