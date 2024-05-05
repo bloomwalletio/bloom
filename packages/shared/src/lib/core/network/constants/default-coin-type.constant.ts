cc43b3df71ff49b24504f57c1e8cbc4a5f30490c
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-05 21:51:33 +0100

    add evm network coin type

--- packages/shared/src/lib/core/network/constants/default-coin-type.constant.ts
+++ packages/shared/src/lib/core/network/constants/default-coin-type.constant.ts
@@ -10,6 +10,7 @@ export const DEFAULT_COIN_TYPE: Readonly<{ [id in NetworkId]: number }> = {
     [SupportedNetworkId.Iota]: IOTA_COIN_TYPE,
     [SupportedNetworkId.Shimmer]: SHIMMER_COIN_TYPE,
     [SupportedNetworkId.Testnet]: TEST_COIN_TYPE,
+    [SupportedNetworkId.IotaEvm]: ETHEREUM_COIN_TYPE,
     [SupportedNetworkId.ShimmerEvm]: ETHEREUM_COIN_TYPE,
     [SupportedNetworkId.TestnetEvm]: ETHEREUM_COIN_TYPE,
     [SupportedNetworkId.Ethereum]: ETHEREUM_COIN_TYPE,
