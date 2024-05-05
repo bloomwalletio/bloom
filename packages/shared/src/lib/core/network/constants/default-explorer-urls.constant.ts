a94129a7857efa1091a46f5debd5fcac5a65f69e
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-05 21:47:43 +0100

    add evm explorer url

--- packages/shared/src/lib/core/network/constants/default-explorer-urls.constant.ts
+++ packages/shared/src/lib/core/network/constants/default-explorer-urls.constant.ts
@@ -5,8 +5,7 @@ export const DEFAULT_EXPLORER_URLS: Readonly<{ [key in NetworkId]?: string }> = 
     [SupportedNetworkId.Iota]: 'https://explorer.iota.org',
     [SupportedNetworkId.Shimmer]: 'https://explorer.shimmer.network',
     [SupportedNetworkId.Testnet]: 'https://explorer.shimmer.network',
-    [SupportedNetworkId.IotaEvm]:
-        'https://github.com/bloomwalletio/bloom-private/pull/17/files#diff-4307059d77fecb4adb7f81099c9404995ef9f4a617496ee0e1fb7cb0eb46c0ae',
+    [SupportedNetworkId.IotaEvm]: 'https://explorer.evm.iota.org',
     [SupportedNetworkId.Ethereum]: 'https://eth.blockscout.com',
     [SupportedNetworkId.ShimmerEvm]: 'https://explorer.evm.shimmer.network',
     [SupportedNetworkId.TestnetEvm]: 'https://explorer.evm.testnet.shimmer.network',
