86922119602ecb4d3e6fe256679e01bb3aff2547
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-08 23:17:40 +0100

    enhancement: delete profile while logged out

--- packages/desktop/views/login/components/ProfileCardActionsMenu.svelte
+++ packages/desktop/views/login/components/ProfileCardActionsMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { IconName, Menu } from '@bloomwalletio/ui'
+    import { deleteProfile } from '@contexts/settings/actions'
     import { localize } from '@core/i18n'
     import { IPersistedProfile } from '@core/profile'
     import { PopupId, openPopup } from '@desktop/auxiliary/popup'
@@ -18,6 +19,23 @@
             false
         )
     }
+
+    function onDeleteClick(): void {
+        openPopup(
+            {
+                id: PopupId.Confirmation,
+                props: {
+                    variant: 'danger',
+                    title: localize('popups.deleteProfile.title'),
+                    alert: { variant: 'warning', text: localize('popups.deleteProfile.confirmation') },
+                    confirmText: localize('actions.delete'),
+                    onConfirm: () => deleteProfile(profile.id),
+                },
+            },
+            false,
+            false
+        )
+    }
 </script>
 
 <Menu
@@ -26,6 +44,12 @@
             icon: IconName.Tool2,
             title: localize('popups.profileDiagnostics.title'),
             onClick: onDiagnosticsClick,
+        },
+        {
+            variant: 'danger',
+            icon: IconName.Trash,
+            title: localize('popups.deleteProfile.title'),
+            onClick: onDeleteClick,
         },
     ]}
 />
--- packages/shared/src/lib/contexts/settings/actions/deleteProfile.ts
+++ packages/shared/src/lib/contexts/settings/actions/deleteProfile.ts
@@ -1,22 +1,18 @@
 import { AppContext } from '@core/app/enums'
 import { removeProfileFolder } from '@core/profile'
 import { logout, removeAllProfileData } from '@core/profile/actions'
-import { activeProfileId, profiles } from '@core/profile/stores'
+import { profiles } from '@core/profile/stores'
 import { routerManager } from '@core/router/stores'
 import { get } from 'svelte/store'
+import { closePopup } from '../../../../../../desktop/lib/auxiliary/popup'
 
 /**
  * It removes the active profile from the app's list of profiles, removes the profile's directory from
  * the file system, and logs the user out
  * @returns A Promise that resolves to void.
  */
-export async function deleteProfile(): Promise<void> {
+export async function deleteProfile(profileId: string): Promise<void> {
     try {
-        const _activeProfileId = get(activeProfileId)
-        if (!_activeProfileId) {
-            return
-        }
-
         /**
          * CAUTION: Logout must occur before the profile is removed
          * from the Svelte store list of profiles.
@@ -27,14 +23,15 @@ export async function deleteProfile(): Promise<void> {
          * CAUTION: The profile and its data must be removed from the
          * app's list of profiles that lives as a Svelte store.
          */
-        removeAllProfileData(_activeProfileId)
+        removeAllProfileData(profileId)
 
         /**
          * CAUTION: This removes the actual directory for the profile,
          * so it should occur last.
          */
-        await removeProfileFolder(_activeProfileId)
+        await removeProfileFolder(profileId)
 
+        closePopup()
         /**
          * NOTE: If there are no more profiles, then the user should be
          * routed to the welcome screen.
