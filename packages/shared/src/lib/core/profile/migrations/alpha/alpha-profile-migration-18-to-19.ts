6b0ce35ccbb7bc58ce802a79ee30a90be969f907
Mark Nardi <nardimark7@gmail.com>
2024-05-10 12:06:04 +0200

    fix migrations

--- packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-18-to-19.ts
+++ packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-18-to-19.ts
@@ -1,12 +1,12 @@
 import { DEFAULT_EXPLORER_URLS } from '@core/network'
 import { IPersistedProfile } from '@core/profile/interfaces'
 
-export function alphaProfileMigration17To18(existingProfile: unknown): Promise<void> {
+export function alphaProfileMigration18To19(existingProfile: unknown): Promise<void> {
     const profile = existingProfile as IPersistedProfile
 
     profile.network = {
         ...profile.network,
-        explorerUrl: DEFAULT_EXPLORER_URLS[profile.network.id],
+        explorerUrl: structuredClone(DEFAULT_EXPLORER_URLS[profile.network.id]),
     }
 
     return Promise.resolve()
