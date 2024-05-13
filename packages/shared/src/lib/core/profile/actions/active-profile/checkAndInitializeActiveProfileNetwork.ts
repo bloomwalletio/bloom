2d94b112b2a4cfc57ea93d0c993ce703bcd62de1
Mark Nardi <nardimark7@gmail.com>
2024-05-13 17:20:26 +0200

    merge conflict

--- packages/shared/src/lib/core/profile/actions/active-profile/checkAndInitializeActiveProfileNetwork.ts
+++ packages/shared/src/lib/core/profile/actions/active-profile/checkAndInitializeActiveProfileNetwork.ts
@@ -1,13 +1,13 @@
 import { get } from 'svelte/store'
 import { localize } from '@core/i18n'
-import { getAndUpdateNodeInfo } from '@core/network/actions'
 import { activeProfile } from '@core/profile/stores'
 import { NetworkNamespace, initializeNetworks } from '@core/network'
+import { getNodeInfo } from '@core/profile-manager'
 
 export async function checkAndInitializeActiveProfileNetwork(): Promise<void> {
     const $activeProfile = get(activeProfile)
     const existingNetwork = $activeProfile?.network
-    const nodeInfoResponse = await getAndUpdateNodeInfo(true)
+    const nodeInfoResponse = await getNodeInfo()
     if (!nodeInfoResponse) {
         throw new Error(localize('error.network.mismatch'))
     }
