2266bd6a676654b24eaf3b1aa97e0980dfc4ffd3
Nicole O'Brien <nicole@bloomwallet.io>
2024-05-15 11:32:08 +0100

    fix: iota testnet constants

--- packages/shared/src/lib/core/network/enums/chain-id.enum.ts
+++ packages/shared/src/lib/core/network/enums/chain-id.enum.ts
@@ -1,8 +1,8 @@
 export enum ChainId {
     IotaEvm = '8822',
     ShimmerEvm = '148',
-    IotaTestnetEvm = '1073',
-    TestnetEvm = '1075',
+    IotaTestnetEvm = '1075',
+    TestnetEvm = '1073',
     Ethereum = '1',
     Sepolia = '11155111',
 }
