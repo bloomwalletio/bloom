d2a535933e480163ee52c4b9720a9600e67a9057
Mark Nardi <nardimark7@gmail.com>
2024-05-17 09:07:30 +0200

    fix linter

--- packages/shared/src/lib/core/layer-2/utils/addGasBuffer.ts
+++ packages/shared/src/lib/core/layer-2/utils/addGasBuffer.ts
@@ -1,5 +1,5 @@
 const GAS_LIMIT_MULTIPLIER = BigInt(110)
 
 export function addGasBuffer(estimatedGas: bigint): bigint {
-    return estimatedGas * GAS_LIMIT_MULTIPLIER / BigInt(100)
+    return (estimatedGas * GAS_LIMIT_MULTIPLIER) / BigInt(100)
 }
