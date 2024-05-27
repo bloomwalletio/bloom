037bec3e0d26e349d36dffbd95d3b85431aa978e
Tuditi <daviddetroch@pm.me>
2024-05-27 15:00:14 +0200

    feat: confirmationPill -> pending pill

--- packages/shared/src/components/pills/ConfirmationPill.svelte
+++ packages/shared/src/components/pills/ConfirmationPill.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Color, Pill } from '@bloomwalletio/ui'
+    import { Pill } from '@bloomwalletio/ui'
     import { Activity, InclusionState } from '@core/activity'
     import { localize } from '@core/i18n'
     import { getEvmNetwork } from '@core/network/stores'
@@ -7,26 +7,7 @@
 
     export let activity: Activity
 
-    let text = ''
-    let color: Color = 'danger'
     let tooltip = ''
-
-    $: setConfirmationPill(activity)
-    function setConfirmationPill(_activity: Activity): void {
-        switch (_activity.inclusionState) {
-            case InclusionState.Confirmed:
-                text = localize('general.confirmed')
-                color = 'success'
-                break
-            case InclusionState.Pending:
-                text = localize('general.pending')
-                color = 'warning'
-                break
-            default:
-                text = localize('general.failed')
-                color = 'danger'
-        }
-    }
 
     async function onHover(): Promise<void> {
         tooltip = 'loading...'
@@ -41,8 +22,10 @@
     }
 </script>
 
-<div on:mouseover={onHover}>
-    <Pill {color} compact tooltipEvent="hover" {tooltip}>
-        {text}
-    </Pill>
-</div>
+{#if activity.inclusionState === InclusionState.Pending}
+    <div role="contentinfo" on:focus={onHover} on:mouseover={onHover}>
+        <Pill color="warning" compact tooltipEvent="hover" {tooltip}>
+            {localize('general.pending')}
+        </Pill>
+    </div>
+{/if}
