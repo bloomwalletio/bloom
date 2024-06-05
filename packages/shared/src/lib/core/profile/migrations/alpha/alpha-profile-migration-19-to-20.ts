ec3e96b2cc390338cb50144d0e03509f8afeda6e
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-05 21:19:45 +0100

    release: desktop 1.0.4 (#2595)
    * chore: onboarding feature flags and migrations
    
    * chore: increment version for alpha
    
    * chore: increment version for prod
    
    * add alpha migration from develop for evm

--- packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-19-to-20.ts
+++ packages/shared/src/lib/core/profile/migrations/alpha/alpha-profile-migration-19-to-20.ts
@@ -1,10 +1,10 @@
-import { IPersistedProfile } from '../../interfaces'
-import { migrateEvmContacts } from '../../migrations/actions/migrateEvmContacts'
+import { IPersistedProfile } from '@core/profile/interfaces'
+import { addDefaultIscChainsToIotaProfiles } from '../actions/addDefaultIscChainsToIotaProfiles'
 
 export function alphaProfileMigration19To20(existingProfile: unknown): Promise<void> {
     const profile = existingProfile as IPersistedProfile
 
-    migrateEvmContacts(profile)
+    addDefaultIscChainsToIotaProfiles(profile)
 
     return Promise.resolve()
 }
