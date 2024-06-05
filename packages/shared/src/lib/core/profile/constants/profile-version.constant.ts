af30c7491150fbf5afe451202d4f312eb5126638
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-05 17:21:18 +0100

    add alpha migration from develop for evm

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -1,7 +1,7 @@
 import { AppStage } from '@core/app/enums'
 
 export const PROFILE_VERSION: Record<AppStage, number> = {
-    [AppStage.ALPHA]: 20,
+    [AppStage.ALPHA]: 21,
     [AppStage.BETA]: 1,
     [AppStage.PROD]: 11,
 }
