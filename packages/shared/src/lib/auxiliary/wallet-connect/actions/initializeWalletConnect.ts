686ca164ef18bcad9fd5942f04639edcd7f3b42b
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-13 08:19:10 +0100

    release: desktop 1.0.5 (#2608)
    * chore: increment version to desktop-1.0.5-alpha-1
    
    * update pill component
    
    * handle crash in auto update manager
    
    * revert project id change
    
    * fix compile error
    
    * add walletconnect
    
    * fix walletconnect core
    
    * chore increment alpha to 5
    
    * chore: increment desktop version to `1.0.5`
    
    ---------
    
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/shared/src/lib/auxiliary/wallet-connect/actions/initializeWalletConnect.ts
+++ packages/shared/src/lib/auxiliary/wallet-connect/actions/initializeWalletConnect.ts
@@ -1,31 +1,35 @@
 import { handleError } from '@core/error/handlers'
 import { Web3Wallet } from '@walletconnect/web3wallet'
 import { get } from 'svelte/store'
-import { WALLET_CONNECT_CORE } from '../constants/wallet-connect-core.constant'
+import { Core } from '@walletconnect/core'
 import { WALLET_METADATA } from '../constants'
 import { onSessionDelete, onSessionProposal, onSessionRequest } from '../handlers'
 import { walletClient } from '../stores/wallet-client.store'
 import { setConnectedDapps } from '../stores/connected-dapps.store'
 import { isFeatureEnabled } from '@lib/features/utils'
 import { notificationsManager } from '../notifications'
+import { ICore } from '@walletconnect/types'
 
 export async function initializeWalletConnect(): Promise<void> {
+    const core = new Core({
+        projectId: process.env.WALLETCONNECT_PROJECT_ID ?? '41511f9b50c46a80cdf8bd1a3532f2f9',
+    })
     if (isFeatureEnabled('walletConnect.web3Wallet')) {
-        await initializeWalletClient()
+        await initializeWalletClient(core)
     }
     if (isFeatureEnabled('walletConnect.notifications')) {
-        await notificationsManager.init()
+        await notificationsManager.init(core)
     }
 }
 
-async function initializeWalletClient(): Promise<void> {
+async function initializeWalletClient(core: ICore): Promise<void> {
     if (get(walletClient)) {
         return
     }
 
     try {
         const _walletClient = await Web3Wallet.init({
-            core: WALLET_CONNECT_CORE,
+            core,
             metadata: WALLET_METADATA,
         })
         walletClient.set(_walletClient)
