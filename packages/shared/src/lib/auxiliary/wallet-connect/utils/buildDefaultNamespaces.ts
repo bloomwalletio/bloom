b753ffbb345b01bffdd8dcb0dfe75b7db7d8e566
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-27 12:03:09 +0100

    fix: dapp connections for multiple evm chains

--- packages/shared/src/lib/auxiliary/wallet-connect/utils/buildDefaultNamespaces.ts
+++ packages/shared/src/lib/auxiliary/wallet-connect/utils/buildDefaultNamespaces.ts
@@ -14,17 +14,18 @@ export function buildDefaultNamespaces(): SupportedNamespaces {
             continue
         }
         if (!namespace[network.namespace]) {
-            const accounts = get(activeAccounts)
-                .map((account) => getCaip10AddressForAccount(account, network.id) as string)
-                .filter(Boolean)
             namespace[network.namespace] = {
                 chains: [],
                 methods: ALL_SUPPORTED_METHODS,
                 events: SUPPORTED_EVENTS,
-                accounts,
+                accounts: [],
             }
         }
         namespace[network.namespace].chains.push(network.id)
+        const accounts = get(activeAccounts)
+            .map((account) => getCaip10AddressForAccount(account, network.id) as string)
+            .filter(Boolean)
+        namespace[network.namespace].accounts.push(...accounts)
     }
     return namespace
 }
