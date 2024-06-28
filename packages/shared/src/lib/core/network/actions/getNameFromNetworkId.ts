ad1adc2ce168141a4ab3cf250834a828773a92bb
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-28 10:56:57 +0100

    fix: get network name form evm networks too (#2679)

--- packages/shared/src/lib/core/network/actions/getNameFromNetworkId.ts
+++ packages/shared/src/lib/core/network/actions/getNameFromNetworkId.ts
@@ -1,12 +1,27 @@
-import { DEFAULT_ISC_CHAINS_CONFIGURATIONS } from '../constants'
+import {
+    DEFAULT_EVM_NETWORK_CONFIGURATIONS_FOR_STARDUST_NETWORK,
+    DEFAULT_ISC_CHAINS_CONFIGURATIONS,
+} from '../constants'
 import { getNetwork } from '../stores'
 import { NetworkId } from '../types'
 
 export function getNameFromNetworkId(networkId: NetworkId): string | undefined {
-    const networkName = getNetwork(networkId)?.name
-    if (networkName) {
-        return networkName
+    const knownNetworkName = getNetwork(networkId)?.name
+    if (knownNetworkName) {
+        return knownNetworkName
     }
 
-    return Object.values(DEFAULT_ISC_CHAINS_CONFIGURATIONS).find((config) => config?.id === networkId)?.name
+    const potentialIscChainName = Object.values(DEFAULT_ISC_CHAINS_CONFIGURATIONS).find(
+        (config) => config?.id === networkId
+    )?.name
+
+    if (potentialIscChainName) {
+        return potentialIscChainName
+    }
+
+    const potentialEvmChainName = Object.values(DEFAULT_EVM_NETWORK_CONFIGURATIONS_FOR_STARDUST_NETWORK)
+        .flat()
+        .find((config) => config?.id === networkId)?.name
+
+    return potentialEvmChainName
 }
