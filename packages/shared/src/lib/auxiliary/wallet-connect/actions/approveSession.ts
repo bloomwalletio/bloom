9358ca2fe1d4f4da8c3ffedaeec140cc90a1f160
Nicole O'Brien <nicole@bloomwallet.io>
2024-06-28 10:50:08 +0100

    fix: prevent creating duplicate sessions (#2678)
    Co-authored-by: Mark Nardi <nardimark7@gmail.com>

--- packages/shared/src/lib/auxiliary/wallet-connect/actions/approveSession.ts
+++ packages/shared/src/lib/auxiliary/wallet-connect/actions/approveSession.ts
@@ -3,14 +3,18 @@ import { handleError } from '@core/error/handlers'
 import { buildApprovedNamespaces } from '@walletconnect/utils'
 import { Web3WalletTypes } from '@walletconnect/web3wallet'
 import { ISession } from '../interface/session.interface'
-import { getWalletClient, setConnectedDapps } from '../stores'
+import { connectedDapps, getWalletClient, setConnectedDapps } from '../stores'
 import { updateAccountForDappSession } from './updateAccountForDappSession'
 import { ISupportedNamespace } from '../types'
 import { normalizeSessionNamespace } from '../utils'
+import { IDappMetadata } from '../interface'
+import { get } from 'svelte/store'
+import { disconnectDapp } from './disconnectDapp'
 
 export async function approveSession(
     sessionProposal: Web3WalletTypes.SessionProposal,
     supportedNamespaces: Record<string, ISupportedNamespace>,
+    dappMetadata: IDappMetadata,
     account: IAccountState
 ): Promise<void> {
     const { id, params } = sessionProposal
@@ -21,6 +25,11 @@ export async function approveSession(
     })
 
     try {
+        const dapp = get(connectedDapps).find((_dapp) => _dapp.metadata.url === dappMetadata.url)
+        if (dapp?.sessionTopic) {
+            await disconnectDapp(dapp)
+        }
+
         const session = (await getWalletClient()?.approveSession({
             id,
             namespaces: approvedNamespaces,
