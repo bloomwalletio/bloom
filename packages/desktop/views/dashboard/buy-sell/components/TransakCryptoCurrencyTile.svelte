307b0493fe07c0a051e50a669d443c84451a292c
Nicole O'Brien <nicole@bloomwallet.io>
2024-08-13 14:27:31 +0100

    refactor: create network name pill component

--- packages/desktop/views/dashboard/buy-sell/components/TransakCryptoCurrencyTile.svelte
+++ packages/desktop/views/dashboard/buy-sell/components/TransakCryptoCurrencyTile.svelte
@@ -1,27 +1,13 @@
 <script lang="ts">
-    import { Icon, IconName, Tile, Text, Pill } from '@bloomwalletio/ui'
     import { TransakCryptoCurrency } from '@auxiliary/transak/stores'
+    import { Icon, IconName, Text, Tile } from '@bloomwalletio/ui'
+    import { NetworkNamePill } from '@ui/pills'
     import TransakCryptoCurrencyAvatar from './TransakCryptoCurrencyAvatar.svelte'
-    import { NetworkId, SupportedNetworkId } from '@core/network'
 
     export let cryptoCurrency: TransakCryptoCurrency | undefined
     export let onClick: (() => unknown) | undefined = undefined
     export let selected = false
     export let variant: 'selector' | 'item' = 'item'
-
-    const PILL_COLOR: { [id in NetworkId]: string } = {
-        [SupportedNetworkId.Iota]: 'iota',
-        [SupportedNetworkId.Shimmer]: 'shimmer',
-        [SupportedNetworkId.Testnet]: 'shimmer',
-        [SupportedNetworkId.IotaTestnet]: 'iota',
-        [SupportedNetworkId.IotaEvm]: 'iota',
-        [SupportedNetworkId.ShimmerEvm]: 'shimmer',
-        [SupportedNetworkId.IotaTestnetEvm]: 'iota',
-        [SupportedNetworkId.TestnetEvm]: 'shimmer',
-        [SupportedNetworkId.Ethereum]: 'ethereum',
-        [SupportedNetworkId.Sepolia]: 'ethereum',
-        [SupportedNetworkId.GenericEvm]: 'ethereum',
-    }
 </script>
 
 <Tile {onClick} {selected} surface={1} width="full" class={cryptoCurrency ? '' : 'animate-pulse'}>
@@ -33,21 +19,21 @@
                     <Text>{cryptoCurrency?.symbol ?? '​'}</Text>
                     <Text fontWeight="medium">{cryptoCurrency?.name ?? '​'}</Text>
                 </div>
-                <Pill color={PILL_COLOR[cryptoCurrency?.network?.id ?? SupportedNetworkId.GenericEvm]} compact>
-                    <Text type="xs" textColor="current" transform="uppercase">
-                        {cryptoCurrency?.network.name ?? '​'}
-                    </Text>
-                </Pill>
+                <NetworkNamePill
+                    networkId={cryptoCurrency?.network.id}
+                    networkName={cryptoCurrency?.network.name}
+                    compact
+                />
             </div>
         {:else}
             <div class="flex w-full justify-between items-center">
                 <div class="flex flex-col">
                     <Text>{cryptoCurrency?.symbol ?? '​'}</Text>
-                    <Pill color={PILL_COLOR[cryptoCurrency?.network?.id ?? SupportedNetworkId.GenericEvm]} compact>
-                        <Text type="xs" textColor="current" transform="uppercase">
-                            {cryptoCurrency?.network.name ?? '​'}
-                        </Text>
-                    </Pill>
+                    <NetworkNamePill
+                        networkId={cryptoCurrency?.network.id}
+                        networkName={cryptoCurrency?.network.name}
+                        compact
+                    />
                 </div>
                 <Icon name={IconName.ChevronDown} size="sm" />
             </div>
