0b8d60680c0b68a4fd787d7f687361880ca50dae
Nicole O'Brien <nicole@bloomwallet.io>
2024-09-04 15:46:23 +0100

    add testnet chain ids and organise

--- packages/shared/src/lib/core/network/enums/chain-id.enum.ts
+++ packages/shared/src/lib/core/network/enums/chain-id.enum.ts
@@ -1,13 +1,33 @@
 export enum ChainId {
+    // IOTA
     IotaEvm = '8822',
-    ShimmerEvm = '148',
     IotaTestnetEvm = '1075',
+
+    // Shimmer
+    ShimmerEvm = '148',
     TestnetEvm = '1073',
+
+    // Ethereum
     Ethereum = '1',
     Sepolia = '11155111',
-    Optimism = '10',
-    Blast = '81457',
-    Base = '8453',
-    Immutable = '13371',
+
+    // Arbitrum
     Arbitrum = '42161',
+    ArbitrumSepoliaTestnet = '421614',
+
+    // Base
+    Base = '8453',
+    BaseSepoliaTestnet = '84532',
+
+    // Blast
+    Blast = '238',
+    BlastSepoliaTestnet = '168587773',
+
+    // Optimism
+    Optimism = '10',
+    OptimismSepoliaTestnet = '11155420',
+
+    // Immutable
+    Immutable = '13371',
+    ImmutableTestnet = '13473',
 }
