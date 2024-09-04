62d3ee0d9a313eeede5609de6c194d8986f38da9
Nicole O'Brien <nicole@bloomwallet.io>
2024-09-04 15:12:27 +0100

    add migration to remove tangle bay nodes on production shimmer profiles

--- packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
+++ packages/shared/src/lib/core/profile/constants/profile-version.constant.ts
@@ -3,5 +3,5 @@ import { AppStage } from '@core/app/enums'
 export const PROFILE_VERSION: Record<AppStage, number> = {
     [AppStage.ALPHA]: 24,
     [AppStage.BETA]: 1,
-    [AppStage.PROD]: 13,
+    [AppStage.PROD]: 14,
 }
