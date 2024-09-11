6e2c1baeb50aa9eca4751af97e0a014ccd9dfdc6
Nicole O'Brien <nicole@bloomwallet.io>
2024-09-11 14:39:07 +0100

    disable feature flag

--- packages/desktop/features/network.features.ts
+++ packages/desktop/features/network.features.ts
@@ -5,7 +5,7 @@ const networkFeatures: INetworkFeatures = {
     config: {
         enabled: true,
         manageNetworks: {
-            enabled: true,
+            enabled: false,
             customChain: {
                 enabled: false,
             },
